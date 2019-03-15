8ca161ad4aee9187f35014b828874bb04423923a
Aarthisk <aarthisk@live.com>
2019-03-15 14:46:26 -0700

    HPA scaling

--- pkg/scalers/azure_queue_scaler.go
+++ pkg/scalers/azure_queue_scaler.go
@@ -2,7 +2,20 @@ package scalers
 
 import (
 	"context"
+
 	log "github.com/Sirupsen/logrus"
+	v2beta1 "k8s.io/api/autoscaling/v2beta1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/metrics/pkg/apis/external_metrics"
+)
+
+const (
+	queueLengthMetricName = "queue-length"
+	targetQueueLength     = 5
+	queueLabelName        = "azure-queue-name"
+	externalMetricType    = "External"
 )
 
 type AzureQueueScaler struct {
@@ -26,6 +39,34 @@ func (s *AzureQueueScaler) GetScaleDecision(ctx context.Context) (int32, error) 
 	}
 
 	return 0, nil
+}
+
+func (s *AzureQueueScaler) GetMetricSpecForScaling() []v2beta1.MetricSpec {
+	targetQueueLengthQty := resource.NewQuantity(targetQueueLength, resource.DecimalSI)
+	externalMetric := &v2beta1.ExternalMetricSource{MetricName: queueLengthMetricName, TargetAverageValue: targetQueueLengthQty}
+	metricSpec := v2beta1.MetricSpec{External: externalMetric, Type: externalMetricType}
+	return []v2beta1.MetricSpec{metricSpec}
+}
+
+// returns value for a supported metric and an error if there is a problem getting the metric
+func (s *AzureQueueScaler) GetMetrics(ctx context.Context, merticName string, metricSelector labels.Selector) ([]external_metrics.ExternalMetricValue, error) {
+	connectionString := getConnectionString(s)
+	queueName := getQueueName(s)
+
+	queuelen, err := getQueueLength(ctx, connectionString, queueName)
+
+	if err != nil {
+		log.Errorf("error getting queue lenngth %s", err)
+		return []external_metrics.ExternalMetricValue{}, err
+	}
+
+	metric := external_metrics.ExternalMetricValue{
+		MetricName: merticName,
+		Value:      *resource.NewQuantity(int64(queuelen), resource.DecimalSI),
+		Timestamp:  metav1.Now(),
+	}
+
+	return append([]external_metrics.ExternalMetricValue{}, metric), nil
 }
 
 func getConnectionString(s *AzureQueueScaler) string {
