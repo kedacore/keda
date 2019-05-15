f9741b5385def1d162a65adcfb47aab78f8cabcc
Zach Dunton <zach.dunton@smartfrog.com>
2019-05-15 10:15:27 +0200

    Update Aws Sqs Scaler test.

--- pkg/scalers/aws_sqs_queue_test.go
+++ pkg/scalers/aws_sqs_queue_test.go
@@ -2,15 +2,25 @@ package scalers
 
 import (
 	"context"
+	"os"
 	"testing"
 )
 
 func TestGetQueueLengthReal(t *testing.T) {
-	length, err := GetAwsSqsQueueLength(context.TODO(), "")
-	t.Error(err)
+	queueURL := os.Getenv("AWS_SQS_QUEUE_URL")
+
+	t.Log("This test will use the environment variable AWS_SQS_QUEUE_URL if it is set")
+	t.Log("Ensure that AWS credentials are configured to be able to access the queue")
+	t.Log("If set, it will connect to the specified SQS Queue & check:")
+	t.Logf("\tQueue '%s' has 0 message\n", queueURL)
+
+	length, err := GetAwsSqsQueueLength(context.TODO(), queueURL)
+	if err != nil {
+		t.Error(err)
+	}
 	t.Log("QueueLength = ", length)
 
-	if length != -1 {
-		t.Error("Expected length to be -1, but got", length)
+	if length != 0 {
+		t.Error("Expected length to be 0, but got", length)
 	}
 }
