127c3a35dd8e44835903ba79f6000e1922523b45
ramz <ramzthecoder@gmail.com>
2019-07-03 21:17:21 +1000

    Updating the formatting for the test data

--- pkg/scalers/aws_cloudwatch_test.go
+++ pkg/scalers/aws_cloudwatch_test.go
@@ -17,34 +17,86 @@ type parseAWSCloudwatchMetadataTestData struct {
 var testAWSCloudwatchMetadata = []parseAWSCloudwatchMetadataTestData{
 	{map[string]string{}, true},
 	// properly formed cloudwatch query and region
-	{map[string]string{"namespace": "AWS/SQS", "dimensionName": "QueueName", "dimensionValue": "keda",
-		"metricName": "ApproximateNumberOfMessagesVisible", "targetMetricValue": "2", "minMetricValue": "0",
-		"region": "eu-west-1", "awsAccessKeyID": "AWS_ACCESS_KEY", "awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"}, false},
+	{map[string]string{
+		"namespace": "AWS/SQS",
+		"dimensionName": "QueueName",
+		"dimensionValue": "keda",
+		"metricName": "ApproximateNumberOfMessagesVisible",
+		"targetMetricValue": "2",
+		"minMetricValue": "0",
+		"region": "eu-west-1",
+		"awsAccessKeyID": "AWS_ACCESS_KEY",
+		"awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"},
+		false},
 		// Properly formed cloudwatch query with optional parameters
-	{map[string]string{"namespace": "AWS/SQS", "dimensionName": "QueueName", "dimensionValue": "keda",
-		"metricName": "ApproximateNumberOfMessagesVisible", "targetMetricValue": "2", "minMetricValue": "0",
-		"metricCollectionTime": "300", "metricStat": "Average", "metricStatPeriod": "300",
-		"region": "eu-west-1", "awsAccessKeyID": "AWS_ACCESS_KEY", "awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"}, false},
+	{map[string]string{
+		"namespace": "AWS/SQS",
+		"dimensionName": "QueueName",
+		"dimensionValue": "keda",
+		"metricName": "ApproximateNumberOfMessagesVisible",
+		"targetMetricValue": "2",
+		"minMetricValue": "0",
+		"metricCollectionTime": "300",
+		"metricStat": "Average",
+		"metricStatPeriod": "300",
+		"region": "eu-west-1",
+		"awsAccessKeyID": "AWS_ACCESS_KEY",
+		"awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"},
+		false},
 		// properly formed cloudwatch query but Region is empty
-	{map[string]string{"namespace": "AWS/SQS", "dimensionName": "QueueName", "dimensionValue": "keda",
-		"metricName": "ApproximateNumberOfMessagesVisible", "targetMetricValue": "2", "minMetricValue": "0",
-		"region": "", "awsAccessKeyID": "AWS_ACCESS_KEY", "awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"}, true},
-		// Missing namespace
-	{map[string]string{"dimensionName": "QueueName", "dimensionValue": "keda",
-		"metricName": "ApproximateNumberOfMessagesVisible", "targetMetricValue": "2", "minMetricValue": "0",
-		"region": "eu-west-1", "awsAccessKeyID": "AWS_ACCESS_KEY", "awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"}, true},
-		// Missing dimensionName
-	{map[string]string{"dimensionName": "QueueName", "dimensionValue": "keda",
-		"metricName": "ApproximateNumberOfMessagesVisible", "targetMetricValue": "2", "minMetricValue": "0",
-		"region": "eu-west-1", "awsAccessKeyID": "AWS_ACCESS_KEY", "awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"}, true},
-		// Missing dimensionValue
-	{map[string]string{"namespace": "AWS/SQS", "dimensionName": "QueueName",
-		"metricName": "ApproximateNumberOfMessagesVisible", "targetMetricValue": "2", "minMetricValue": "0",
-		"region": "eu-west-1", "awsAccessKeyID": "AWS_ACCESS_KEY", "awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"}, true},
-		// Missing metricName
-	{map[string]string{"namespace": "AWS/SQS", "dimensionName": "QueueName", "dimensionValue": "keda",
+	{map[string]string{
+		"namespace": "AWS/SQS",
+		"dimensionName": "QueueName",
+		"dimensionValue": "keda",
+		"metricName": "ApproximateNumberOfMessagesVisible",
 		"targetMetricValue": "2", "minMetricValue": "0",
-		"region": "eu-west-1", "awsAccessKeyID": "AWS_ACCESS_KEY", "awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"}, true},
+		"region": "",
+		"awsAccessKeyID": "AWS_ACCESS_KEY",
+		"awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"},
+		true},
+		// Missing namespace
+	{map[string]string{"dimensionName": "QueueName",
+		"dimensionValue": "keda",
+		"metricName": "ApproximateNumberOfMessagesVisible",
+		"targetMetricValue": "2",
+		"minMetricValue": "0",
+		"region": "eu-west-1",
+		"awsAccessKeyID": "AWS_ACCESS_KEY",
+		"awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"},
+		true},
+		// Missing dimensionName
+	{map[string]string{
+		"dimensionName": "QueueName",
+		"dimensionValue": "keda",
+		"metricName": "ApproximateNumberOfMessagesVisible",
+		"targetMetricValue": "2",
+		"minMetricValue": "0",
+		"region": "eu-west-1",
+		"awsAccessKeyID": "AWS_ACCESS_KEY",
+		"awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"},
+		true},
+		// Missing dimensionValue
+	{map[string]string{
+		"namespace": "AWS/SQS",
+		"dimensionName": "QueueName",
+		"metricName": "ApproximateNumberOfMessagesVisible",
+		"targetMetricValue": "2",
+		"minMetricValue": "0",
+		"region": "eu-west-1",
+		"awsAccessKeyID": "AWS_ACCESS_KEY",
+		"awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"},
+		true},
+		// Missing metricName
+	{map[string]string{
+		"namespace": "AWS/SQS",
+		"dimensionName": "QueueName",
+		"dimensionValue": "keda",
+		"targetMetricValue": "2",
+		"minMetricValue": "0",
+		"region": "eu-west-1",
+		"awsAccessKeyID": "AWS_ACCESS_KEY",
+		"awsSecretAccessKey": "AWS_SECRET_ACCESS_KEY"},
+		true},
 }
 
 func TestCloudwatchParseMetadata(t *testing.T) {
