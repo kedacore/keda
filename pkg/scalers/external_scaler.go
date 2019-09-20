f5be75f26ff0668d0aec94eef2c1029caaa7403f
Ahmed ElSayed <ahmels@microsoft.com>
2019-09-20 10:21:21 -0700

    Fix build

--- pkg/scalers/external_scaler.go
+++ pkg/scalers/external_scaler.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	keda_v1alpha1 "github.com/kedacore/keda/pkg/apis/keda/v1alpha1"
 	pb "github.com/kedacore/keda/pkg/scalers/externalscaler"
 	"google.golang.org/grpc"
