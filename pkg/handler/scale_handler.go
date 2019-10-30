d785e633a9c01bae74dc4047dac5f5713564297b
Emad Alashi <emad.ashi@gmail.com>
2019-10-31 04:02:32 +1100

    Don't fail resolving env if could'nt read a value (#404)

--- pkg/handler/scale_handler.go
+++ pkg/handler/scale_handler.go
@@ -408,7 +408,8 @@ func (h *ScaleHandler) resolveEnv(container *core_v1.Container, namespace string
 							namespace)
 					}
 				} else {
-					return nil, fmt.Errorf("cannot resolve env %s to a value. fieldRef and resourceFieldRef env are skipped", envVar.Name)
+					log.Warningf("cannot resolve env %s to a value. fieldRef and resourceFieldRef env are skipped", envVar.Name)
+					continue
 				}
 
 			}
