b54ebe118300b6a57d184569f2143fc5627af565
Ahmed ElSayed <ahmed@elsayed.io>
2019-11-07 16:34:15 -0800

    Stamp GitCommit to each build

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -32,6 +32,7 @@ import (
 
 // Change below variables to serve metrics on different host or port.
 var (
+	GitCommit           string
 	metricsHost               = "0.0.0.0"
 	metricsPort         int32 = 8383
 	operatorMetricsPort int32 = 8686
@@ -42,6 +43,7 @@ func printVersion() {
 	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
 	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
 	log.Info(fmt.Sprintf("Version of operator-sdk: %v", sdkVersion.Version))
+	log.Info(fmt.Sprintf("Keda Commit: %s", GitCommit))
 }
 
 func main() {
