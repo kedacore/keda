f931ab337ac09f497fd7f6382be1844d0e05bfd9
Thomas Lamure <thomas.lamure@eridanis.com>
2020-02-21 10:04:24 +0100

    add tests

--- pkg/scalers/azure_queue_test.go
+++ pkg/scalers/azure_queue_test.go
@@ -75,6 +75,33 @@ func TestGetQueueLength(t *testing.T) {
 	if !strings.Contains(err.Error(), "illegal base64") {
 		t.Error("Expected error to contain base64 error message, but got", err.Error())
 	}
+
+	length, err := GetAzureVisibleQueueLength(context.TODO(), "", "", "queueName", "")
+	if length != -1 {
+		t.Error("Expected length to be -1, but got", length)
+	}
+
+	if err == nil {
+		t.Error("Expected error for empty connection string, but got nil")
+	}
+
+	if !strings.Contains(err.Error(), "parse storage connection string") {
+		t.Error("Expected error to contain parsing error message, but got", err.Error())
+	}
+
+	length, err = GetAzureVisibleQueueLength(context.TODO(), "", "DefaultEndpointsProtocol=https;AccountName=name;AccountKey=key==;EndpointSuffix=core.windows.net", "queueName", "")
+
+	if length != -1 {
+		t.Error("Expected length to be -1, but got", length)
+	}
+
+	if err == nil {
+		t.Error("Expected error for empty connection string, but got nil")
+	}
+
+	if !strings.Contains(err.Error(), "illegal base64") {
+		t.Error("Expected error to contain base64 error message, but got", err.Error())
+	}
 }
 
 var testAzQueueResolvedEnv = map[string]string{
