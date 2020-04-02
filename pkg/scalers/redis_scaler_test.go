cd4b40901e1d37d374b95c1cd6445b7e4b683f39
Ivan <ivan.gualandri@gmail.com>
2020-04-02 11:14:33 +0100

    Add redis host and port parameter to the scaler with tests (#719)
    Signed-off-by: Ivan Gualandri <ivan.gualandri@gmail.com>

--- pkg/scalers/redis_scaler_test.go
+++ pkg/scalers/redis_scaler_test.go
@@ -6,6 +6,7 @@ import (
 
 var testRedisResolvedEnv = map[string]string{
 	"REDIS_HOST":     "none",
+	"REDIS_PORT":     "6379",
 	"REDIS_PASSWORD": "none",
 }
 
@@ -20,6 +21,12 @@ var testRedisMetadata = []parseRedisMetadataTestData{
 	{map[string]string{}, true, map[string]string{}},
 	// properly formed listName
 	{map[string]string{"listName": "mylist", "listLength": "10", "address": "REDIS_HOST", "password": "REDIS_PASSWORD"}, false, map[string]string{}},
+	// properly formed hostPort
+	{map[string]string{"listName": "mylist", "listLength": "10", "host": "REDIS_HOST", "port": "REDIS_PORT", "password": "REDIS_PASSWORD"}, false, map[string]string{}},
+	// properly formed hostPort
+	{map[string]string{"listName": "mylist", "listLength": "10", "address": "REDIS_HOST", "host": "REDIS_HOST", "port": "REDIS_PORT", "password": "REDIS_PASSWORD"}, false, map[string]string{}},
+	// improperly formed hostPort
+	{map[string]string{"listName": "mylist", "listLength": "10", "host": "REDIS_HOST", "password": "REDIS_PASSWORD"}, true, map[string]string{}},
 	// properly formed listName, empty address
 	{map[string]string{"listName": "mylist", "listLength": "10", "address": "", "password": ""}, true, map[string]string{}},
 	// improperly formed listLength
