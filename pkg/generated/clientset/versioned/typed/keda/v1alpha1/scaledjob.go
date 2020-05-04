1e4b8dbba02c28a881c55295daa84e1bbbebf818
Zbynek Roubalik <726523+zroubalik@users.noreply.github.com>
2020-05-05 00:29:50 +0200

    [V2] bump k8s to 1.18 and remove knative.dev deps (#803)

--- pkg/generated/clientset/versioned/typed/keda/v1alpha1/scaledjob.go
+++ pkg/generated/clientset/versioned/typed/keda/v1alpha1/scaledjob.go
@@ -18,6 +18,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
 	"time"
 
 	v1alpha1 "github.com/kedacore/keda/pkg/apis/keda/v1alpha1"
@@ -36,15 +37,15 @@ type ScaledJobsGetter interface {
 
 // ScaledJobInterface has methods to work with ScaledJob resources.
 type ScaledJobInterface interface {
-	Create(*v1alpha1.ScaledJob) (*v1alpha1.ScaledJob, error)
-	Update(*v1alpha1.ScaledJob) (*v1alpha1.ScaledJob, error)
-	UpdateStatus(*v1alpha1.ScaledJob) (*v1alpha1.ScaledJob, error)
-	Delete(name string, options *v1.DeleteOptions) error
-	DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error
-	Get(name string, options v1.GetOptions) (*v1alpha1.ScaledJob, error)
-	List(opts v1.ListOptions) (*v1alpha1.ScaledJobList, error)
-	Watch(opts v1.ListOptions) (watch.Interface, error)
-	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.ScaledJob, err error)
+	Create(ctx context.Context, scaledJob *v1alpha1.ScaledJob, opts v1.CreateOptions) (*v1alpha1.ScaledJob, error)
+	Update(ctx context.Context, scaledJob *v1alpha1.ScaledJob, opts v1.UpdateOptions) (*v1alpha1.ScaledJob, error)
+	UpdateStatus(ctx context.Context, scaledJob *v1alpha1.ScaledJob, opts v1.UpdateOptions) (*v1alpha1.ScaledJob, error)
+	Delete(ctx context.Context, name string, opts v1.DeleteOptions) error
+	DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error
+	Get(ctx context.Context, name string, opts v1.GetOptions) (*v1alpha1.ScaledJob, error)
+	List(ctx context.Context, opts v1.ListOptions) (*v1alpha1.ScaledJobList, error)
+	Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error)
+	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.ScaledJob, err error)
 	ScaledJobExpansion
 }
 
@@ -63,20 +64,20 @@ func newScaledJobs(c *KedaV1alpha1Client, namespace string) *scaledJobs {
 }
 
 // Get takes name of the scaledJob, and returns the corresponding scaledJob object, and an error if there is any.
-func (c *scaledJobs) Get(name string, options v1.GetOptions) (result *v1alpha1.ScaledJob, err error) {
+func (c *scaledJobs) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.ScaledJob, err error) {
 	result = &v1alpha1.ScaledJob{}
 	err = c.client.Get().
 		Namespace(c.ns).
 		Resource("scaledjobs").
 		Name(name).
 		VersionedParams(&options, scheme.ParameterCodec).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // List takes label and field selectors, and returns the list of ScaledJobs that match those selectors.
-func (c *scaledJobs) List(opts v1.ListOptions) (result *v1alpha1.ScaledJobList, err error) {
+func (c *scaledJobs) List(ctx context.Context, opts v1.ListOptions) (result *v1alpha1.ScaledJobList, err error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
@@ -87,13 +88,13 @@ func (c *scaledJobs) List(opts v1.ListOptions) (result *v1alpha1.ScaledJobList, 
 		Resource("scaledjobs").
 		VersionedParams(&opts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Watch returns a watch.Interface that watches the requested scaledJobs.
-func (c *scaledJobs) Watch(opts v1.ListOptions) (watch.Interface, error) {
+func (c *scaledJobs) Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
@@ -104,87 +105,90 @@ func (c *scaledJobs) Watch(opts v1.ListOptions) (watch.Interface, error) {
 		Resource("scaledjobs").
 		VersionedParams(&opts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Watch()
+		Watch(ctx)
 }
 
 // Create takes the representation of a scaledJob and creates it.  Returns the server's representation of the scaledJob, and an error, if there is any.
-func (c *scaledJobs) Create(scaledJob *v1alpha1.ScaledJob) (result *v1alpha1.ScaledJob, err error) {
+func (c *scaledJobs) Create(ctx context.Context, scaledJob *v1alpha1.ScaledJob, opts v1.CreateOptions) (result *v1alpha1.ScaledJob, err error) {
 	result = &v1alpha1.ScaledJob{}
 	err = c.client.Post().
 		Namespace(c.ns).
 		Resource("scaledjobs").
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(scaledJob).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Update takes the representation of a scaledJob and updates it. Returns the server's representation of the scaledJob, and an error, if there is any.
-func (c *scaledJobs) Update(scaledJob *v1alpha1.ScaledJob) (result *v1alpha1.ScaledJob, err error) {
+func (c *scaledJobs) Update(ctx context.Context, scaledJob *v1alpha1.ScaledJob, opts v1.UpdateOptions) (result *v1alpha1.ScaledJob, err error) {
 	result = &v1alpha1.ScaledJob{}
 	err = c.client.Put().
 		Namespace(c.ns).
 		Resource("scaledjobs").
 		Name(scaledJob.Name).
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(scaledJob).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // UpdateStatus was generated because the type contains a Status member.
 // Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
-
-func (c *scaledJobs) UpdateStatus(scaledJob *v1alpha1.ScaledJob) (result *v1alpha1.ScaledJob, err error) {
+func (c *scaledJobs) UpdateStatus(ctx context.Context, scaledJob *v1alpha1.ScaledJob, opts v1.UpdateOptions) (result *v1alpha1.ScaledJob, err error) {
 	result = &v1alpha1.ScaledJob{}
 	err = c.client.Put().
 		Namespace(c.ns).
 		Resource("scaledjobs").
 		Name(scaledJob.Name).
 		SubResource("status").
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(scaledJob).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Delete takes name of the scaledJob and deletes it. Returns an error if one occurs.
-func (c *scaledJobs) Delete(name string, options *v1.DeleteOptions) error {
+func (c *scaledJobs) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	return c.client.Delete().
 		Namespace(c.ns).
 		Resource("scaledjobs").
 		Name(name).
-		Body(options).
-		Do().
+		Body(&opts).
+		Do(ctx).
 		Error()
 }
 
 // DeleteCollection deletes a collection of objects.
-func (c *scaledJobs) DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error {
+func (c *scaledJobs) DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error {
 	var timeout time.Duration
-	if listOptions.TimeoutSeconds != nil {
-		timeout = time.Duration(*listOptions.TimeoutSeconds) * time.Second
+	if listOpts.TimeoutSeconds != nil {
+		timeout = time.Duration(*listOpts.TimeoutSeconds) * time.Second
 	}
 	return c.client.Delete().
 		Namespace(c.ns).
 		Resource("scaledjobs").
-		VersionedParams(&listOptions, scheme.ParameterCodec).
+		VersionedParams(&listOpts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Body(options).
-		Do().
+		Body(&opts).
+		Do(ctx).
 		Error()
 }
 
 // Patch applies the patch and returns the patched scaledJob.
-func (c *scaledJobs) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.ScaledJob, err error) {
+func (c *scaledJobs) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.ScaledJob, err error) {
 	result = &v1alpha1.ScaledJob{}
 	err = c.client.Patch(pt).
 		Namespace(c.ns).
 		Resource("scaledjobs").
-		SubResource(subresources...).
 		Name(name).
+		SubResource(subresources...).
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(data).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
--- pkg/generated/clientset/versioned/typed/keda/v1alpha1/triggerauthentication.go
+++ pkg/generated/clientset/versioned/typed/keda/v1alpha1/triggerauthentication.go
@@ -18,6 +18,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
 	"time"
 
 	v1alpha1 "github.com/kedacore/keda/pkg/apis/keda/v1alpha1"
@@ -36,14 +37,14 @@ type TriggerAuthenticationsGetter interface {
 
 // TriggerAuthenticationInterface has methods to work with TriggerAuthentication resources.
 type TriggerAuthenticationInterface interface {
-	Create(*v1alpha1.TriggerAuthentication) (*v1alpha1.TriggerAuthentication, error)
-	Update(*v1alpha1.TriggerAuthentication) (*v1alpha1.TriggerAuthentication, error)
-	Delete(name string, options *v1.DeleteOptions) error
-	DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error
-	Get(name string, options v1.GetOptions) (*v1alpha1.TriggerAuthentication, error)
-	List(opts v1.ListOptions) (*v1alpha1.TriggerAuthenticationList, error)
-	Watch(opts v1.ListOptions) (watch.Interface, error)
-	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.TriggerAuthentication, err error)
+	Create(ctx context.Context, triggerAuthentication *v1alpha1.TriggerAuthentication, opts v1.CreateOptions) (*v1alpha1.TriggerAuthentication, error)
+	Update(ctx context.Context, triggerAuthentication *v1alpha1.TriggerAuthentication, opts v1.UpdateOptions) (*v1alpha1.TriggerAuthentication, error)
+	Delete(ctx context.Context, name string, opts v1.DeleteOptions) error
+	DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error
+	Get(ctx context.Context, name string, opts v1.GetOptions) (*v1alpha1.TriggerAuthentication, error)
+	List(ctx context.Context, opts v1.ListOptions) (*v1alpha1.TriggerAuthenticationList, error)
+	Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error)
+	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.TriggerAuthentication, err error)
 	TriggerAuthenticationExpansion
 }
 
@@ -62,20 +63,20 @@ func newTriggerAuthentications(c *KedaV1alpha1Client, namespace string) *trigger
 }
 
 // Get takes name of the triggerAuthentication, and returns the corresponding triggerAuthentication object, and an error if there is any.
-func (c *triggerAuthentications) Get(name string, options v1.GetOptions) (result *v1alpha1.TriggerAuthentication, err error) {
+func (c *triggerAuthentications) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.TriggerAuthentication, err error) {
 	result = &v1alpha1.TriggerAuthentication{}
 	err = c.client.Get().
 		Namespace(c.ns).
 		Resource("triggerauthentications").
 		Name(name).
 		VersionedParams(&options, scheme.ParameterCodec).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // List takes label and field selectors, and returns the list of TriggerAuthentications that match those selectors.
-func (c *triggerAuthentications) List(opts v1.ListOptions) (result *v1alpha1.TriggerAuthenticationList, err error) {
+func (c *triggerAuthentications) List(ctx context.Context, opts v1.ListOptions) (result *v1alpha1.TriggerAuthenticationList, err error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
@@ -86,13 +87,13 @@ func (c *triggerAuthentications) List(opts v1.ListOptions) (result *v1alpha1.Tri
 		Resource("triggerauthentications").
 		VersionedParams(&opts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Watch returns a watch.Interface that watches the requested triggerAuthentications.
-func (c *triggerAuthentications) Watch(opts v1.ListOptions) (watch.Interface, error) {
+func (c *triggerAuthentications) Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
@@ -103,71 +104,74 @@ func (c *triggerAuthentications) Watch(opts v1.ListOptions) (watch.Interface, er
 		Resource("triggerauthentications").
 		VersionedParams(&opts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Watch()
+		Watch(ctx)
 }
 
 // Create takes the representation of a triggerAuthentication and creates it.  Returns the server's representation of the triggerAuthentication, and an error, if there is any.
-func (c *triggerAuthentications) Create(triggerAuthentication *v1alpha1.TriggerAuthentication) (result *v1alpha1.TriggerAuthentication, err error) {
+func (c *triggerAuthentications) Create(ctx context.Context, triggerAuthentication *v1alpha1.TriggerAuthentication, opts v1.CreateOptions) (result *v1alpha1.TriggerAuthentication, err error) {
 	result = &v1alpha1.TriggerAuthentication{}
 	err = c.client.Post().
 		Namespace(c.ns).
 		Resource("triggerauthentications").
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(triggerAuthentication).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Update takes the representation of a triggerAuthentication and updates it. Returns the server's representation of the triggerAuthentication, and an error, if there is any.
-func (c *triggerAuthentications) Update(triggerAuthentication *v1alpha1.TriggerAuthentication) (result *v1alpha1.TriggerAuthentication, err error) {
+func (c *triggerAuthentications) Update(ctx context.Context, triggerAuthentication *v1alpha1.TriggerAuthentication, opts v1.UpdateOptions) (result *v1alpha1.TriggerAuthentication, err error) {
 	result = &v1alpha1.TriggerAuthentication{}
 	err = c.client.Put().
 		Namespace(c.ns).
 		Resource("triggerauthentications").
 		Name(triggerAuthentication.Name).
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(triggerAuthentication).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Delete takes name of the triggerAuthentication and deletes it. Returns an error if one occurs.
-func (c *triggerAuthentications) Delete(name string, options *v1.DeleteOptions) error {
+func (c *triggerAuthentications) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	return c.client.Delete().
 		Namespace(c.ns).
 		Resource("triggerauthentications").
 		Name(name).
-		Body(options).
-		Do().
+		Body(&opts).
+		Do(ctx).
 		Error()
 }
 
 // DeleteCollection deletes a collection of objects.
-func (c *triggerAuthentications) DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error {
+func (c *triggerAuthentications) DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error {
 	var timeout time.Duration
-	if listOptions.TimeoutSeconds != nil {
-		timeout = time.Duration(*listOptions.TimeoutSeconds) * time.Second
+	if listOpts.TimeoutSeconds != nil {
+		timeout = time.Duration(*listOpts.TimeoutSeconds) * time.Second
 	}
 	return c.client.Delete().
 		Namespace(c.ns).
 		Resource("triggerauthentications").
-		VersionedParams(&listOptions, scheme.ParameterCodec).
+		VersionedParams(&listOpts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Body(options).
-		Do().
+		Body(&opts).
+		Do(ctx).
 		Error()
 }
 
 // Patch applies the patch and returns the patched triggerAuthentication.
-func (c *triggerAuthentications) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.TriggerAuthentication, err error) {
+func (c *triggerAuthentications) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.TriggerAuthentication, err error) {
 	result = &v1alpha1.TriggerAuthentication{}
 	err = c.client.Patch(pt).
 		Namespace(c.ns).
 		Resource("triggerauthentications").
-		SubResource(subresources...).
 		Name(name).
+		SubResource(subresources...).
+		VersionedParams(&opts, scheme.ParameterCodec).
 		Body(data).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
