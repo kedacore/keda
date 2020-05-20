4abddb39b30189c1af067bf93bead1ab67c77f1d
Zbynek Roubalik <zroubali@redhat.com>
2020-05-20 10:21:52 +0200

    resolve merge conflicts after rebase
    Signed-off-by: Zbynek Roubalik <zroubali@redhat.com>

--- pkg/scalers/azure_blob_scaler_test.go
+++ pkg/scalers/azure_blob_scaler_test.go
@@ -1,39 +1,6 @@
 package scalers
 
-import (
-	"context"
-	"strings"
-	"testing"
-)
-
-func TestGetBlobLength(t *testing.T) {
-	length, err := GetAzureBlobListLength(context.TODO(), "", "", "blobContainerName", "", "", "")
-	if length != -1 {
-		t.Error("Expected length to be -1, but got", length)
-	}
-
-	if err == nil {
-		t.Error("Expected error for empty connection string, but got nil")
-	}
-
-	if !strings.Contains(err.Error(), "parse storage connection string") {
-		t.Error("Expected error to contain parsing error message, but got", err.Error())
-	}
-
-	length, err = GetAzureBlobListLength(context.TODO(), "", "DefaultEndpointsProtocol=https;AccountName=name;AccountKey=key==;EndpointSuffix=core.windows.net", "blobContainerName", "", "", "")
-
-	if length != -1 {
-		t.Error("Expected length to be -1, but got", length)
-	}
-
-	if err == nil {
-		t.Error("Expected error for empty connection string, but got nil")
-	}
-
-	if !strings.Contains(err.Error(), "illegal base64") {
-		t.Error("Expected error to contain base64 error message, but got", err.Error())
-	}
-}
+import "testing"
 
 var testAzBlobResolvedEnv = map[string]string{
 	"CONNECTION": "SAMPLE",
