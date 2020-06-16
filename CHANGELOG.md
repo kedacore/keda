20b419954d39762e63931f17bcd3396d307738d9
Ahmed ElSayed <ahmels@microsoft.com>
2020-06-16 00:06:41 -0700

    Add PushScaler interface and impl external-push scaler (#865)
    * Add PushScaler interface and impl external-push scaler
    
    Closes #820
    
    Signed-off-by: Ahmed ElSayed <ahmels@microsoft.com>
    
    * pass metadata to scaler
    
    Signed-off-by: Ahmed ElSayed <ahmels@microsoft.com>
    
    * add section for breaking changes in the CHANGELOG
    
    Signed-off-by: Ahmed ElSayed <ahmels@microsoft.com>
    
    * use correct protoc-gen-go version
    
    Signed-off-by: Ahmed ElSayed <ahmels@microsoft.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,12 +6,19 @@
 
 ## History
 
+- [v2.0.0](#v200)
 - [v1.4.1](#v141)
 - [v1.4.0](#v140)
 - [v1.3.0](#v130)
 - [v1.2.0](#v120)
 - [v1.1.0](#v110)
 - [v1.0.0](#v100)
+
+## v2.0.0
+
+### Breaking Changes
+
+- Remove `New()` and `Close()` from the interface of `service ExternalScaler` in `externalscaler.proto`.
 
 ## v1.4.1
 
--- pkg/scalers/external_scaler.go
+++ pkg/scalers/external_scaler.go
@@ -3,6 +3,9 @@ package scalers
 import (
 	"context"
 	"fmt"
+	"github.com/mitchellh/hashstructure"
+	"sync"
+	"time"
 
 	pb "github.com/kedacore/keda/pkg/scalers/externalscaler"
 	"google.golang.org/grpc"
@@ -16,95 +19,95 @@ import (
 )
 
 type externalScaler struct {
-	metadata        *externalScalerMetadata
+	metadata        externalScalerMetadata
 	scaledObjectRef pb.ScaledObjectRef
-	grpcClient      pb.ExternalScalerClient
-	grpcConnection  *grpc.ClientConn
+}
+
+type externalPushScaler struct {
+	externalScaler
 }
 
 type externalScalerMetadata struct {
 	scalerAddress string
 	tlsCertFile   string
-	metadata      map[string]string
+	originalMetadata      map[string]string
 }
+
+type connectionGroup struct {
+	grpcConnection *grpc.ClientConn
+	waitGroup      *sync.WaitGroup
+}
+
+// a pool of connectionGroup per metadata hash
+var connectionPool sync.Map
 
 var externalLog = logf.Log.WithName("external_scaler")
 
 // NewExternalScaler creates a new external scaler - calls the GRPC interface
 // to create a new scaler
-func NewExternalScaler(name, namespace string, resolvedEnv, metadata map[string]string) (Scaler, error) {
-
-	meta, err := parseExternalScalerMetadata(metadata, resolvedEnv)
+func NewExternalScaler(name, namespace string, metadata map[string]string) (Scaler, error) {
+	meta, err := parseExternalScalerMetadata(metadata)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing external scaler metadata: %s", err)
 	}
 
-	scaler := &externalScaler{
+	return &externalScaler{
 		metadata: meta,
 		scaledObjectRef: pb.ScaledObjectRef{
 			Name:      name,
 			Namespace: namespace,
 		},
-	}
-
-	// TODO: Pass Context
-	ctx := context.Background()
-
-	// Call GRPC Interface to parse metadata
-	err = scaler.getGRPCClient()
-	if err != nil {
-		return nil, err
-	}
-
-	request := &pb.NewRequest{
-		ScaledObjectRef: &scaler.scaledObjectRef,
-		Metadata:        scaler.metadata.metadata,
-	}
-
-	_, err = scaler.grpcClient.New(ctx, request)
-	if err != nil {
-		return nil, err
-	}
-
-	return scaler, nil
+	}, nil
 }
 
-func parseExternalScalerMetadata(metadata, resolvedEnv map[string]string) (*externalScalerMetadata, error) {
-	meta := externalScalerMetadata{}
+func NewExternalPushScaler(name, namespace string, metadata map[string]string) (PushScaler, error) {
+	meta, err := parseExternalScalerMetadata(metadata)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing external scaler metadata: %s", err)
+	}
+
+	return &externalPushScaler{
+		externalScaler{
+			metadata: meta,
+			scaledObjectRef: pb.ScaledObjectRef{
+				Name:      name,
+				Namespace: namespace,
+				ScalerMetadata: meta.originalMetadata,
+			},
+		},
+	}, nil
+}
+
+func parseExternalScalerMetadata(metadata map[string]string) (externalScalerMetadata, error) {
+	meta := externalScalerMetadata{
+		originalMetadata: metadata,
+	}
 
 	// Check if scalerAddress is present
 	if val, ok := metadata["scalerAddress"]; ok && val != "" {
 		meta.scalerAddress = val
 	} else {
-		return nil, fmt.Errorf("Scaler Address is a required field")
+		return meta, fmt.Errorf("scaler Address is a required field")
 	}
 
 	if val, ok := metadata["tlsCertFile"]; ok && val != "" {
 		meta.tlsCertFile = val
 	}
 
-	meta.metadata = make(map[string]string)
-
-	// Add elements to metadata
-	for key, value := range metadata {
-		// Check if key is in resolved environment and resolve
-		if val, ok := resolvedEnv[value]; ok && val != "" {
-			meta.metadata[key] = val
-		} else {
-			meta.metadata[key] = value
-		}
-	}
-
-	return &meta, nil
+	return meta, nil
 }
 
 // IsActive checks if there are any messages in the subscription
 func (s *externalScaler) IsActive(ctx context.Context) (bool, error) {
-
-	// Call GRPC Interface to check if active
-	response, err := s.grpcClient.IsActive(ctx, &s.scaledObjectRef)
+	grpcClient, done, err := getClientForConnectionPool(s.metadata)
 	if err != nil {
-		externalLog.Error(err, "error")
+		return false, err
+	}
+	defer done()
+
+	response, err := grpcClient.IsActive(ctx, &s.scaledObjectRef)
+	if err != nil {
+		externalLog.Error(err, "error calling IsActive on external scaler")
 		return false, err
 	}
 
@@ -112,35 +115,25 @@ func (s *externalScaler) IsActive(ctx context.Context) (bool, error) {
 }
 
 func (s *externalScaler) Close() error {
-	// Call GRPC Interface to close connection
-
-	// TODO: Pass Context
-	ctx := context.Background()
-
-	_, err := s.grpcClient.Close(ctx, &s.scaledObjectRef)
-	if err != nil {
-		externalLog.Error(err, "error")
-		return err
-	}
-	defer s.grpcConnection.Close()
-
 	return nil
 }
 
 // GetMetricSpecForScaling returns the metric spec for the HPA
 func (s *externalScaler) GetMetricSpecForScaling() []v2beta2.MetricSpec {
+	var result []v2beta2.MetricSpec
 
-	// TODO: Pass Context
-	ctx := context.Background()
+	grpcClient, done, err := getClientForConnectionPool(s.metadata)
+	if err != nil {
+		externalLog.Error(err, "error building grpc connection")
+		return result
+	}
+	defer done()
 
-	// Call GRPC Interface to get metric specs
-	response, err := s.grpcClient.GetMetricSpec(ctx, &s.scaledObjectRef)
+	response, err := grpcClient.GetMetricSpec(context.TODO(), &s.scaledObjectRef)
 	if err != nil {
 		externalLog.Error(err, "error")
 		return nil
 	}
-
-	var result []v2beta2.MetricSpec
 
 	for _, spec := range response.MetricSpecs {
 		// Construct the target subscription size as a quantity
@@ -170,16 +163,19 @@ func (s *externalScaler) GetMetricSpecForScaling() []v2beta2.MetricSpec {
 
 // GetMetrics connects calls the gRPC interface to get the metrics with a specific name
 func (s *externalScaler) GetMetrics(ctx context.Context, metricName string, metricSelector labels.Selector) ([]external_metrics.ExternalMetricValue, error) {
-
 	var metrics []external_metrics.ExternalMetricValue
-	// Call GRPC Interface to get metric specs
+	grpcClient, done, err := getClientForConnectionPool(s.metadata)
+	if err != nil {
+		return metrics, err
+	}
+	defer done()
 
 	request := &pb.GetMetricsRequest{
 		MetricName:      metricName,
 		ScaledObjectRef: &s.scaledObjectRef,
 	}
 
-	response, err := s.grpcClient.GetMetrics(ctx, request)
+	response, err := grpcClient.GetMetrics(ctx, request)
 	if err != nil {
 		externalLog.Error(err, "error")
 		return []external_metrics.ExternalMetricValue{}, err
@@ -198,27 +194,122 @@ func (s *externalScaler) GetMetrics(ctx context.Context, metricName string, metr
 	return metrics, nil
 }
 
-// getGRPCClient creates a new gRPC client
-func (s *externalScaler) getGRPCClient() error {
+// handleIsActiveStream is the only writer to the active channel and will close it on return.
+func (s *externalPushScaler) Run(ctx context.Context, active chan<- bool) {
+	defer close(active)
+	// It's possible for the connection to get terminated anytime, we need to run this in a retry loop
+	runWithLog := func() {
+		grpcClient, done, err := getClientForConnectionPool(s.metadata)
+		if err != nil {
+			externalLog.Error(err, "error running internalRun")
+			return
+		}
+		handleIsActiveStream(ctx, s.scaledObjectRef, grpcClient, active)
+		done()
+	}
 
-	var err error
+	// retry on error from runWithLog() starting by 2 sec backing off * 2 with a max of 1 minute
+	retryDuration := time.Second * 2
+	retryBackoff := func() <-chan time.Time {
+		ch := time.After(retryDuration)
+		retryDuration *= time.Second * 2
+		if retryDuration > time.Minute*1 {
+			retryDuration = time.Minute * 1
+		}
+		return ch
+	}
 
-	if s.metadata.tlsCertFile != "" {
-		certFile := fmt.Sprintf("/grpccerts/%s", s.metadata.tlsCertFile)
-		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+	// start the first run without delay
+	runWithLog()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-retryBackoff():
+			runWithLog()
+		}
+	}
+}
+
+// handleIsActiveStream calls blocks on a stream call from the GRPC server. It'll only terminate on error, stream completion, or ctx cancellation.
+func handleIsActiveStream(ctx context.Context, scaledObjectRef pb.ScaledObjectRef, grpcClient pb.ExternalScalerClient, active chan<- bool) error {
+	stream, err := grpcClient.StreamIsActive(ctx, &scaledObjectRef)
+	if err != nil {
+		return err
+	}
+
+	for {
+		resp, err := stream.Recv()
 		if err != nil {
 			return err
 		}
-		s.grpcConnection, err = grpc.Dial(s.metadata.scalerAddress, grpc.WithTransportCredentials(creds))
-	} else {
-		s.grpcConnection, err = grpc.Dial(s.metadata.scalerAddress, grpc.WithInsecure())
+
+		active <- resp.Result
+	}
+}
+
+var connectionPoolMutex sync.Mutex
+
+// getClientForConnectionPool returns a grpcClient and a done() Func. The done() function must be called once the client is no longer
+// in use to clean up the shared grpc.ClientConn
+func getClientForConnectionPool(metadata externalScalerMetadata) (pb.ExternalScalerClient, func(), error) {
+	connectionPoolMutex.Lock()
+	defer connectionPoolMutex.Unlock()
+
+	buildGRPCConnection := func(metadata externalScalerMetadata) (*grpc.ClientConn, error) {
+		if metadata.tlsCertFile != "" {
+			creds, err := credentials.NewClientTLSFromFile(metadata.tlsCertFile, "")
+			if err != nil {
+				return nil, err
+			}
+			return grpc.Dial(metadata.scalerAddress, grpc.WithTransportCredentials(creds))
+		}
+
+		return grpc.Dial(metadata.scalerAddress, grpc.WithInsecure())
 	}
 
+	// create a unique key per-metadata. If scaledObjects share the same connection properties
+	// in the metadata, they will share the same grpc.ClientConn
+	key, err := hashstructure.Hash(metadata, nil)
 	if err != nil {
-		return fmt.Errorf("cannot connect to external scaler over grpc interface: %s", err)
+		return nil, nil, err
 	}
 
-	s.grpcClient = pb.NewExternalScalerClient(s.grpcConnection)
+	if i, ok := connectionPool.Load(key); ok {
+		if connGroup, ok := i.(*connectionGroup); ok {
+			connGroup.waitGroup.Add(1)
+			return pb.NewExternalScalerClient(connGroup.grpcConnection), func() {
+				connGroup.waitGroup.Done()
+			}, nil
+		}
+	}
 
-	return nil
+	conn, err := buildGRPCConnection(metadata)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	waitGroup := &sync.WaitGroup{}
+	waitGroup.Add(1)
+	connGroup := connectionGroup{
+		grpcConnection: conn,
+		waitGroup:      waitGroup,
+	}
+
+	connectionPool.Store(key, connGroup)
+
+	go func() {
+		// clean up goroutine.
+		// once all waitGroup is done, remove the connection from the pool and Close() grpc.ClientConn
+		connGroup.waitGroup.Wait()
+		connectionPoolMutex.Lock()
+		defer connectionPoolMutex.Unlock()
+		connectionPool.Delete(key)
+		connGroup.grpcConnection.Close()
+	}()
+
+	return pb.NewExternalScalerClient(connGroup.grpcConnection), func() {
+		connGroup.waitGroup.Done()
+	}, nil
 }
