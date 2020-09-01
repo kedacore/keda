6c2b33f562782b7eb8d1ba724a5e63a83796c87e
Daniel Cooper <5233036+danielcooperxyz@users.noreply.github.com>
2020-09-01 10:07:30 +0100

    Make pkg/util golint compatible (#1060)
    Signed-off-by: Daniel Cooper <cooper.daniel45@gmail.com>

--- pkg/util/normalize_string.go
+++ pkg/util/normalize_string.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 )
 
+// NormalizeString will replace all slashes, dots and colons with dashes
 func NormalizeString(s string) string {
 	s = strings.ReplaceAll(s, "/", "-")
 	s = strings.ReplaceAll(s, ".", "-")
