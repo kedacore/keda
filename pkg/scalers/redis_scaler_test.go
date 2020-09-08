2c2c8f6daee65b7165a54f7468d18e492fd7cea5
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-09-08 12:51:28 +0100

    1056 Fixed go vet error
    Signed-off-by: Kashif Jamal Soofi <kashif.soofi+git@gmail.com>

--- pkg/scalers/redis_scaler_test.go
+++ pkg/scalers/redis_scaler_test.go
@@ -1,7 +1,6 @@
 package scalers
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -57,7 +56,7 @@ func TestRedisParseMetadata(t *testing.T) {
 	for _, testData := range testRedisMetadata {
 		_, err := parseRedisMetadata(testData.metadata, testRedisResolvedEnv, testData.authParams)
 		if err != nil && !testData.isError {
-			t.Errorf("Expected success but got error for unit test # %v", testCaseNum, err)
+			t.Errorf("Expected success but got error for unit test # %v", testCaseNum)
 		}
 		if testData.isError && err == nil {
 			t.Errorf("Expected error but got success for unit test #%v", testCaseNum)
