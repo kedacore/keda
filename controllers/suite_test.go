6b6f55c77a239669b9bc154defce5275e053d24b
silenceper <silenceper@gmail.com>
2020-10-01 16:00:08 +0800

    enable golangci lint:goimports
    Signed-off-by: silenceper <silenceper@gmail.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -22,6 +22,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
 	//"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
