521b03866a549f77158a9377aad3542a775fecd0
Noah Kantrowitz <noah@coderanger.net>
2020-12-28 12:03:36 -0800

    Add statefulsets to the prefill list too.
    Signed-off-by: Noah Kantrowitz <noah@coderanger.net>

--- controllers/scaledobject_controller.go
+++ controllers/scaledobject_controller.go
@@ -60,7 +60,8 @@ var isScalableCache map[string]bool
 func init() {
 	// Prefill the cache with some known values for core resources in case of future parallelism to avoid stampeding herd on startup.
 	isScalableCache = map[string]bool{
-		"deploymen.apps": true,
+		"deployments.apps": true,
+		"statefusets.apps": true,
 	}
 }
 
