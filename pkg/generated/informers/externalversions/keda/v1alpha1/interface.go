6d282ae21db2c4e1e0c21edea9ce9f73c84356d7
Zbynek Roubalik <726523+zroubalik@users.noreply.github.com>
2021-01-07 14:10:31 +0100

    Generate clientset for ClusterTriggerAuthentication (#1486)
    * Generate clientset for ClusterTriggerAuthentication
    
    Signed-off-by: Zbynek Roubalik <zroubali@redhat.com>
    
    * update changelog
    
    Signed-off-by: Zbynek Roubalik <zroubali@redhat.com>

--- pkg/generated/informers/externalversions/keda/v1alpha1/interface.go
+++ pkg/generated/informers/externalversions/keda/v1alpha1/interface.go
@@ -24,6 +24,8 @@ import (
 
 // Interface provides access to all the informers in this group version.
 type Interface interface {
+	// ClusterTriggerAuthentications returns a ClusterTriggerAuthenticationInformer.
+	ClusterTriggerAuthentications() ClusterTriggerAuthenticationInformer
 	// ScaledJobs returns a ScaledJobInformer.
 	ScaledJobs() ScaledJobInformer
 	// ScaledObjects returns a ScaledObjectInformer.
@@ -41,6 +43,11 @@ type version struct {
 // New returns a new Interface.
 func New(f internalinterfaces.SharedInformerFactory, namespace string, tweakListOptions internalinterfaces.TweakListOptionsFunc) Interface {
 	return &version{factory: f, namespace: namespace, tweakListOptions: tweakListOptions}
+}
+
+// ClusterTriggerAuthentications returns a ClusterTriggerAuthenticationInformer.
+func (v *version) ClusterTriggerAuthentications() ClusterTriggerAuthenticationInformer {
+	return &clusterTriggerAuthenticationInformer{factory: v.factory, tweakListOptions: v.tweakListOptions}
 }
 
 // ScaledJobs returns a ScaledJobInformer.
