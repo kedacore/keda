3351a0b52fdc358d5e0e3de68f31e40124dc01ff
Shubham Kuchhal <shubham.kuchhal@india.nec.com>
2021-01-25 18:26:37 +0530

    Bump klog version to v2
    Signed-off-by: Shubham Kuchhal <shubham.kuchhal@india.nec.com>

--- adapter/main.go
+++ adapter/main.go
@@ -13,8 +13,8 @@ import (
 	openapinamer "k8s.io/apiserver/pkg/endpoints/openapi"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/klog"
-	"k8s.io/klog/klogr"
+	"k8s.io/klog/v2"
+	"k8s.io/klog/v2/klogr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 
