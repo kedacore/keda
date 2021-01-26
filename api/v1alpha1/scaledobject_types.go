bc725e67386aa54d9282ba0c6165a4321cbe9c0d
Ritika <52399571+Ritikaa96@users.noreply.github.com>
2021-01-26 16:21:56 +0530

    Show min/max replica count when using kubectl get scaledobject/scaledjob (#1534)
    Signed-off-by: Ritikaa96 <ritika@india.nec.com>

--- api/v1alpha1/scaledobject_types.go
+++ api/v1alpha1/scaledobject_types.go
@@ -11,6 +11,8 @@ import (
 // +kubebuilder:resource:path=scaledobjects,scope=Namespaced,shortName=so
 // +kubebuilder:printcolumn:name="ScaleTargetKind",type="string",JSONPath=".status.scaleTargetKind"
 // +kubebuilder:printcolumn:name="ScaleTargetName",type="string",JSONPath=".spec.scaleTargetRef.name"
+// +kubebuilder:printcolumn:name="Min",type="integer",JSONPath=".spec.minReplicaCount"
+// +kubebuilder:printcolumn:name="Max",type="integer",JSONPath=".spec.maxReplicaCount"
 // +kubebuilder:printcolumn:name="Triggers",type="string",JSONPath=".spec.triggers[*].type"
 // +kubebuilder:printcolumn:name="Authentication",type="string",JSONPath=".spec.triggers[*].authenticationRef.name"
 // +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status"
