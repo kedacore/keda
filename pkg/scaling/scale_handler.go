fd9f9a52123fb350b88e84a3a9602595143a080b
prashanth <rprashanth27@gmail.com>
2021-07-19 13:29:25 +0200

    Added new scaler for selenium grid
    Signed-off-by: prashanth <rprashanth27@gmail.com>

--- pkg/scaling/scale_handler.go
+++ pkg/scaling/scale_handler.go
@@ -449,6 +449,8 @@ func buildScaler(triggerType string, config *scalers.ScalerConfig) (scalers.Scal
 		return scalers.NewRedisStreamsScaler(true, config)
 	case "redis-streams":
 		return scalers.NewRedisStreamsScaler(false, config)
+	case "selenium-grid":
+		return scalers.NewSeleniumGridScaler(config)
 	case "solace-event-queue":
 		return scalers.NewSolaceScaler(config)
 	case "stan":
