bc9097274cc59586b9b6ebad55d2e8948d18bbdc
jorturfer <jorge_turrado@hotmail.es>
2021-08-03 20:24:25 +0200

    Completed the scaler config parsing
    Signed-off-by: jorturfer <jorge_turrado@hotmail.es>

--- pkg/scalers/kubernetes_workload_scaler.go
+++ pkg/scalers/kubernetes_workload_scaler.go
@@ -6,11 +6,13 @@ import (
 	"strconv"
 
 	kedautil "github.com/kedacore/keda/v2/pkg/util"
-	"go.etcd.io/etcd/client"
 	"k8s.io/api/autoscaling/v2beta2"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/labels"
+
 	"k8s.io/metrics/pkg/apis/external_metrics"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type kubernetesWorkloadScaler struct {
@@ -19,9 +21,10 @@ type kubernetesWorkloadScaler struct {
 }
 
 const (
-	podSelectorKey       = "PodSelector"
-	namespaceSelectorKey = "NamespaceSelector"
-	valueKey             = "Value"
+	kubernetesWorkloadMetricType = "External"
+	podSelectorKey               = "podSelector"
+	namespaceSelectorKey         = "namespaceSelector"
+	valueKey                     = "value"
 )
 
 type kubernetesWorkloadMetadata struct {
@@ -46,19 +49,35 @@ func parseWorkloadMetadata(config *ScalerConfig) (*kubernetesWorkloadMetadata, e
 	meta := &kubernetesWorkloadMetadata{}
 	var err error
 	meta.podSelector, err = labels.Parse(config.TriggerMetadata[podSelectorKey])
-	if err != nil {
+	if err != nil || meta.podSelector.String() == "" {
 		return nil, fmt.Errorf("invalid pod selector")
 	}
 	meta.value, err = strconv.ParseInt(config.TriggerMetadata[valueKey], 10, 64)
-	if err != nil {
-		return nil, fmt.Errorf("value must be an integer")
+	if err != nil || meta.value == 0 {
+		return nil, fmt.Errorf("value must be an integer greater than 0")
 	}
 	return meta, nil
 }
 
 // IsActive determines if we need to scale from zero
 func (s *kubernetesWorkloadScaler) IsActive(ctx context.Context) (bool, error) {
-	//TODO
+	podList := &corev1.PodList{}
+	listOptions := client.ListOptions{}
+	opts := []client.ListOption{}
+
+	listOptions.LabelSelector = s.metadata.podSelector
+	//Could be wrong, review it
+	listOptions.ApplyOptions(opts)
+
+	err := s.kubeClient.List(ctx, podList, opts...)
+	if err != nil {
+		return false, err
+	}
+
+	if len(podList.Items) == 0 {
+		return false, nil
+	}
+
 	return true, nil
 }
 
@@ -79,7 +98,7 @@ func (s *kubernetesWorkloadScaler) GetMetricSpecForScaling() []v2beta2.MetricSpe
 			Value: targetMetricValue,
 		},
 	}
-	metricSpec := v2beta2.MetricSpec{External: externalMetric, Type: v2beta2.ResourceMetricSourceType}
+	metricSpec := v2beta2.MetricSpec{External: externalMetric, Type: kubernetesWorkloadMetricType}
 	return []v2beta2.MetricSpec{metricSpec}
 }
 
