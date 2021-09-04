c5e4cd02f2906fcda3998fc579fc1c18144be199
Tsuyoshi Ushio <ushio@simplearchitect.com>
2021-09-04 12:10:05 -0700

    revert version and refactor scaling logic
    Signed-off-by: Tsuyoshi Ushio <ushio@simplearchitect.com>

--- pkg/scaling/scale_handler_test.go
+++ pkg/scaling/scale_handler_test.go
@@ -3,7 +3,6 @@ package scaling
 import (
 	"context"
 	"errors"
-	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -15,44 +14,12 @@ import (
 	"github.com/kedacore/keda/v2/pkg/scalers"
 	"github.com/kedacore/keda/v2/pkg/scaling/executor"
 	"k8s.io/client-go/tools/record"
-	"k8s.io/metrics/pkg/apis/external_metrics"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/api/autoscaling/v2beta2"
 	"k8s.io/apimachinery/pkg/api/resource"
 )
-
-func TestTargetAverageValue(t *testing.T) {
-	// count = 0
-	specs := []v2beta2.MetricSpec{}
-	targetAverageValue := getTargetAverageValue(specs)
-	assert.Equal(t, int64(0), targetAverageValue)
-	// 1 1
-	specs = []v2beta2.MetricSpec{
-		createMetricSpec(1),
-		createMetricSpec(1),
-	}
-	targetAverageValue = getTargetAverageValue(specs)
-	assert.Equal(t, int64(1), targetAverageValue)
-	// 5 5 3
-	specs = []v2beta2.MetricSpec{
-		createMetricSpec(5),
-		createMetricSpec(5),
-		createMetricSpec(3),
-	}
-	targetAverageValue = getTargetAverageValue(specs)
-	assert.Equal(t, int64(4), targetAverageValue)
-
-	// 5 5 4
-	specs = []v2beta2.MetricSpec{
-		createMetricSpec(5),
-		createMetricSpec(5),
-		createMetricSpec(3),
-	}
-	targetAverageValue = getTargetAverageValue(specs)
-	assert.Equal(t, int64(4), targetAverageValue)
-}
 
 func TestCheckScaledObjectScalersWithError(t *testing.T) {
 	ctrl := gomock.NewController(t)
@@ -121,160 +88,4 @@ func createMetricSpec(averageValue int) v2beta2.MetricSpec {
 			},
 		},
 	}
-}
-
-func TestIsScaledJobActive(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	client := mock_client.NewMockClient(ctrl)
-	recorder := record.NewFakeRecorder(1)
-
-	scaleHandler := &scaleHandler{
-		client:            client,
-		logger:            logf.Log.WithName("scalehandler"),
-		scaleLoopContexts: &sync.Map{},
-		scaleExecutor:     executor.NewScaleExecutor(client, nil, nil, recorder),
-		globalHTTPTimeout: 5 * time.Second,
-		recorder:          recorder,
-	}
-
-	// Keep the current behavior
-	// Assme 1 trigger only
-	scaledJobSingle := createScaledObject(100, "") // testing default = max
-	scalerSingle := []scalers.Scaler{
-		createScaler(ctrl, int64(20), int32(2), true),
-	}
-
-	isActive, queueLength, maxValue := scaleHandler.isScaledJobActive(context.TODO(), scalerSingle, scaledJobSingle)
-	assert.Equal(t, true, isActive)
-	assert.Equal(t, int64(20), queueLength)
-	assert.Equal(t, int64(10), maxValue)
-
-	// Non-Active trigger only
-	scalerSingle = []scalers.Scaler{
-		createScaler(ctrl, int64(0), int32(2), false),
-	}
-
-	isActive, queueLength, maxValue = scaleHandler.isScaledJobActive(context.TODO(), scalerSingle, scaledJobSingle)
-	assert.Equal(t, false, isActive)
-	assert.Equal(t, int64(0), queueLength)
-	assert.Equal(t, int64(0), maxValue)
-
-	// Test the valiation
-	scalerTestDatam := []scalerTestData{
-		newScalerTestData(100, "max", 20, 1, true, 10, 2, true, 5, 3, true, 7, 4, false, true, 20, 20),
-		newScalerTestData(100, "min", 20, 1, true, 10, 2, true, 5, 3, true, 7, 4, false, true, 5, 2),
-		newScalerTestData(100, "avg", 20, 1, true, 10, 2, true, 5, 3, true, 7, 4, false, true, 12, 9),
-		newScalerTestData(100, "sum", 20, 1, true, 10, 2, true, 5, 3, true, 7, 4, false, true, 35, 27),
-		newScalerTestData(25, "sum", 20, 1, true, 10, 2, true, 5, 3, true, 7, 4, false, true, 35, 25),
-	}
-
-	for index, scalerTestData := range scalerTestDatam {
-		scaledJob := createScaledObject(scalerTestData.MaxReplicaCount, scalerTestData.MultipleScalersOption)
-		scalers := []scalers.Scaler{
-			createScaler(ctrl, scalerTestData.Scaler1QueueLength, scalerTestData.Scaler1AverageValue, scalerTestData.Scaler1IsActive),
-			createScaler(ctrl, scalerTestData.Scaler2QueueLength, scalerTestData.Scaler2AverageValue, scalerTestData.Scaler2IsActive),
-			createScaler(ctrl, scalerTestData.Scaler3QueueLength, scalerTestData.Scaler3AverageValue, scalerTestData.Scaler3IsActive),
-			createScaler(ctrl, scalerTestData.Scaler4QueueLength, scalerTestData.Scaler4AverageValue, scalerTestData.Scaler4IsActive),
-		}
-		fmt.Printf("index: %d", index)
-		isActive, queueLength, maxValue = scaleHandler.isScaledJobActive(context.TODO(), scalers, scaledJob)
-		//	assert.Equal(t, 5, index)
-		assert.Equal(t, scalerTestData.ResultIsActive, isActive)
-		assert.Equal(t, scalerTestData.ResultQueueLength, queueLength)
-		assert.Equal(t, scalerTestData.ResultMaxValue, maxValue)
-	}
-}
-
-func newScalerTestData(
-	maxReplicaCount int,
-	multipleScalersOption string,
-	scaler1QueueLength, //nolint:golint,unparam
-	scaler1AverageValue int, //nolint:golint,unparam
-	scaler1IsActive bool, //nolint:golint,unparam
-	scaler2QueueLength, //nolint:golint,unparam
-	scaler2AverageValue int, //nolint:golint,unparam
-	scaler2IsActive bool, //nolint:golint,unparam
-	scaler3QueueLength, //nolint:golint,unparam
-	scaler3AverageValue int, //nolint:golint,unparam
-	scaler3IsActive bool, //nolint:golint,unparam
-	scaler4QueueLength, //nolint:golint,unparam
-	scaler4AverageValue int, //nolint:golint,unparam
-	scaler4IsActive bool, //nolint:golint,unparam
-	resultIsActive bool, //nolint:golint,unparam
-	resultQueueLength,
-	resultMaxLength int) scalerTestData {
-	return scalerTestData{
-		MaxReplicaCount:       int32(maxReplicaCount),
-		MultipleScalersOption: multipleScalersOption,
-		Scaler1QueueLength:    int64(scaler1QueueLength),
-		Scaler1AverageValue:   int32(scaler1AverageValue),
-		Scaler1IsActive:       scaler1IsActive,
-		Scaler2QueueLength:    int64(scaler2QueueLength),
-		Scaler2AverageValue:   int32(scaler2AverageValue),
-		Scaler2IsActive:       scaler2IsActive,
-		Scaler3QueueLength:    int64(scaler3QueueLength),
-		Scaler3AverageValue:   int32(scaler3AverageValue),
-		Scaler3IsActive:       scaler3IsActive,
-		Scaler4QueueLength:    int64(scaler4QueueLength),
-		Scaler4AverageValue:   int32(scaler4AverageValue),
-		Scaler4IsActive:       scaler4IsActive,
-		ResultIsActive:        resultIsActive,
-		ResultQueueLength:     int64(resultQueueLength),
-		ResultMaxValue:        int64(resultMaxLength),
-	}
-}
-
-type scalerTestData struct {
-	MaxReplicaCount       int32
-	MultipleScalersOption string
-	Scaler1QueueLength    int64
-	Scaler1AverageValue   int32
-	Scaler1IsActive       bool
-	Scaler2QueueLength    int64
-	Scaler2AverageValue   int32
-	Scaler2IsActive       bool
-	Scaler3QueueLength    int64
-	Scaler3AverageValue   int32
-	Scaler3IsActive       bool
-	Scaler4QueueLength    int64
-	Scaler4AverageValue   int32
-	Scaler4IsActive       bool
-	ResultIsActive        bool
-	ResultQueueLength     int64
-	ResultMaxValue        int64
-}
-
-func createScaledObject(maxReplicaCount int32, multipleScalersOption string) *kedav1alpha1.ScaledJob {
-	if multipleScalersOption != "" {
-		return &kedav1alpha1.ScaledJob{
-			Spec: kedav1alpha1.ScaledJobSpec{
-				MaxReplicaCount: &maxReplicaCount,
-				ScalingStrategy: kedav1alpha1.ScalingStrategy{
-					MultipleScalersOption: multipleScalersOption,
-				},
-			},
-		}
-	}
-	return &kedav1alpha1.ScaledJob{
-		Spec: kedav1alpha1.ScaledJobSpec{
-			MaxReplicaCount: &maxReplicaCount,
-		},
-	}
-}
-
-func createScaler(ctrl *gomock.Controller, queueLength int64, averageValue int32, isActive bool) *mock_scalers.MockScaler {
-	metricName := "queueLength"
-	scaler := mock_scalers.NewMockScaler(ctrl)
-	metricsSpecs := []v2beta2.MetricSpec{createMetricSpec(int(averageValue))}
-	metrics := []external_metrics.ExternalMetricValue{
-		{
-			MetricName: metricName,
-			Value:      *resource.NewQuantity(queueLength, resource.DecimalSI),
-		},
-	}
-	scaler.EXPECT().IsActive(gomock.Any()).Return(isActive, nil)
-	scaler.EXPECT().GetMetricSpecForScaling().Return(metricsSpecs)
-	scaler.EXPECT().GetMetrics(gomock.Any(), metricName, nil).Return(metrics, nil)
-	scaler.EXPECT().Close()
-	return scaler
 }
---
+++ pkg/scaling/scaledjob/scale_metrics.go
@@ -0,0 +1,181 @@
+package scaledjob
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	kedav1alpha1 "github.com/kedacore/keda/v2/api/v1alpha1"
+	"github.com/kedacore/keda/v2/pkg/eventreason"
+	"github.com/kedacore/keda/v2/pkg/scalers"
+	"k8s.io/api/autoscaling/v2beta2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+type scalerMetrics struct {
+	queueLength int64
+	maxValue    int64
+	isActive    bool
+}
+
+// GetScaleMetrics gets the metrics for decision making of scaling.
+func GetScaleMetrics(ctx context.Context, scalers []scalers.Scaler, scaledJob *kedav1alpha1.ScaledJob, recorder record.EventRecorder) (bool, int64, int64) {
+	var queueLength int64
+	var maxValue int64
+	isActive := false
+
+	logger := logf.Log.WithName("scalemetrics")
+	scalersMetrics := getScalersMetrics(ctx, scalers, scaledJob, logger, recorder)
+	switch scaledJob.Spec.ScalingStrategy.MultipleScalersOption {
+	case "min":
+		for _, metrics := range scalersMetrics {
+			if (queueLength == 0 || metrics.queueLength < queueLength) && metrics.isActive {
+				queueLength = metrics.queueLength
+				maxValue = metrics.maxValue
+				isActive = metrics.isActive
+			}
+		}
+	case "avg":
+		queueLengthSum := int64(0)
+		maxValueSum := int64(0)
+		length := 0
+		for _, metrics := range scalersMetrics {
+			if metrics.isActive {
+				queueLengthSum += metrics.queueLength
+				maxValueSum += metrics.maxValue
+				isActive = metrics.isActive
+				length++
+			}
+		}
+		if length != 0 {
+			queueLength = divideWithCeil(queueLengthSum, int64(length))
+			maxValue = divideWithCeil(maxValueSum, int64(length))
+		}
+	case "sum":
+		for _, metrics := range scalersMetrics {
+			if metrics.isActive {
+				queueLength += metrics.queueLength
+				maxValue += metrics.maxValue
+				isActive = metrics.isActive
+			}
+		}
+	default: // max
+		for _, metrics := range scalersMetrics {
+			if metrics.queueLength > queueLength && metrics.isActive {
+				queueLength = metrics.queueLength
+				maxValue = metrics.maxValue
+				isActive = metrics.isActive
+			}
+		}
+	}
+	maxValue = min(scaledJob.MaxReplicaCount(), maxValue)
+	logger.V(1).WithValues("ScaledJob", scaledJob.Name).Info("Checking if ScaleJob scalers are active", "isActive", isActive, "maxValue", maxValue, "MultipleScalersOption", scaledJob.Spec.ScalingStrategy.MultipleScalersOption)
+
+	return isActive, queueLength, maxValue
+}
+
+func getScalersMetrics(ctx context.Context, scalers []scalers.Scaler, scaledJob *kedav1alpha1.ScaledJob, logger logr.Logger, recorder record.EventRecorder) []scalerMetrics {
+	scalersMetrics := []scalerMetrics{}
+
+	// TODO refactor this, do chores, reduce the verbosity ie: V(1) and frequency of logs
+	// move relevant funcs getTargetAverageValue(), min() and divideWithCeil() out of scaler_handler.go
+	for _, scaler := range scalers {
+		var queueLength int64
+		var targetAverageValue int64
+		isActive := false
+		maxValue := int64(0)
+		scalerType := fmt.Sprintf("%T:", scaler)
+
+		scalerLogger := logger.WithValues("ScaledJob", scaledJob.Name, "Scaler", scalerType)
+
+		metricSpecs := scaler.GetMetricSpecForScaling()
+
+		// skip scaler that doesn't return any metric specs (usually External scaler with incorrect metadata)
+		// or skip cpu/memory resource scaler
+		if len(metricSpecs) < 1 || metricSpecs[0].External == nil {
+			continue
+		}
+
+		isTriggerActive, err := scaler.IsActive(ctx)
+
+		scalerLogger.Info("Active trigger", "isTriggerActive", isTriggerActive)
+
+		targetAverageValue = getTargetAverageValue(metricSpecs)
+
+		scalerLogger.Info("Scaler targetAverageValue", "targetAverageValue", targetAverageValue)
+
+		metrics, _ := scaler.GetMetrics(ctx, "queueLength", nil)
+
+		var metricValue int64
+
+		for _, m := range metrics {
+			if m.MetricName == "queueLength" {
+				metricValue, _ = m.Value.AsInt64()
+				queueLength += metricValue
+			}
+		}
+		scalerLogger.Info("QueueLength Metric value", "queueLength", queueLength)
+
+		scaler.Close()
+		if err != nil {
+			scalerLogger.V(1).Info("Error getting scale decision, but continue", "Error", err)
+			recorder.Event(scaledJob, corev1.EventTypeWarning, eventreason.KEDAScalerFailed, err.Error())
+			continue
+		} else if isTriggerActive {
+			isActive = true
+			scalerLogger.Info("Scaler is active")
+		}
+
+		if targetAverageValue != 0 {
+			maxValue = min(scaledJob.MaxReplicaCount(), divideWithCeil(queueLength, targetAverageValue))
+		}
+		scalersMetrics = append(scalersMetrics, scalerMetrics{
+			queueLength: queueLength,
+			maxValue:    maxValue,
+			isActive:    isActive,
+		})
+	}
+	return scalersMetrics
+}
+
+func getTargetAverageValue(metricSpecs []v2beta2.MetricSpec) int64 {
+	var targetAverageValue int64
+	var metricValue int64
+	var flag bool
+	for _, metric := range metricSpecs {
+		if metric.External.Target.AverageValue == nil {
+			metricValue = 0
+		} else {
+			metricValue, flag = metric.External.Target.AverageValue.AsInt64()
+			if !flag {
+				metricValue = 0
+			}
+		}
+
+		targetAverageValue += metricValue
+	}
+	count := int64(len(metricSpecs))
+	if count != 0 {
+		return targetAverageValue / count
+	}
+	return 0
+}
+
+func divideWithCeil(x, y int64) int64 {
+	ans := x / y
+	reminder := x % y
+	if reminder != 0 {
+		return ans + 1
+	}
+	return ans
+}
+
+// Min function for int64
+func min(x, y int64) int64 {
+	if x > y {
+		return y
+	}
+	return x
+}
