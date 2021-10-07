c3715f5e865e073a3c933b065ff1475b80353c04
Dimitris Tsioumas <dtsioumas@protonmail.com>
2021-10-07 16:47:13 +0200

    Add `unsafeSsl` parameter on InfluxDB (#2157)
    Signed-off-by: Dimitris Tsioumas <dtsioumas@protonmail.com>
    Signed-off-by: nilayasiktoprak <nilayasiktoprak@gmail.com>

--- pkg/scalers/influxdb_scaler.go
+++ pkg/scalers/influxdb_scaler.go
@@ -2,6 +2,7 @@ package scalers
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"net/url"
 	"strconv"
@@ -28,6 +29,7 @@ type influxDBMetadata struct {
 	organizationName string
 	query            string
 	serverURL        string
+	unsafeSsL        bool
 	thresholdValue   float64
 }
 
@@ -41,7 +43,13 @@ func NewInfluxDBScaler(config *ScalerConfig) (Scaler, error) {
 	}
 
 	influxDBLog.Info("starting up influxdb client")
-
+	// In case unsafeSsL is enabled.
+	if meta.unsafeSsL {
+		return &influxDBScaler{
+			client:   influxdb2.NewClientWithOptions(meta.serverURL, meta.authToken, influxdb2.DefaultOptions().SetTLSConfig(&tls.Config{InsecureSkipVerify: true})),
+			metadata: meta,
+		}, nil
+	}
 	return &influxDBScaler{
 		client:   influxdb2.NewClient(meta.serverURL, meta.authToken),
 		metadata: meta,
@@ -55,6 +63,7 @@ func parseInfluxDBMetadata(config *ScalerConfig) (*influxDBMetadata, error) {
 	var organizationName string
 	var query string
 	var serverURL string
+	var unsafeSsL bool
 	var thresholdValue float64
 
 	val, ok := config.TriggerMetadata["authToken"]
@@ -127,6 +136,14 @@ func parseInfluxDBMetadata(config *ScalerConfig) (*influxDBMetadata, error) {
 	} else {
 		return nil, fmt.Errorf("no threshold value given")
 	}
+	unsafeSsL = false
+	if val, ok := config.TriggerMetadata["unsafeSsL"]; ok {
+		parsedVal, err := strconv.ParseBool(val)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing unsafeSsL: %s", err)
+		}
+		unsafeSsL = parsedVal
+	}
 
 	return &influxDBMetadata{
 		authToken:        authToken,
@@ -135,6 +152,7 @@ func parseInfluxDBMetadata(config *ScalerConfig) (*influxDBMetadata, error) {
 		query:            query,
 		serverURL:        serverURL,
 		thresholdValue:   thresholdValue,
+		unsafeSsL:        unsafeSsL,
 	}, nil
 }
 
