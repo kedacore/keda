ca8a90bd7a618dbf4cf5e7703a4d03e8a14d832c
Brandon Pinske <brandon.pinske@crowdstrike.com>
2021-11-29 11:56:52 -0800

    Graphite scaler should use latest datapoint, not earliest, returned.
    Signed-off-by: Brandon Pinske <brandon.pinske@crowdstrike.com>

--- pkg/scalers/graphite_scaler.go
+++ pkg/scalers/graphite_scaler.go
@@ -194,7 +194,8 @@ func (s *graphiteScaler) ExecuteGrapQuery(ctx context.Context) (float64, error) 
 	}
 
 	// https://graphite-api.readthedocs.io/en/latest/api.html#json
-	datapoint := result[0].Datapoints[0][0]
+	latestDatapoint := len(result[0].Datapoints) - 1
+	datapoint := result[0].Datapoints[latestDatapoint][0]
 
 	return datapoint, nil
 }
