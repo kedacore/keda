8a5ef1ba60416dfa4405d9736b3585814e4c89b6
Jinli Liang <paul.liang@rokt.com>
2021-12-08 19:41:59 +1100

    wip

--- pkg/scalers/kafka_scaler.go
+++ pkg/scalers/kafka_scaler.go
@@ -209,23 +209,24 @@ func parseKafkaMetadata(config *ScalerConfig) (kafkaMetadata, error) {
 
 // IsActive determines if we need to scale from zero
 func (s *kafkaScaler) IsActive(ctx context.Context) (bool, error) {
-	partitions, err := s.getPartitions()
+	topicPartitions, err := s.getTopicPartitions()
 	if err != nil {
 		return false, err
 	}
 
-	offsets, err := s.getOffsets(partitions)
+	offsets, err := s.getOffsets(topicPartitions)
 	if err != nil {
 		return false, err
 	}
 
-	topicOffsets, err := s.getTopicOffsets(partitions)
+	topicPartitionsOffsets, err := s.getTopicPartitionOffsets(topicPartitions)
 	if err != nil {
 		return false, err
 	}
 
-	for _, partition := range partitions {
-		lag, err := s.getLagForPartition(partition, offsets, topicOffsets)
+	for topic, partitions := range topicPartitions {
+		for _, partition := range partitions {
+		lag, err := s.getLagForPartition(partition, offsets, topicPartitionsOffsets)
 		if err != nil && lag == invalidOffset {
 			return true, nil
 		}
@@ -234,6 +235,7 @@ func (s *kafkaScaler) IsActive(ctx context.Context) (bool, error) {
 		// Return as soon as a lag was detected for any partition
 		if lag > 0 {
 			return true, nil
+		}
 		}
 	}
 
@@ -289,29 +291,41 @@ func getKafkaClients(metadata kafkaMetadata) (sarama.Client, sarama.ClusterAdmin
 	return client, admin, nil
 }
 
-func (s *kafkaScaler) getPartitions() ([]int32, error) {
-	topicsMetadata, err := s.admin.DescribeTopics([]string{s.metadata.topic})
+func (s *kafkaScaler) getTopicPartitions() (map[string][]int32, error) {
+	var topicsToDescribe = make([]string, 0)
+
+	// when no topic is specified, query to cg group to fetch all subscribed topics
+	if s.metadata.topic == "" {
+		listCGOffsetResponse, err := s.admin.ListConsumerGroupOffsets(s.metadata.group, nil)
+		if err != nil {
+			return nil, fmt.Errorf("error listing cg offset: %s", err)
+		}
+		for topicName, _ := range listCGOffsetResponse.Blocks {
+			topicsToDescribe = append(topicsToDescribe, topicName)
+		}
+	} else {
+		topicsToDescribe = []string{s.metadata.topic}
+	}
+
+	topicsMetadata, err := s.admin.DescribeTopics(topicsToDescribe)
 	if err != nil {
 		return nil, fmt.Errorf("error describing topics: %s", err)
 	}
-	if len(topicsMetadata) != 1 {
-		return nil, fmt.Errorf("expected only 1 topic metadata, got %d", len(topicsMetadata))
-	}
 
-	partitionMetadata := topicsMetadata[0].Partitions
-	partitions := make([]int32, len(partitionMetadata))
-	for i, p := range partitionMetadata {
-		partitions[i] = p.ID
+	topicPartitions := make(map[string][]int32, len(topicsMetadata))
+	for _, topicMetadata := range topicsMetadata {
+		partitionMetadata := topicMetadata.Partitions
+		partitions := make([]int32, len(partitionMetadata))
+		for i, p := range partitionMetadata {
+			partitions[i] = p.ID
+		}
+		topicPartitions[topicMetadata.Name] = partitions
 	}
-
-	return partitions, nil
+	return topicPartitions, nil
 }
 
-func (s *kafkaScaler) getOffsets(partitions []int32) (*sarama.OffsetFetchResponse, error) {
-	offsets, err := s.admin.ListConsumerGroupOffsets(s.metadata.group, map[string][]int32{
-		s.metadata.topic: partitions,
-	})
-
+func (s *kafkaScaler) getOffsets(topicPartitions map[string][]int32) (*sarama.OffsetFetchResponse, error) {
+	offsets, err := s.admin.ListConsumerGroupOffsets(s.metadata.group, topicPartitions)
 	if err != nil {
 		return nil, fmt.Errorf("error listing consumer group offsets: %s", err)
 	}
@@ -366,34 +380,34 @@ func (s *kafkaScaler) GetMetricSpecForScaling(context.Context) []v2beta2.MetricS
 
 // GetMetrics returns value for a supported metric and an error if there is a problem getting the metric
 func (s *kafkaScaler) GetMetrics(ctx context.Context, metricName string, metricSelector labels.Selector) ([]external_metrics.ExternalMetricValue, error) {
-	partitions, err := s.getPartitions()
+	topicPartitions, err := s.getTopicPartitions()
 	if err != nil {
 		return []external_metrics.ExternalMetricValue{}, err
 	}
 
-	offsets, err := s.getOffsets(partitions)
+	offsets, err := s.getOffsets(topicPartitions)
 	if err != nil {
 		return []external_metrics.ExternalMetricValue{}, err
 	}
 
-	topicOffsets, err := s.getTopicOffsets(partitions)
+	topicOffsets, err := s.getTopicPartitionOffsets(topicPartitions)
 	if err != nil {
 		return []external_metrics.ExternalMetricValue{}, err
 	}
 
 	totalLag := int64(0)
-	for _, partition := range partitions {
+	for _, partition := range topicPartitions {
 		lag, _ := s.getLagForPartition(partition, offsets, topicOffsets)
 
 		totalLag += lag
 	}
 
-	kafkaLog.V(1).Info(fmt.Sprintf("Kafka scaler: Providing metrics based on totalLag %v, partitions %v, threshold %v", totalLag, len(partitions), s.metadata.lagThreshold))
+	kafkaLog.V(1).Info(fmt.Sprintf("Kafka scaler: Providing metrics based on totalLag %v, topicPartitions %v, threshold %v", totalLag, len(topicPartitions), s.metadata.lagThreshold))
 
 	if !s.metadata.allowIdleConsumers {
-		// don't scale out beyond the number of partitions
-		if (totalLag / s.metadata.lagThreshold) > int64(len(partitions)) {
-			totalLag = int64(len(partitions)) * s.metadata.lagThreshold
+		// don't scale out beyond the number of topicPartitions
+		if (totalLag / s.metadata.lagThreshold) > int64(len(topicPartitions)) {
+			totalLag = int64(len(topicPartitions)) * s.metadata.lagThreshold
 		}
 	}
 
@@ -406,7 +420,7 @@ func (s *kafkaScaler) GetMetrics(ctx context.Context, metricName string, metricS
 	return append([]external_metrics.ExternalMetricValue{}, metric), nil
 }
 
-func (s *kafkaScaler) getTopicOffsets(partitions []int32) (map[int32]int64, error) {
+func (s *kafkaScaler) getTopicPartitionOffsets(topicPartitions map[string][]int32) (map[string]map[int32]int64, error) {
 	version := int16(0)
 	if s.client.Config().Version.IsAtLeast(sarama.V0_10_1_0) {
 		version = 1
@@ -415,41 +429,42 @@ func (s *kafkaScaler) getTopicOffsets(partitions []int32) (map[int32]int64, erro
 	// Step 1: build one OffsetRequest instance per broker.
 	requests := make(map[*sarama.Broker]*sarama.OffsetRequest)
 
-	for _, partitionID := range partitions {
-		broker, err := s.client.Leader(s.metadata.topic, partitionID)
-		if err != nil {
-			return nil, err
+	for topic, partitions := range topicPartitions {
+		for _, partitionID := range partitions {
+			broker, err := s.client.Leader(topic, partitionID)
+			if err != nil {
+				return nil, err
+			}
+			request, ok := requests[broker]
+			if !ok {
+				request = &sarama.OffsetRequest{Version: version}
+				requests[broker] = request
+			}
+			request.AddBlock(topic, partitionID, sarama.OffsetNewest, 1)
 		}
-
-		request, ok := requests[broker]
-		if !ok {
-			request = &sarama.OffsetRequest{Version: version}
-			requests[broker] = request
-		}
-
-		request.AddBlock(s.metadata.topic, partitionID, sarama.OffsetNewest, 1)
 	}
 
-	offsets := make(map[int32]int64)
+	topicPartitionsOffsets := make(map[string]map[int32]int64)
 
-	// Step 2: send requests, one per broker, and collect offsets
+	// Step 2: send requests, one per broker, and collect topicPartitionsOffsets
 	for broker, request := range requests {
 		response, err := broker.GetAvailableOffsets(request)
-
 		if err != nil {
 			return nil, err
 		}
 
-		for _, blocks := range response.Blocks {
+		for topic, blocks := range response.Blocks {
+			if _, found := topicPartitionsOffsets[topic]; !found {
+				topicPartitionsOffsets[topic] = make(map[int32]int64)
+			}
 			for partitionID, block := range blocks {
 				if block.Err != sarama.ErrNoError {
 					return nil, block.Err
 				}
-
-				offsets[partitionID] = block.Offset
+				topicPartitionsOffsets[topic][partitionID] = block.Offset
 			}
 		}
 	}
 
-	return offsets, nil
+	return topicPartitionsOffsets, nil
 }
