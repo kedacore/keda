59f8abba89860e5e0399dddab6a481fa3dcfba59
Daniel <daniel-yavorovich@users.noreply.github.com>
2022-01-26 16:19:39 +0200

    Add PredictKube scaler (#2418)
    Signed-off-by: Daniel Yavorovych <daniel@dysnix.com>
    Signed-off-by: alex60217101990 <alex6021710@gmail.com>

--- go.mod
+++ go.mod
@@ -18,9 +18,12 @@ require (
 	github.com/Shopify/sarama v1.31.0
 	github.com/aws/aws-sdk-go v1.42.40
 	github.com/denisenkom/go-mssqldb v0.12.0
+	github.com/dysnix/predictkube-libs v0.0.0-20220125103715-5502104557b3
+	github.com/dysnix/predictkube-proto v0.0.0-20211223141524-d309509b6b5f
 	github.com/elastic/go-elasticsearch/v7 v7.16.0
 	github.com/go-logr/logr v1.2.2
 	github.com/go-playground/assert/v2 v2.0.1
+	github.com/go-playground/validator/v10 v10.9.0
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gocql/gocql v0.0.0-20211222173705-d73e6b1002a7
@@ -35,14 +38,17 @@ require (
 	github.com/newrelic/newrelic-client-go v0.70.0
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.18.0
+	github.com/phayes/freeport v0.0.0-20180830031419-95f893ade6f2
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.12.0
+	github.com/prometheus/common v0.32.1
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/streadway/amqp v1.0.0
 	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/gjson v1.13.0
 	github.com/xdg/scram v1.0.5
+	github.com/xhit/go-str2duration/v2 v2.0.0
 	go.mongodb.org/mongo-driver v1.8.2
 	google.golang.org/api v0.65.0
 	google.golang.org/genproto v0.0.0-20220118154757-00ab72f36ad5
@@ -87,6 +93,7 @@ require (
 	github.com/NYTimes/gziphandler v1.1.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/andybalholm/brotli v1.0.3 // indirect
 	github.com/armon/go-metrics v0.3.9 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -107,13 +114,15 @@ require (
 	github.com/emicklei/go-restful-swagger12 v0.0.0-20201014110547-68ccff494617 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
-	github.com/fatih/color v1.7.0 // indirect
+	github.com/fatih/color v1.9.0 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-logr/zapr v1.2.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
@@ -126,6 +135,7 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
@@ -159,10 +169,12 @@ require (
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-ieproxy v0.0.1 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
@@ -172,12 +184,13 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/oklog/run v1.0.0 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
@@ -186,6 +199,10 @@ require (
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
+	github.com/ulikunitz/unixtime v0.1.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.31.0 // indirect
+	github.com/wagslane/go-password-validator v0.3.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
---
+++ pkg/scalers/predictkube_scaler_test.go
@@ -0,0 +1,222 @@
+package scalers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/phayes/freeport"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	libsSrv "github.com/dysnix/predictkube-libs/external/grpc/server"
+	pb "github.com/dysnix/predictkube-proto/external/proto/services"
+)
+
+type server struct {
+	grpcSrv  *grpc.Server
+	listener net.Listener
+	port     int
+	val      int64
+}
+
+func (s *server) GetPredictMetric(_ context.Context, _ *pb.ReqGetPredictMetric) (res *pb.ResGetPredictMetric, err error) {
+	s.val = int64(rand.Intn(30000-10000) + 10000)
+	return &pb.ResGetPredictMetric{
+		ResultMetric: s.val,
+	}, nil
+}
+
+func (s *server) start() <-chan error {
+	errCh := make(chan error, 1)
+
+	go func() {
+		defer close(errCh)
+
+		var (
+			err error
+		)
+
+		s.port, err = freeport.GetFreePort()
+		if err != nil {
+			log.Fatalf("Could not get free port for init mock grpc server: %s", err)
+		}
+
+		serverURL := fmt.Sprintf("0.0.0.0:%d", s.port)
+		if s.listener == nil {
+			var err error
+			s.listener, err = net.Listen("tcp4", serverURL)
+
+			if err != nil {
+				log.Println("starting grpc server with error")
+
+				errCh <- err
+				return
+			}
+		}
+
+		log.Printf("🚀 starting mock grpc server. On host 0.0.0.0, with port: %d", s.port)
+
+		if err := s.grpcSrv.Serve(s.listener); err != nil {
+			log.Println(err, "serving grpc server with error")
+
+			errCh <- err
+			return
+		}
+	}()
+
+	return errCh
+}
+
+func (s *server) stop() error {
+	s.grpcSrv.GracefulStop()
+	return libsSrv.CheckNetErrClosing(s.listener.Close())
+}
+
+func runMockGrpcPredictServer() (*server, *grpc.Server) {
+	grpcServer := grpc.NewServer()
+
+	mockGrpcServer := &server{grpcSrv: grpcServer}
+
+	defer func() {
+		if r := recover(); r != nil {
+			_ = mockGrpcServer.stop()
+			panic(r)
+		}
+	}()
+
+	go func() {
+		for errCh := range mockGrpcServer.start() {
+			if errCh != nil {
+				log.Printf("GRPC server listen error: %3v", errCh)
+			}
+		}
+	}()
+
+	pb.RegisterMlEngineServiceServer(grpcServer, mockGrpcServer)
+
+	return mockGrpcServer, grpcServer
+}
+
+const testAPIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0ZXN0IENyZWF0ZUNsaWVudCIsImV4cCI6MTY0NjkxNzI3Nywic3ViIjoiODM4NjY5ODAtM2UzNS0xMWVjLTlmMjQtYWNkZTQ4MDAxMTIyIn0.5QEuO6_ysdk2abGvk3Xp7Q25M4H4pIFXeqP2E7n9rKI"
+
+type predictKubeMetadataTestData struct {
+	metadata   map[string]string
+	authParams map[string]string
+	isError    bool
+}
+
+var testPredictKubeMetadata = []predictKubeMetadataTestData{
+	// all properly formed
+	{
+		map[string]string{"predictHorizon": "2h", "historyTimeWindow": "7d", "prometheusAddress": "http://demo.robustperception.io:9090", "queryStep": "2m", "threshold": "2000", "query": "up"},
+		map[string]string{"apiKey": testAPIKey}, false,
+	},
+	// missing prometheusAddress
+	{
+		map[string]string{"predictHorizon": "2h", "historyTimeWindow": "7d", "prometheusAddress": "", "queryStep": "2m", "threshold": "2000", "query": "up"},
+		map[string]string{"apiKey": testAPIKey}, true,
+	},
+	// malformed threshold
+	{
+		map[string]string{"predictHorizon": "2h", "historyTimeWindow": "7d", "prometheusAddress": "http://localhost:9090", "queryStep": "2m", "threshold": "one", "query": "up"},
+
+		map[string]string{"apiKey": testAPIKey}, true,
+	},
+	// missing query
+	{
+		map[string]string{"predictHorizon": "2h", "historyTimeWindow": "7d", "prometheusAddress": "http://localhost:9090", "queryStep": "2m", "threshold": "one", "query": ""},
+		map[string]string{"apiKey": testAPIKey}, true,
+	},
+}
+
+func TestPredictKubeParseMetadata(t *testing.T) {
+	for _, testData := range testPredictKubeMetadata {
+		_, err := parsePredictKubeMetadata(&ScalerConfig{TriggerMetadata: testData.metadata, AuthParams: testData.authParams})
+		if err != nil && !testData.isError {
+			t.Error("Expected success but got error", err)
+		}
+		if testData.isError && err == nil {
+			t.Error("Expected error but got success")
+		}
+	}
+}
+
+type predictKubeMetricIdentifier struct {
+	metadataTestData *predictKubeMetadataTestData
+	scalerIndex      int
+	name             string
+}
+
+var predictKubeMetricIdentifiers = []predictKubeMetricIdentifier{
+	{&testPredictKubeMetadata[0], 0, fmt.Sprintf("s0-predictkube-%s", predictKubeMetricPrefix)},
+	{&testPredictKubeMetadata[0], 1, fmt.Sprintf("s1-predictkube-%s", predictKubeMetricPrefix)},
+}
+
+func TestPredictKubeGetMetricSpecForScaling(t *testing.T) {
+	mockPredictServer, grpcServer := runMockGrpcPredictServer()
+	defer func() {
+		_ = mockPredictServer.stop()
+		grpcServer.GracefulStop()
+	}()
+
+	mlEngineHost = "0.0.0.0"
+	mlEnginePort = mockPredictServer.port
+
+	for _, testData := range predictKubeMetricIdentifiers {
+		mockPredictKubeScaler, err := NewPredictKubeScaler(
+			context.Background(), &ScalerConfig{
+				TriggerMetadata: testData.metadataTestData.metadata,
+				AuthParams:      testData.metadataTestData.authParams,
+				ScalerIndex:     testData.scalerIndex,
+			},
+		)
+		assert.NoError(t, err)
+
+		metricSpec := mockPredictKubeScaler.GetMetricSpecForScaling(context.Background())
+		metricName := metricSpec[0].External.Metric.Name
+		if metricName != testData.name {
+			t.Error("Wrong External metric source name:", metricName)
+			return
+		}
+
+		t.Log(metricSpec)
+	}
+}
+
+func TestPredictKubeGetMetrics(t *testing.T) {
+	grpcConf.Conn.Insecure = true
+
+	mockPredictServer, grpcServer := runMockGrpcPredictServer()
+	<-time.After(time.Second * 3)
+	defer func() {
+		_ = mockPredictServer.stop()
+		grpcServer.GracefulStop()
+	}()
+
+	mlEngineHost = "0.0.0.0"
+	mlEnginePort = mockPredictServer.port
+
+	for _, testData := range predictKubeMetricIdentifiers {
+		mockPredictKubeScaler, err := NewPredictKubeScaler(
+			context.Background(), &ScalerConfig{
+				TriggerMetadata: testData.metadataTestData.metadata,
+				AuthParams:      testData.metadataTestData.authParams,
+				ScalerIndex:     testData.scalerIndex,
+			},
+		)
+		assert.NoError(t, err)
+
+		result, err := mockPredictKubeScaler.GetMetrics(context.Background(), predictKubeMetricPrefix, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, len(result), 1)
+		assert.Equal(t, result[0].Value, *resource.NewQuantity(mockPredictServer.val, resource.DecimalSI))
+
+		t.Logf("get: %v, want: %v, predictMetric: %d", result[0].Value, *resource.NewQuantity(mockPredictServer.val, resource.DecimalSI), mockPredictServer.val)
+	}
+}
