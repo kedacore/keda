0ef96ad94605877e88dd9a6fc10bec0f29bc52b7
Vighnesh Shenoy <vshenoy@microsoft.com>
2022-03-23 01:47:05 +0530

    Support for non-public clouds for EventHub scaler. (#2788)
    Signed-off-by: Vighnesh Shenoy <vshenoy@microsoft.com>

--- pkg/scalers/azure/azure_cloud_environment_test.go
+++ pkg/scalers/azure/azure_cloud_environment_test.go
@@ -7,33 +7,35 @@ import (
 	az "github.com/Azure/go-autorest/autorest/azure"
 )
 
-type parseEndpointSuffixTestData struct {
-	metadata       map[string]string
-	endpointSuffix string
-	suffixProvider EnvironmentSuffixProvider
-	isError        bool
+type parseEnvironmentPropertyTestData struct {
+	metadata            map[string]string
+	endpointSuffix      string
+	endpointKey         string
+	envPropertyProvider EnvironmentPropertyProvider
+	isError             bool
 }
 
-var testSuffixProvider EnvironmentSuffixProvider = func(env az.Environment) (string, error) {
+var testPropertyProvider EnvironmentPropertyProvider = func(env az.Environment) (string, error) {
 	if env == az.USGovernmentCloud {
 		return "", fmt.Errorf("test endpoint is not available in %s", env.Name)
 	}
 	return fmt.Sprintf("%s.suffix", env.Name), nil
 }
 
-var parseEndpointSuffixTestDataset = []parseEndpointSuffixTestData{
-	{map[string]string{}, "AzurePublicCloud.suffix", testSuffixProvider, false},
-	{map[string]string{"cloud": "Invalid"}, "", testSuffixProvider, true},
-	{map[string]string{"cloud": "AzureUSGovernmentCloud"}, "", testSuffixProvider, true},
-	{map[string]string{"cloud": "AzureGermanCloud"}, "AzureGermanCloud.suffix", testSuffixProvider, false},
-	{map[string]string{"cloud": "Private"}, "", testSuffixProvider, true},
-	{map[string]string{"cloud": "Private", "endpointSuffix": "suffix.private.cloud"}, "suffix.private.cloud", testSuffixProvider, false},
-	{map[string]string{"endpointSuffix": "ignored"}, "AzurePublicCloud.suffix", testSuffixProvider, false},
+var parseEnvironmentPropertyTestDataset = []parseEnvironmentPropertyTestData{
+	{map[string]string{}, "AzurePublicCloud.suffix", DefaultEndpointSuffixKey, testPropertyProvider, false},
+	{map[string]string{"cloud": "Invalid"}, "", DefaultEndpointSuffixKey, testPropertyProvider, true},
+	{map[string]string{"cloud": "AzureUSGovernmentCloud"}, "", DefaultEndpointSuffixKey, testPropertyProvider, true},
+	{map[string]string{"cloud": "AzureGermanCloud"}, "AzureGermanCloud.suffix", DefaultEndpointSuffixKey, testPropertyProvider, false},
+	{map[string]string{"cloud": "Private"}, "", DefaultEndpointSuffixKey, testPropertyProvider, true},
+	{map[string]string{"cloud": "Private", "endpointSuffix": "suffix.private.cloud"}, "suffix.private.cloud", DefaultEndpointSuffixKey, testPropertyProvider, false},
+	{map[string]string{"endpointSuffix": "ignored"}, "AzurePublicCloud.suffix", DefaultEndpointSuffixKey, testPropertyProvider, false},
+	{map[string]string{"cloud": "Private", "endpointSuffixDiff": "suffix.private.cloud"}, "suffix.private.cloud", "endpointSuffixDiff", testPropertyProvider, false},
 }
 
-func TestParseEndpointSuffix(t *testing.T) {
-	for _, testData := range parseEndpointSuffixTestDataset {
-		endpointSuffix, err := ParseEndpointSuffix(testData.metadata, testData.suffixProvider)
+func TestParseEnvironmentProperty(t *testing.T) {
+	for _, testData := range parseEnvironmentPropertyTestDataset {
+		endpointSuffix, err := ParseEnvironmentProperty(testData.metadata, testData.endpointKey, testData.envPropertyProvider)
 		if !testData.isError && err != nil {
 			t.Error("Expected success but got error", err)
 		}
