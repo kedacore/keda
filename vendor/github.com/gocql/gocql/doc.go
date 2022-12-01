12d8901b75ef260f7b7c9da4c59d11dd8ae685e8
Jorge Turrado <jorge_turrado@hotmail.es>
2022-12-01 15:48:55 +0100

    bump deps
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- vendor/github.com/gocql/gocql/doc.go
+++ vendor/github.com/gocql/gocql/doc.go
@@ -5,15 +5,15 @@
 // Package gocql implements a fast and robust Cassandra driver for the
 // Go programming language.
 //
-// Connecting to the cluster
+// # Connecting to the cluster
 //
 // Pass a list of initial node IP addresses to NewCluster to create a new cluster configuration:
 //
-//  cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
+//	cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
 //
 // Port can be specified as part of the address, the above is equivalent to:
 //
-//  cluster := gocql.NewCluster("192.168.1.1:9042", "192.168.1.2:9042", "192.168.1.3:9042")
+//	cluster := gocql.NewCluster("192.168.1.1:9042", "192.168.1.2:9042", "192.168.1.3:9042")
 //
 // It is recommended to use the value set in the Cassandra config for broadcast_address or listen_address,
 // an IP address not a domain name. This is because events from Cassandra will use the configured IP
@@ -22,9 +22,9 @@
 //
 // Then you can customize more options (see ClusterConfig):
 //
-//  cluster.Keyspace = "example"
-//  cluster.Consistency = gocql.Quorum
-//  cluster.ProtoVersion = 4
+//	cluster.Keyspace = "example"
+//	cluster.Consistency = gocql.Quorum
+//	cluster.ProtoVersion = 4
 //
 // The driver tries to automatically detect the protocol version to use if not set, but you might want to set the
 // protocol version explicitly, as it's not defined which version will be used in certain situations (for example
@@ -32,13 +32,13 @@
 //
 // When ready, create a session from the configuration. Don't forget to Close the session once you are done with it:
 //
-//  session, err := cluster.CreateSession()
-//  if err != nil {
-//  	return err
-//  }
-//  defer session.Close()
+//	session, err := cluster.CreateSession()
+//	if err != nil {
+//		return err
+//	}
+//	defer session.Close()
 //
-// Authentication
+// # Authentication
 //
 // CQL protocol uses a SASL-based authentication mechanism and so consists of an exchange of server challenges and
 // client response pairs. The details of the exchanged messages depend on the authenticator used.
@@ -47,18 +47,18 @@
 //
 // PasswordAuthenticator is provided to use for username/password authentication:
 //
-//  cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
-//  cluster.Authenticator = gocql.PasswordAuthenticator{
-//		Username: "user",
-//		Password: "password"
-//  }
-//  session, err := cluster.CreateSession()
-//  if err != nil {
-//  	return err
-//  }
-//  defer session.Close()
+//	 cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
+//	 cluster.Authenticator = gocql.PasswordAuthenticator{
+//			Username: "user",
+//			Password: "password"
+//	 }
+//	 session, err := cluster.CreateSession()
+//	 if err != nil {
+//	 	return err
+//	 }
+//	 defer session.Close()
 //
-// Transport layer security
+// # Transport layer security
 //
 // It is possible to secure traffic between the client and server with TLS.
 //
@@ -69,38 +69,38 @@
 // to true if no Config is set. Most users should set SslOptions.Config to a *tls.Config.
 // SslOptions and Config.InsecureSkipVerify interact as follows:
 //
-//  Config.InsecureSkipVerify | EnableHostVerification | Result
-//  Config is nil             | false                  | do not verify host
-//  Config is nil             | true                   | verify host
-//  false                     | false                  | verify host
-//  true                      | false                  | do not verify host
-//  false                     | true                   | verify host
-//  true                      | true                   | verify host
+//	Config.InsecureSkipVerify | EnableHostVerification | Result
+//	Config is nil             | false                  | do not verify host
+//	Config is nil             | true                   | verify host
+//	false                     | false                  | verify host
+//	true                      | false                  | do not verify host
+//	false                     | true                   | verify host
+//	true                      | true                   | verify host
 //
 // For example:
 //
-//  cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
-//  cluster.SslOpts = &gocql.SslOptions{
-//  	EnableHostVerification: true,
-//  }
-//  session, err := cluster.CreateSession()
-//  if err != nil {
-//  	return err
-//  }
-//  defer session.Close()
+//	cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
+//	cluster.SslOpts = &gocql.SslOptions{
+//		EnableHostVerification: true,
+//	}
+//	session, err := cluster.CreateSession()
+//	if err != nil {
+//		return err
+//	}
+//	defer session.Close()
 //
-// Data-center awareness and query routing
+// # Data-center awareness and query routing
 //
 // To route queries to local DC first, use DCAwareRoundRobinPolicy. For example, if the datacenter you
 // want to primarily connect is called dc1 (as configured in the database):
 //
-//  cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
-//  cluster.PoolConfig.HostSelectionPolicy = gocql.DCAwareRoundRobinPolicy("dc1")
+//	cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
+//	cluster.PoolConfig.HostSelectionPolicy = gocql.DCAwareRoundRobinPolicy("dc1")
 //
 // The driver can route queries to nodes that hold data replicas based on partition key (preferring local DC).
 //
-//  cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
-//  cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.DCAwareRoundRobinPolicy("dc1"))
+//	cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
+//	cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.DCAwareRoundRobinPolicy("dc1"))
 //
 // Note that TokenAwareHostPolicy can take options such as gocql.ShuffleReplicas and gocql.NonLocalReplicasFallback.
 //
@@ -109,50 +109,59 @@
 // The driver can only use token-aware routing for queries where all partition key columns are query parameters.
 // For example, instead of
 //
-//  session.Query("select value from mytable where pk1 = 'abc' AND pk2 = ?", "def")
+//	session.Query("select value from mytable where pk1 = 'abc' AND pk2 = ?", "def")
 //
 // use
 //
-//  session.Query("select value from mytable where pk1 = ? AND pk2 = ?", "abc", "def")
+//	session.Query("select value from mytable where pk1 = ? AND pk2 = ?", "abc", "def")
 //
-// Executing queries
+// # Rack-level awareness
+//
+// The DCAwareRoundRobinPolicy can be replaced with RackAwareRoundRobinPolicy, which takes two parameters, datacenter and rack.
+//
+// Instead of dividing hosts with two tiers (local datacenter and remote datacenters) it divides hosts into three
+// (the local rack, the rest of the local datacenter, and everything else).
+//
+// RackAwareRoundRobinPolicy can be combined with TokenAwareHostPolicy in the same way as DCAwareRoundRobinPolicy.
+//
+// # Executing queries
 //
 // Create queries with Session.Query. Query values must not be reused between different executions and must not be
 // modified after starting execution of the query.
 //
 // To execute a query without reading results, use Query.Exec:
 //
-//  err := session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
-//		"me", gocql.TimeUUID(), "hello world").WithContext(ctx).Exec()
+//	 err := session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
+//			"me", gocql.TimeUUID(), "hello world").WithContext(ctx).Exec()
 //
 // Single row can be read by calling Query.Scan:
 //
-//  err := session.Query(`SELECT id, text FROM tweet WHERE timeline = ? LIMIT 1`,
-//		"me").WithContext(ctx).Consistency(gocql.One).Scan(&id, &text)
+//	 err := session.Query(`SELECT id, text FROM tweet WHERE timeline = ? LIMIT 1`,
+//			"me").WithContext(ctx).Consistency(gocql.One).Scan(&id, &text)
 //
 // Multiple rows can be read using Iter.Scanner:
 //
-//  scanner := session.Query(`SELECT id, text FROM tweet WHERE timeline = ?`,
-//  	"me").WithContext(ctx).Iter().Scanner()
-//  for scanner.Next() {
-//  	var (
-//  		id gocql.UUID
-//		text string
-//  	)
-//  	err = scanner.Scan(&id, &text)
-//  	if err != nil {
-//  		log.Fatal(err)
-//  	}
-//  	fmt.Println("Tweet:", id, text)
-//  }
-//  // scanner.Err() closes the iterator, so scanner nor iter should be used afterwards.
-//  if err := scanner.Err(); err != nil {
-//  	log.Fatal(err)
-//  }
+//	 scanner := session.Query(`SELECT id, text FROM tweet WHERE timeline = ?`,
+//	 	"me").WithContext(ctx).Iter().Scanner()
+//	 for scanner.Next() {
+//	 	var (
+//	 		id gocql.UUID
+//			text string
+//	 	)
+//	 	err = scanner.Scan(&id, &text)
+//	 	if err != nil {
+//	 		log.Fatal(err)
+//	 	}
+//	 	fmt.Println("Tweet:", id, text)
+//	 }
+//	 // scanner.Err() closes the iterator, so scanner nor iter should be used afterwards.
+//	 if err := scanner.Err(); err != nil {
+//	 	log.Fatal(err)
+//	 }
 //
 // See Example for complete example.
 //
-// Prepared statements
+// # Prepared statements
 //
 // The driver automatically prepares DML queries (SELECT/INSERT/UPDATE/DELETE/BATCH statements) and maintains a cache
 // of prepared statements.
@@ -163,71 +172,71 @@
 // The main advantage is the ability to keep the same prepared statement even when you don't
 // want to update some fields, where before you needed to make another prepared statement.
 //
-// Executing multiple queries concurrently
+// # Executing multiple queries concurrently
 //
 // Session is safe to use from multiple goroutines, so to execute multiple concurrent queries, just execute them
 // from several worker goroutines. Gocql provides synchronously-looking API (as recommended for Go APIs) and the queries
 // are executed asynchronously at the protocol level.
 //
-//  results := make(chan error, 2)
-//  go func() {
-//  	results <- session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
-//  		"me", gocql.TimeUUID(), "hello world 1").Exec()
-//  }()
-//  go func() {
-//  	results <- session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
-//  		"me", gocql.TimeUUID(), "hello world 2").Exec()
-//  }()
+//	results := make(chan error, 2)
+//	go func() {
+//		results <- session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
+//			"me", gocql.TimeUUID(), "hello world 1").Exec()
+//	}()
+//	go func() {
+//		results <- session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
+//			"me", gocql.TimeUUID(), "hello world 2").Exec()
+//	}()
 //
-// Nulls
+// # Nulls
 //
 // Null values are are unmarshalled as zero value of the type. If you need to distinguish for example between text
 // column being null and empty string, you can unmarshal into *string variable instead of string.
 //
-//  var text *string
-//  err := scanner.Scan(&text)
-//  if err != nil {
-//  	// handle error
-//  }
-//  if text != nil {
-//  	// not null
-//  }
-//  else {
-//  	// null
-//  }
+//	var text *string
+//	err := scanner.Scan(&text)
+//	if err != nil {
+//		// handle error
+//	}
+//	if text != nil {
+//		// not null
+//	}
+//	else {
+//		// null
+//	}
 //
 // See Example_nulls for full example.
 //
-// Reusing slices
+// # Reusing slices
 //
 // The driver reuses backing memory of slices when unmarshalling. This is an optimization so that a buffer does not
 // need to be allocated for every processed row. However, you need to be careful when storing the slices to other
 // memory structures.
 //
-//  scanner := session.Query(`SELECT myints FROM table WHERE pk = ?`, "key").WithContext(ctx).Iter().Scanner()
-//  var myInts []int
-//  for scanner.Next() {
-//  	// This scan reuses backing store of myInts for each row.
-//  	err = scanner.Scan(&myInts)
-//  	if err != nil {
-//  		log.Fatal(err)
-//  	}
-//  }
+//	scanner := session.Query(`SELECT myints FROM table WHERE pk = ?`, "key").WithContext(ctx).Iter().Scanner()
+//	var myInts []int
+//	for scanner.Next() {
+//		// This scan reuses backing store of myInts for each row.
+//		err = scanner.Scan(&myInts)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	}
 //
 // When you want to save the data for later use, pass a new slice every time. A common pattern is to declare the
 // slice variable within the scanner loop:
 //
-//  scanner := session.Query(`SELECT myints FROM table WHERE pk = ?`, "key").WithContext(ctx).Iter().Scanner()
-//  for scanner.Next() {
-//  	var myInts []int
-//  	// This scan always gets pointer to fresh myInts slice, so does not reuse memory.
-//  	err = scanner.Scan(&myInts)
-//  	if err != nil {
-//  		log.Fatal(err)
-//  	}
-//  }
+//	scanner := session.Query(`SELECT myints FROM table WHERE pk = ?`, "key").WithContext(ctx).Iter().Scanner()
+//	for scanner.Next() {
+//		var myInts []int
+//		// This scan always gets pointer to fresh myInts slice, so does not reuse memory.
+//		err = scanner.Scan(&myInts)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	}
 //
-// Paging
+// # Paging
 //
 // The driver supports paging of results with automatic prefetch, see ClusterConfig.PageSize, Session.SetPrefetch,
 // Query.PageSize, and Query.Prefetch.
@@ -258,14 +267,14 @@
 //
 // See Example_paging for an example of manual paging.
 //
-// Dynamic list of columns
+// # Dynamic list of columns
 //
 // There are certain situations when you don't know the list of columns in advance, mainly when the query is supplied
 // by the user. Iter.Columns, Iter.RowData, Iter.MapScan and Iter.SliceMap can be used to handle this case.
 //
 // See Example_dynamicColumns.
 //
-// Batches
+// # Batches
 //
 // The CQL protocol supports sending batches of DML statements (INSERT/UPDATE/DELETE) and so does gocql.
 // Use Session.NewBatch to create a new batch and then fill-in details of individual queries.
@@ -292,7 +301,7 @@
 //
 // See Example_batch for an example.
 //
-// Lightweight transactions
+// # Lightweight transactions
 //
 // Query.ScanCAS or Query.MapScanCAS can be used to execute a single-statement lightweight transaction (an
 // INSERT/UPDATE .. IF statement) and reading its result. See example for Query.MapScanCAS.
@@ -302,7 +311,7 @@
 // Session.MapExecuteBatchCAS when executing the batch to learn about the result of the LWT. See example for
 // Session.MapExecuteBatchCAS.
 //
-// Retries and speculative execution
+// # Retries and speculative execution
 //
 // Queries can be marked as idempotent. Marking the query as idempotent tells the driver that the query can be executed
 // multiple times without affecting its result. Non-idempotent queries are not eligible for retrying nor speculative
@@ -316,21 +325,21 @@
 // is still executing. The two parallel executions of the query race to return a result, the first received result will
 // be returned.
 //
-// User-defined types
+// # User-defined types
 //
 // UDTs can be mapped (un)marshaled from/to map[string]interface{} a Go struct (or a type implementing
 // UDTUnmarshaler, UDTMarshaler, Unmarshaler or Marshaler interfaces).
 //
 // For structs, cql tag can be used to specify the CQL field name to be mapped to a struct field:
 //
-//  type MyUDT struct {
-//  	FieldA int32 `cql:"a"`
-//  	FieldB string `cql:"b"`
-//  }
+//	type MyUDT struct {
+//		FieldA int32 `cql:"a"`
+//		FieldB string `cql:"b"`
+//	}
 //
 // See Example_userDefinedTypesMap, Example_userDefinedTypesStruct, ExampleUDTMarshaler, ExampleUDTUnmarshaler.
 //
-// Metrics and tracing
+// # Metrics and tracing
 //
 // It is possible to provide observer implementations that could be used to gather metrics:
 //
