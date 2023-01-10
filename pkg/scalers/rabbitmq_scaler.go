57f070876297442676e30ae83652bda345bc41a7
Abhishek Mohite <b518004@iiit-bh.ac.in>
2023-01-10 18:08:13 +0530

    Fix: linting errors
    Signed-off-by: Abhishek Mohite <b518004@iiit-bh.ac.in>

--- pkg/scalers/rabbitmq_scaler.go
+++ pkg/scalers/rabbitmq_scaler.go
@@ -38,7 +38,7 @@ const (
 	defaultRabbitMQQueueLength             = 20
 	rabbitMetricType                       = "External"
 	rabbitRootVhostPath                    = "/%2F"
-	enable                                 = "enable"
+	rmqTlsEnable                           = "enable"
 )
 
 const (
@@ -180,7 +180,7 @@ func parseRabbitMQMetadata(config *ScalerConfig) (*rabbitMQMetadata, error) {
 	meta.enableTLS = false
 	if val, ok := config.AuthParams["tls"]; ok {
 		val = strings.TrimSpace(val)
-		if val == enable {
+		if val == rmqTlsEnable {
 			certGiven := config.AuthParams["cert"] != ""
 			keyGiven := config.AuthParams["key"] != ""
 			if certGiven && !keyGiven {
