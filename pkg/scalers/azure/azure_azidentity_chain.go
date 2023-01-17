5b16b24f043d211e12eceecf3d4e7e63f16d1ac1
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2023-01-17 14:43:04 +0100

    cherry-pick: fix aad pod identity
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- pkg/scalers/azure/azure_azidentity_chain.go
+++ pkg/scalers/azure/azure_azidentity_chain.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+
+	"github.com/kedacore/keda/v2/apis/keda/v1alpha1"
 )
 
-func NewChainedCredential(identityID string) (*azidentity.ChainedTokenCredential, error) {
+func NewChainedCredential(identityID string, podIdentity v1alpha1.PodIdentityProvider) (*azidentity.ChainedTokenCredential, error) {
 	var creds []azcore.TokenCredential
 
 	// Used for local debug based on az-cli user
@@ -19,15 +21,24 @@ func NewChainedCredential(identityID string) (*azidentity.ChainedTokenCredential
 		}
 	}
 
-	// Used for aad-pod-identity
-	msiCred, err := ManagedIdentityWrapperCredential(identityID)
-	if err == nil {
-		creds = append(creds, msiCred)
-	}
-
-	wiCred, err := NewADWorkloadIdentityCredential(identityID)
-	if err == nil {
-		creds = append(creds, wiCred)
+	// https://github.com/kedacore/keda/issues/4123
+	// We shouldn't register both in the same chain because if both are registered, KEDA will use the first one
+	// which returns a valid token. This could produce an unintended behaviour if end-users use 2 different identities
+	// with 2 different permissions. They could set workload-identity with the identity A, but KEDA would use
+	// aad-pod-identity with the identity B. If both identities are differents or have different permissions, this blocks
+	// workload identity
+	switch podIdentity {
+	case v1alpha1.PodIdentityProviderAzure:
+		// Used for aad-pod-identity
+		msiCred, err := ManagedIdentityWrapperCredential(identityID)
+		if err == nil {
+			creds = append(creds, msiCred)
+		}
+	case v1alpha1.PodIdentityProviderAzureWorkload:
+		wiCred, err := NewADWorkloadIdentityCredential(identityID)
+		if err == nil {
+			creds = append(creds, wiCred)
+		}
 	}
 
 	// Create the chained credential based on the previous 3
