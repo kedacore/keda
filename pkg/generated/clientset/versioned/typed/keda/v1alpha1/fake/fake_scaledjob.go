30387aa4b185405692f9c340c58d5af9dee1869e
Jorge Turrado <jorge_turrado@hotmail.es>
2023-05-09 21:51:30 +0200

    update generated files
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- pkg/generated/clientset/versioned/typed/keda/v1alpha1/fake/fake_scaledjob.go
+++ pkg/generated/clientset/versioned/typed/keda/v1alpha1/fake/fake_scaledjob.go
@@ -24,7 +24,6 @@ import (
 	v1alpha1 "github.com/kedacore/keda/v2/apis/keda/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -36,9 +35,9 @@ type FakeScaledJobs struct {
 	ns   string
 }
 
-var scaledjobsResource = schema.GroupVersionResource{Group: "keda", Version: "v1alpha1", Resource: "scaledjobs"}
+var scaledjobsResource = v1alpha1.SchemeGroupVersion.WithResource("scaledjobs")
 
-var scaledjobsKind = schema.GroupVersionKind{Group: "keda", Version: "v1alpha1", Kind: "ScaledJob"}
+var scaledjobsKind = v1alpha1.SchemeGroupVersion.WithKind("ScaledJob")
 
 // Get takes name of the scaledJob, and returns the corresponding scaledJob object, and an error if there is any.
 func (c *FakeScaledJobs) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.ScaledJob, err error) {
