9d3791449e53f5a4e6d7384c363f2a9eb9c40a09
Tobo Atchou <tobo.atchou@gmail.com>
2023-05-31 19:27:54 +0200

    fix: stop scale loop for pause Scaledbject (issue #4253). manifest for paused condition
    Signed-off-by: Tobo Atchou <tobo.atchou@gmail.com>

--- apis/keda/v1alpha1/scaledobject_types.go
+++ apis/keda/v1alpha1/scaledobject_types.go
@@ -34,6 +34,7 @@ import (
 // +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status"
 // +kubebuilder:printcolumn:name="Active",type="string",JSONPath=".status.conditions[?(@.type==\"Active\")].status"
 // +kubebuilder:printcolumn:name="Fallback",type="string",JSONPath=".status.conditions[?(@.type==\"Fallback\")].status"
+// +kubebuilder:printcolumn:name="Paused",type="string",JSONPath=".status.conditions[?(@.type==\"Paused\")].status"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 
 // ScaledObject is a specification for a ScaledObject resource
