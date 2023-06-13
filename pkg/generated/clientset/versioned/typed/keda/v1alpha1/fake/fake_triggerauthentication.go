644d73086c897add404cd96c65671bfcbd1bc08e
SpirtZhou <iammrzhouzhenghan@gmail.com>
2023-06-13 20:01:31 +0800

    Update
    Signed-off-by: spiritzhou <iammrzhouzhenghan@gmail.com>
    Signed-off-by: SpiritZhou <iammrzhouzhenghan@gmail.com>

--- pkg/generated/clientset/versioned/typed/keda/v1alpha1/fake/fake_triggerauthentication.go
+++ pkg/generated/clientset/versioned/typed/keda/v1alpha1/fake/fake_triggerauthentication.go
@@ -101,6 +101,18 @@ func (c *FakeTriggerAuthentications) Update(ctx context.Context, triggerAuthenti
 	return obj.(*v1alpha1.TriggerAuthentication), err
 }
 
+// UpdateStatus was generated because the type contains a Status member.
+// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
+func (c *FakeTriggerAuthentications) UpdateStatus(ctx context.Context, triggerAuthentication *v1alpha1.TriggerAuthentication, opts v1.UpdateOptions) (*v1alpha1.TriggerAuthentication, error) {
+	obj, err := c.Fake.
+		Invokes(testing.NewUpdateSubresourceAction(triggerauthenticationsResource, "status", c.ns, triggerAuthentication), &v1alpha1.TriggerAuthentication{})
+
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v1alpha1.TriggerAuthentication), err
+}
+
 // Delete takes name of the triggerAuthentication and deletes it. Returns an error if one occurs.
 func (c *FakeTriggerAuthentications) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	_, err := c.Fake.
