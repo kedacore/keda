c9c6f1b9502c0242a22b4ce8ea269311fe5a17b9
Jakub Adamus <krarky@zobak.cz>
2023-06-16 00:30:37 +0200

    Add E2E tests for RabbitMQ with Azure Workload Identity auth & fix rabbitmq generic tests
    Signed-off-by: Jakub Adamus <jakub.adamus@vivantis.cz>

--- tests/scalers/rabbitmq/rabbitmq_queue_http_regex/rabbitmq_queue_http_regex_test.go
+++ tests/scalers/rabbitmq/rabbitmq_queue_http_regex/rabbitmq_queue_http_regex_test.go
@@ -17,7 +17,7 @@ import (
 )
 
 // Load environment variables from .env file
-var _ = godotenv.Load("../../.env")
+var _ = godotenv.Load("../../../.env")
 
 const (
 	testName = "rmq-queue-http-regex-test"
@@ -83,7 +83,7 @@ func TestScaler(t *testing.T) {
 	kc := GetKubernetesClient(t)
 	data, templates := getTemplateData()
 
-	RMQInstall(t, kc, rmqNamespace, user, password, vhost)
+	RMQInstall(t, kc, rmqNamespace, user, password, vhost, WithoutOAuth())
 	CreateKubernetesResources(t, kc, testNamespace, data, templates)
 
 	assert.True(t, WaitForDeploymentReplicaReadyCount(t, kc, deploymentName, testNamespace, 0, 60, 1),
@@ -94,7 +94,7 @@ func TestScaler(t *testing.T) {
 	// cleanup
 	t.Log("--- cleaning up ---")
 	DeleteKubernetesResources(t, testNamespace, data, templates)
-	RMQUninstall(t, rmqNamespace, user, password, vhost)
+	RMQUninstall(t, rmqNamespace, user, password, vhost, WithoutOAuth())
 }
 
 func getTemplateData() (templateData, []Template) {
