c9c35f2a135bfe54ab475b8cee573ad67ad650f5
Alexander Narsudinov <a.narsudinov@gmail.com>
2023-06-20 10:40:17 +0200

    test: Factor out helpers from postgresql_test.go to separate package
    We are going to introduce a new test for HA PostgreSQL, so this patch moves
    all reusable code from the test code to the helper.
    
    In the next patch we'll use this helper to build a new test case.
    
    Signed-off-by: Alexander Narsudinov <a.narsudinov@gmail.com>

--- tests/scalers/postgresql/postgresql_standalone/postgresql_test.go
+++ tests/scalers/postgresql/postgresql_standalone/postgresql_test.go
@@ -13,6 +13,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 
 	. "github.com/kedacore/keda/v2/tests/helper"
+	pg "github.com/kedacore/keda/v2/tests/scalers/postgresql/helper"
 )
 
 // Load environment variables from .env file
@@ -28,15 +29,15 @@ var (
 	scaledObjectName           = fmt.Sprintf("%s-so", testName)
 	triggerAuthenticationName  = fmt.Sprintf("%s-ta", testName)
 	secretName                 = fmt.Sprintf("%s-secret", testName)
+	secretKey                  = "postgresql_conn_str"
 	postgreSQLStatefulSetName  = "postgresql"
 	postgresqlPodName          = fmt.Sprintf("%s-0", postgreSQLStatefulSetName)
 	postgreSQLUsername         = "test-user"
 	postgreSQLPassword         = "test-password"
 	postgreSQLDatabase         = "test_db"
-	postgreSQLConnectionString = fmt.Sprintf("postgresql://%s:%s@postgresql.%s.svc.cluster.local:5432/%s?sslmode=disable",
-		postgreSQLUsername, postgreSQLPassword, testNamespace, postgreSQLDatabase)
-	minReplicaCount = 0
-	maxReplicaCount = 2
+	postgreSQLConnectionString = pg.GetConnectionString(postgreSQLUsername, postgreSQLPassword, []string{postgreSQLStatefulSetName}, testNamespace, postgreSQLDatabase)
+	minReplicaCount            = 0
+	maxReplicaCount            = 2
 )
 
 type templateData struct {
@@ -45,6 +46,8 @@ type templateData struct {
 	ScaledObjectName                 string
 	TriggerAuthenticationName        string
 	SecretName                       string
+	SecretKey                        string
+	PostgreSQLImage                  string
 	PostgreSQLStatefulSetName        string
 	PostgreSQLConnectionStringBase64 string
 	PostgreSQLUsername               string
@@ -55,40 +58,6 @@ type templateData struct {
 }
 
 const (
-	deploymentTemplate = `apiVersion: apps/v1
-kind: Deployment
-metadata:
-  labels:
-    app: postgresql-update-worker
-  name: {{.DeploymentName}}
-  namespace: {{.TestNamespace}}
-spec:
-  replicas: 0
-  selector:
-    matchLabels:
-      app: postgresql-update-worker
-  template:
-    metadata:
-      labels:
-        app: postgresql-update-worker
-    spec:
-      containers:
-      - image: ghcr.io/kedacore/tests-postgresql
-        imagePullPolicy: Always
-        name: postgresql-processor-test
-        command:
-          - /app
-          - update
-        env:
-          - name: TASK_INSTANCES_COUNT
-            value: "6000"
-          - name: CONNECTION_STRING
-            valueFrom:
-              secretKeyRef:
-                name: {{.SecretName}}
-                key: postgresql_conn_str
-`
-
 	secretTemplate = `apiVersion: v1
 kind: Secret
 metadata:
@@ -97,155 +66,6 @@ metadata:
 type: Opaque
 data:
   postgresql_conn_str: {{.PostgreSQLConnectionStringBase64}}
-`
-
-	triggerAuthenticationTemplate = `apiVersion: keda.sh/v1alpha1
-kind: TriggerAuthentication
-metadata:
-  name: {{.TriggerAuthenticationName}}
-  namespace: {{.TestNamespace}}
-spec:
-  secretTargetRef:
-  - parameter: connection
-    name: {{.SecretName}}
-    key: postgresql_conn_str
-`
-
-	scaledObjectTemplate = `apiVersion: keda.sh/v1alpha1
-kind: ScaledObject
-metadata:
-  name: {{.ScaledObjectName}}
-  namespace: {{.TestNamespace}}
-spec:
-  scaleTargetRef:
-    name: {{.DeploymentName}}
-  pollingInterval: 5
-  cooldownPeriod:  10
-  minReplicaCount: {{.MinReplicaCount}}
-  maxReplicaCount: {{.MaxReplicaCount}}
-  triggers:
-  - type: postgresql
-    metadata:
-      targetQueryValue: "4"
-      activationTargetQueryValue: "5"
-      query: "SELECT CEIL(COUNT(*) / 5) FROM task_instance WHERE state='running' OR state='queued'"
-    authenticationRef:
-      name: {{.TriggerAuthenticationName}}
-`
-
-	postgreSQLStatefulSetTemplate = `apiVersion: apps/v1
-kind: StatefulSet
-metadata:
-  labels:
-    app: {{.PostgreSQLStatefulSetName}}
-  name: {{.PostgreSQLStatefulSetName}}
-  namespace: {{.TestNamespace}}
-spec:
-  replicas: 1
-  serviceName: {{.PostgreSQLStatefulSetName}}
-  selector:
-    matchLabels:
-      app: {{.PostgreSQLStatefulSetName}}
-  template:
-    metadata:
-      labels:
-        app: {{.PostgreSQLStatefulSetName}}
-    spec:
-      containers:
-      - image: postgres:10.5
-        name: postgresql
-        env:
-          - name: POSTGRES_USER
-            value: {{.PostgreSQLUsername}}
-          - name: POSTGRES_PASSWORD
-            value: {{.PostgreSQLPassword}}
-          - name: POSTGRES_DB
-            value: {{.PostgreSQLDatabase}}
-        ports:
-          - name: postgresql
-            protocol: TCP
-            containerPort: 5432
-`
-
-	postgreSQLServiceTemplate = `apiVersion: v1
-kind: Service
-metadata:
-  labels:
-    app: {{.PostgreSQLStatefulSetName}}
-  name: {{.PostgreSQLStatefulSetName}}
-  namespace: {{.TestNamespace}}
-spec:
-  ports:
-  - port: 5432
-    protocol: TCP
-    targetPort: 5432
-  selector:
-    app: {{.PostgreSQLStatefulSetName}}
-  type: ClusterIP
-`
-
-	lowLevelRecordsJobTemplate = `apiVersion: batch/v1
-kind: Job
-metadata:
-  labels:
-    app: postgresql-insert-low-level-job
-  name: postgresql-insert-low-level-job
-  namespace: {{.TestNamespace}}
-spec:
-  template:
-    metadata:
-      labels:
-        app: postgresql-insert-low-level-job
-    spec:
-      containers:
-      - image: ghcr.io/kedacore/tests-postgresql
-        imagePullPolicy: Always
-        name: postgresql-processor-test
-        command:
-          - /app
-          - insert
-        env:
-          - name: TASK_INSTANCES_COUNT
-            value: "20"
-          - name: CONNECTION_STRING
-            valueFrom:
-              secretKeyRef:
-                name: {{.SecretName}}
-                key: postgresql_conn_str
-      restartPolicy: Never
-  backoffLimit: 4
-`
-
-	insertRecordsJobTemplate = `apiVersion: batch/v1
-kind: Job
-metadata:
-  labels:
-    app: postgresql-insert-job
-  name: postgresql-insert-job
-  namespace: {{.TestNamespace}}
-spec:
-  template:
-    metadata:
-      labels:
-        app: postgresql-insert-job
-    spec:
-      containers:
-      - image: ghcr.io/kedacore/tests-postgresql
-        imagePullPolicy: Always
-        name: postgresql-processor-test
-        command:
-          - /app
-          - insert
-        env:
-          - name: TASK_INSTANCES_COUNT
-            value: "10000"
-          - name: CONNECTION_STRING
-            valueFrom:
-              secretKeyRef:
-                name: {{.SecretName}}
-                key: postgresql_conn_str
-      restartPolicy: Never
-  backoffLimit: 4
 `
 )
 
@@ -280,14 +100,14 @@ func TestPostreSQLScaler(t *testing.T) {
 
 func testActivation(t *testing.T, kc *kubernetes.Clientset, data templateData) {
 	t.Log("--- testing activation ---")
-	KubectlApplyWithTemplate(t, data, "lowLevelRecordsJobTemplate", lowLevelRecordsJobTemplate)
+	KubectlApplyWithTemplate(t, data, "lowLevelRecordsJobTemplate", pg.LowLevelRecordsJobTemplate)
 
 	AssertReplicaCountNotChangeDuringTimePeriod(t, kc, deploymentName, testNamespace, minReplicaCount, 60)
 }
 
 func testScaleOut(t *testing.T, kc *kubernetes.Clientset, data templateData) {
 	t.Log("--- testing scale out ---")
-	KubectlApplyWithTemplate(t, data, "insertRecordsJobTemplate", insertRecordsJobTemplate)
+	KubectlApplyWithTemplate(t, data, "insertRecordsJobTemplate", pg.InsertRecordsJobTemplate)
 
 	assert.True(t, WaitForDeploymentReplicaReadyCount(t, kc, deploymentName, testNamespace, maxReplicaCount, 60, 3),
 		"replica count should be %d after 3 minutes", maxReplicaCount)
@@ -309,6 +129,8 @@ var data = templateData{
 	MaxReplicaCount:                  maxReplicaCount,
 	TriggerAuthenticationName:        triggerAuthenticationName,
 	SecretName:                       secretName,
+	SecretKey:                        secretKey,
+	PostgreSQLImage:                  pg.PostgresqlImage,
 	PostgreSQLUsername:               postgreSQLUsername,
 	PostgreSQLPassword:               postgreSQLPassword,
 	PostgreSQLDatabase:               postgreSQLDatabase,
@@ -317,16 +139,16 @@ var data = templateData{
 
 func getPostgreSQLTemplateData() (templateData, []Template) {
 	return data, []Template{
-		{Name: "postgreSQLStatefulSetTemplate", Config: postgreSQLStatefulSetTemplate},
-		{Name: "postgreSQLServiceTemplate", Config: postgreSQLServiceTemplate},
+		{Name: "postgreSQLStatefulSetTemplate", Config: pg.PostgreSQLStatefulSetTemplate},
+		{Name: "postgreSQLServiceTemplate", Config: pg.PostgreSQLServiceTemplate},
 	}
 }
 
 func getTemplateData() (templateData, []Template) {
 	return data, []Template{
 		{Name: "secretTemplate", Config: secretTemplate},
-		{Name: "deploymentTemplate", Config: deploymentTemplate},
-		{Name: "triggerAuthenticationTemplate", Config: triggerAuthenticationTemplate},
-		{Name: "scaledObjectTemplate", Config: scaledObjectTemplate},
+		{Name: "deploymentTemplate", Config: pg.DeploymentTemplate},
+		{Name: "triggerAuthenticationTemplate", Config: pg.TriggerAuthenticationTemplate},
+		{Name: "scaledObjectTemplate", Config: pg.ScaledObjectTemplate},
 	}
 }
