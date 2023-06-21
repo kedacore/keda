e949528485e6ee4e1caa08182571a6badb24619d
Tom Kerkhove <kerkhove.tom@gmail.com>
2023-06-21 16:07:03 +0200

    Revert
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- controllers/keda/triggerauthentication_controller.go
+++ controllers/keda/triggerauthentication_controller.go
@@ -21,7 +21,7 @@ import (
 	"sync"
 
 	corev1 "k8s.io/api/core/v1"
-	errors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
