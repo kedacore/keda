42f14a1345d0fa59ecae4cfe8182fb85105f3d96
SpiritZhou <iammrzhouzhenghan@gmail.com>
2023-07-07 15:43:50 +0800

    Fix
    Signed-off-by: SpiritZhou <iammrzhouzhenghan@gmail.com>

--- tests/scalers/azure/azure_aad_pod_identity/azure_aad_pod_identityte_test.go
+++ tests/scalers/azure/azure_aad_pod_identity/azure_aad_pod_identityte_test.go
@@ -10,10 +10,9 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
-	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/kedacore/keda/v2/tests/helper"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Load environment variables from .env file
@@ -25,26 +24,26 @@ const (
 
 var (
 	testNamespace                 = fmt.Sprintf("%s-ns", testName)
-	triggerAuthEmptyIdName        = fmt.Sprintf("%s-ta-empty", testName)
-	triggerAuthNilIdName          = fmt.Sprintf("%s-ta-nil", testName)
-	clusterTriggerAuthEmptyIdName = fmt.Sprintf("%s-cta-empty", testName)
-	clusterTriggerAuthNilIdName   = fmt.Sprintf("%s-cta-nil", testName)
+	triggerAuthEmptyIDName        = fmt.Sprintf("%s-ta-empty", testName)
+	triggerAuthNilIDName          = fmt.Sprintf("%s-ta-nil", testName)
+	clusterTriggerAuthEmptyIDName = fmt.Sprintf("%s-cta-empty", testName)
+	clusterTriggerAuthNilIDName   = fmt.Sprintf("%s-cta-nil", testName)
 )
 
 type templateData struct {
 	TestNamespace                 string
-	TriggerAuthEmptyIdName        string
-	TriggerAuthNilIdName          string
-	ClusterTriggerAuthEmptyIdName string
-	ClusterTriggerAuthNilIdName   string
+	TriggerAuthEmptyIDName        string
+	TriggerAuthNilIDName          string
+	ClusterTriggerAuthEmptyIDName string
+	ClusterTriggerAuthNilIDName   string
 }
 
 const (
-	triggerAuthEmptyIdTemplate = `
+	triggerAuthEmptyIDTemplate = `
 apiVersion: keda.sh/v1alpha1
 kind: TriggerAuthentication
 metadata:
-  name: {{.TriggerAuthEmptyIdName}}
+  name: {{.TriggerAuthEmptyIDName}}
   namespace: {{.TestNamespace}}
 spec:
   podIdentity:
@@ -52,32 +51,32 @@ spec:
     identityId: ""
 `
 
-	triggerAuthNilIdTemplate = `
+	triggerAuthNilIDTemplate = `
 apiVersion: keda.sh/v1alpha1
 kind: TriggerAuthentication
 metadata:
-  name: {{.TriggerAuthNilIdName}}
+  name: {{.TriggerAuthNilIDName}}
   namespace: {{.TestNamespace}}
 spec:
   podIdentity:
     provider: azure
 `
-	clusterTriggerAuthEmptyIdTemplate = `
+	clusterTriggerAuthEmptyIDTemplate = `
 apiVersion: keda.sh/v1alpha1
 kind: ClusterTriggerAuthentication
 metadata:
-  name: {{.ClusterTriggerAuthEmptyIdName}}
+  name: {{.ClusterTriggerAuthEmptyIDName}}
 spec:
   podIdentity:
     provider: azure
     identityId: ""
 `
 
-	clusterTriggerAuthNilIdTemplate = `
+	clusterTriggerAuthNilIDTemplate = `
 apiVersion: keda.sh/v1alpha1
 kind: ClusterTriggerAuthentication
 metadata:
-  name: {{.ClusterTriggerAuthNilIdName}}
+  name: {{.ClusterTriggerAuthNilIDName}}
 spec:
   podIdentity:
     provider: azure
@@ -95,10 +94,10 @@ func TestScaler(t *testing.T) {
 	CreateKubernetesResources(t, kc, testNamespace, data, templates)
 
 	// test auth
-	testTriggerAuthenticationWithEmptyID(t, kc, data)
-	testTriggerAuthenticationWithNilID(t, kc, data)
-	testClusterTriggerAuthenticationWithEmptyID(t, kc, data)
-	testClusterTriggerAuthenticationWithNilID(t, kc, data)
+	testTriggerAuthenticationWithEmptyID(t, data)
+	testTriggerAuthenticationWithNilID(t, data)
+	testClusterTriggerAuthenticationWithEmptyID(t, data)
+	testClusterTriggerAuthenticationWithNilID(t, data)
 
 	// cleanup
 	DeleteKubernetesResources(t, testNamespace, data, templates)
@@ -107,47 +106,47 @@ func TestScaler(t *testing.T) {
 func getTemplateData() (templateData, []Template) {
 	return templateData{
 		TestNamespace:                 testNamespace,
-		TriggerAuthEmptyIdName:        triggerAuthEmptyIdName,
-		TriggerAuthNilIdName:          triggerAuthNilIdName,
-		ClusterTriggerAuthEmptyIdName: clusterTriggerAuthEmptyIdName,
-		ClusterTriggerAuthNilIdName:   clusterTriggerAuthNilIdName,
+		TriggerAuthEmptyIDName:        triggerAuthEmptyIDName,
+		TriggerAuthNilIDName:          triggerAuthNilIDName,
+		ClusterTriggerAuthEmptyIDName: clusterTriggerAuthEmptyIDName,
+		ClusterTriggerAuthNilIDName:   clusterTriggerAuthNilIDName,
 	}, []Template{}
 }
 
 // expect triggerauthentication should not be created with empty identity id
-func testTriggerAuthenticationWithEmptyID(t *testing.T, kc *kubernetes.Clientset, data templateData) {
+func testTriggerAuthenticationWithEmptyID(t *testing.T, data templateData) {
 	t.Log("--- create triggerauthentication with empty identity id  ---")
 
-	err := KubectlApplyWithErrors(t, data, "triggerAuthEmptyIdTemplate", triggerAuthEmptyIdTemplate)
+	err := KubectlApplyWithErrors(t, data, "triggerAuthEmptyIDTemplate", triggerAuthEmptyIDTemplate)
 	assert.Errorf(t, err, "can deploy TriggerAuthtication - %s", err)
 }
 
 // expect triggerauthentication can be created without identity id property
-func testTriggerAuthenticationWithNilID(t *testing.T, kc *kubernetes.Clientset, data templateData) {
+func testTriggerAuthenticationWithNilID(t *testing.T, data templateData) {
 	t.Log("--- create triggerauthentication with nil identity id  ---")
 
 	kedaKc := GetKedaKubernetesClient(t)
-	KubectlApplyWithTemplate(t, data, "triggerAuthNilIdTemplate", triggerAuthNilIdTemplate)
+	KubectlApplyWithTemplate(t, data, "triggerAuthNilITemplate", triggerAuthNilIDTemplate)
 
-	triggerauthentication, _ := kedaKc.TriggerAuthentications(testNamespace).Get(context.Background(), triggerAuthNilIdName, v1.GetOptions{})
+	triggerauthentication, _ := kedaKc.TriggerAuthentications(testNamespace).Get(context.Background(), triggerAuthNilIDName, v1.GetOptions{})
 	assert.NotNil(t, triggerauthentication)
 }
 
 // expect clustertriggerauthentication should not be created with empty identity id
-func testClusterTriggerAuthenticationWithEmptyID(t *testing.T, kc *kubernetes.Clientset, data templateData) {
+func testClusterTriggerAuthenticationWithEmptyID(t *testing.T, data templateData) {
 	t.Log("--- create clustertriggerauthentication with empty identity id  ---")
 
-	err := KubectlApplyWithErrors(t, data, "clusterTriggerAuthEmptyIdTemplate", clusterTriggerAuthEmptyIdTemplate)
+	err := KubectlApplyWithErrors(t, data, "clusterTriggerAuthEmptyIDTemplate", clusterTriggerAuthEmptyIDTemplate)
 	assert.Errorf(t, err, "can deploy ClusterTriggerAuthtication - %s", err)
 }
 
 // expect clustertriggerauthentication can be created without identity id property
-func testClusterTriggerAuthenticationWithNilID(t *testing.T, kc *kubernetes.Clientset, data templateData) {
+func testClusterTriggerAuthenticationWithNilID(t *testing.T, data templateData) {
 	t.Log("--- create clustertriggerauthentication with nil identity id  ---")
 
 	kedaKc := GetKedaKubernetesClient(t)
-	KubectlApplyWithTemplate(t, data, "clusterTriggerAuthNilIdTemplate", clusterTriggerAuthNilIdTemplate)
+	KubectlApplyWithTemplate(t, data, "clusterTriggerAuthNilIDTemplate", clusterTriggerAuthNilIDTemplate)
 
-	clustertriggerauthentication, _ := kedaKc.ClusterTriggerAuthentications().Get(context.Background(), clusterTriggerAuthNilIdTemplate, v1.GetOptions{})
+	clustertriggerauthentication, _ := kedaKc.ClusterTriggerAuthentications().Get(context.Background(), clusterTriggerAuthNilIDTemplate, v1.GetOptions{})
 	assert.NotNil(t, clustertriggerauthentication)
 }
