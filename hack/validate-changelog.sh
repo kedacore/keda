bea538eee2e1dd01e182ace6f3fc303e7f42a96a
Jorge Turrado <jorge_turrado@hotmail.es>
2023-08-16 23:33:26 +0200

    Add script to check the whole changelog and fix issues there
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- hack/validate-changelog.sh
+++ hack/validate-changelog.sh
@@ -11,19 +11,14 @@ if [[ ! -f "$filename" ]]; then
     exit 1
 fi
 
-# Read content between "## Unreleased" and "## v" into variable
-unreleased=$(awk '/## Unreleased/{flag=1;next}/## v[0-9\.]+/{flag=0}flag' "$filename")
-
-# Check if "Unreleased" section exists
-if [[ -z "$unreleased" ]]; then
-    echo "Error: No 'Unreleased' section found in $filename."
-    exit 1
-fi
+# Storing the version to be checked
+mapfile -t versions < <(awk '/## History/{flag=1;next}/## /{flag=0}flag' "$filename" | grep -o '\[[^]]*\]' | grep -v "v1." | sed 's/[][]//g')
 
 # Define a function to extract and sort sections
 function extract_and_check() {
   local section=$1
-  local content=$(awk "/### $section/{flag=1;next}/### /{flag=0}flag" <<< "$unreleased" | grep '^- \*\*')
+  local content_block=$2
+  local content=$(awk "/### $section/{flag=1;next}/### /{flag=0}flag" <<< "$content_block" | grep '^- \*\*')
 
   # Skip if content does not exist
   if [[ -z "$content" ]]; then
@@ -36,25 +31,43 @@ function extract_and_check() {
   # Sort the remaining lines
   local sorted_content=$(echo "$content" | grep -v '^- \*\*General\*\*:' | sort)
 
-  # Concatenate the sorted **General**: lines at the top of the sorted_content
-  sorted_content=$(printf "%s\n%s" "$sorted_general_lines" "$sorted_content")
+  # Check if sorted_general_lines is not empty, then concatenate
+  if [[ -n "$sorted_general_lines" ]]; then
+      sorted_content=$(printf "%s\n%s" "$sorted_general_lines" "$sorted_content")
+  fi
 
   # Check pattern and throw error if wrong pattern found
   while IFS= read -r line; do
-      echo "Error: Wrong pattern found in $section section, line: $line"
+      echo "Error: Wrong pattern found in section: $section , line: $line"
       exit 1
-  done < <(grep -Pv '^(-\s\*\*[^*]+\*\*: .*\(\[#(\d+)\]\(https:\/\/github\.com\/kedacore\/keda\/(pull|issues)\/\2\)\))$' <<< "$content")
+  done < <(grep -Pv '^(-\s\*\*[^*]+\*\*: .*\(\[#(\d+)\]\(https:\/\/github\.com\/kedacore\/(keda|charts|governance)\/(pull|issues|discussions)\/\2\)(?:\|\[#(\d+)\]\(https:\/\/github\.com\/kedacore\/(keda|charts|governance)\/(pull|issues|discussions)\/\5\)){0,}\))$' <<< "$content")
 
   if [ "$content" != "$sorted_content" ]; then
-      echo "Error: The $section section is not sorted correctly. Correct order:"
+      echo "Error: Section: $section is not sorted correctly. Correct order:"
       echo "$sorted_content"
       exit 1
   fi
 }
 
-# Separate content into different sections and check sorting
-extract_and_check "New"
-extract_and_check "Improvements"
-extract_and_check "Fixes"
-extract_and_check "Deprecations"
-extract_and_check "Other"
+
+# Extract release sections, including "Unreleased", and check them
+for version in "${versions[@]}"; do
+  release_content=$(awk "/## $version/{flag=1;next}/## v[0-9\.]+/{flag=0}flag" "$filename")
+
+
+  if [[ -z "$release_content" ]]; then
+    echo "No content found for $version Skipping."
+    continue
+  fi
+
+  echo "Checking section: $version"
+
+  # Separate content into different sections and check sorting for each release
+  extract_and_check "New" "$release_content"
+  extract_and_check "Experimental" "$release_content"
+  extract_and_check "Improvements" "$release_content"
+  extract_and_check "Fixes" "$release_content"
+  extract_and_check "Deprecations" "$release_content"
+  extract_and_check "Other" "$release_content"
+
+done
