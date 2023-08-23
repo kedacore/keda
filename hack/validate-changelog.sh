8438380b25f13dbdb335079facc46051e2fcb60c
Jorge Turrado <jorge_turrado@hotmail.es>
2023-08-23 18:20:46 +0200

    set location and add dummy change
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- hack/validate-changelog.sh
+++ hack/validate-changelog.sh
@@ -26,10 +26,10 @@ function extract_and_check() {
   fi
 
   # Separate and sort the **General**: lines
-  local sorted_general_lines=$(echo "$content" | grep '^- \*\*General\*\*:' | sort --ignore-case --dictionary-order)
+  local sorted_general_lines=$(echo "$content" | grep '^- \*\*General\*\*:' | LC_ALL=en_US sort --ignore-case --dictionary-order)
 
   # Sort the remaining lines
-  local sorted_content=$(echo "$content" | grep -v '^- \*\*General\*\*:' | sort --ignore-case --dictionary-order)
+  local sorted_content=$(echo "$content" | grep -v '^- \*\*General\*\*:' | LC_ALL=en_US sort --ignore-case --dictionary-order)
 
   # Check if sorted_general_lines is not empty, then concatenate
   if [[ -n "$sorted_general_lines" ]]; then
