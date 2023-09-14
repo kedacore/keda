defd1ac52bcf61331b813a7b72649f7e2d46e409
Souyama Debnath <souyama.debnath@atos.net>
2023-09-14 21:42:15 +0530

    Add header block for apache-kafka scaler - With summary of diference between kafka scaler implementation
    Signed-off-by: Souyama Debnath <souyama.debnath@atos.net>

--- pkg/scalers/kafka_scaler.go
+++ pkg/scalers/kafka_scaler.go
@@ -1,3 +1,22 @@
+/*
+Copyright 2023 The KEDA Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// This scaler is based on sarama library.
+// It lacks support for AWS MSK. For AWS MSK please see: apache-kafka scaler.
+
 package scalers
 
 import (
