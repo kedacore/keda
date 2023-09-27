106b94228e5fb2ed9a01ac6fd6b756c206dc9d53
SpiritZhou <iammrzhouzhenghan@gmail.com>
2023-09-27 23:09:59 +0800

    Add temp log for e2e testing
    Signed-off-by: SpiritZhou <iammrzhouzhenghan@gmail.com>

--- tests/sequential/prometheus_metrics/prometheus_metrics_test.go
+++ tests/sequential/prometheus_metrics/prometheus_metrics_test.go
@@ -342,6 +342,10 @@ func testScaledObjectErrors(t *testing.T, data templateData) {
 	time.Sleep(2 * time.Second)
 
 	family := fetchAndParsePrometheusMetrics(t, fmt.Sprintf("curl --insecure %s", kedaOperatorPrometheusURL))
+	for k, v := range family {
+		t.Log(fmt.Sprintf("fetch metric key:%s value:%T\n", k, v))
+	}
+
 	if val, ok := family["keda_scaled_object_errors"]; ok {
 		errCounterVal1 := getErrorMetricsValue(val)
 
