6d81456773b26d5dc71f08710dd53b78e0a9c0c2
Jorge Turrado <jorge_turrado@hotmail.es>
2023-11-22 01:29:49 +0100

    fix: Replace string interpolation with url parsing on prometheus tests
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- pkg/scalers/prometheus_scaler_test.go
+++ pkg/scalers/prometheus_scaler_test.go
@@ -391,16 +391,16 @@ func TestPrometheusScalerExecutePromQueryParameters(t *testing.T) {
 		"second": "bar",
 	}
 	server := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		queryParameter := request.URL.Query()
 		time := time.Now().UTC().Format(time.RFC3339)
-		expectedPath := fmt.Sprintf("/api/v1/query?query=&time=%s&first=foo&second=bar", time)
+		require.Equal(t, queryParameter.Get("time"), fmt.Sprintf("%s", time))
+
 		for queryParameterName, queryParameterValue := range queryParametersValue {
-			queryParameter := request.URL.Query()
-			queryParameter.Add(queryParameterName, queryParameterValue)
+			require.Equal(t, queryParameter.Get(queryParameterName), queryParameterValue)
 		}
 
-		if request.RequestURI != expectedPath {
-			t.Error("Expect request path to =", expectedPath, "but it is", request.RequestURI)
-		}
+		expectedPath := "/api/v1/query"
+		require.Equal(t, request.URL.Path, expectedPath)
 
 		writer.WriteHeader(testData.responseStatus)
 		if _, err := writer.Write([]byte(testData.bodyStr)); err != nil {
