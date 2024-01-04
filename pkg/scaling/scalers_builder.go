842a85fd4bba7b7facb4558d556659f870f72619
Ara Pulido <ara.pulido@datadoghq.com>
2024-01-04 15:25:49 +0100

    Merge both Datadog scalers into one
    Signed-off-by: Ara Pulido <ara.pulido@datadoghq.com>

--- pkg/scaling/scalers_builder.go
+++ pkg/scaling/scalers_builder.go
@@ -162,8 +162,6 @@ func buildScaler(ctx context.Context, client client.Client, triggerType string, 
 		return scalers.NewCronScaler(config)
 	case "datadog":
 		return scalers.NewDatadogScaler(ctx, config)
-	case "datadog-external":
-		return scalers.NewDatadogExternalScaler(ctx, config)
 	case "elasticsearch":
 		return scalers.NewElasticsearchScaler(config)
 	case "etcd":
