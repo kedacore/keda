59969b008962fffb66eb7ced03114c16a4a6f83f
Jorge Turrado <jorge.turrado@scrm.lidl>
2024-01-11 14:41:12 +0100

    Add unit tests
    Signed-off-by: Jorge Turrado <jorge.turrado@scrm.lidl>

--- pkg/util/ginkgo_reporter.go
+++ pkg/util/ginkgo_reporter.go
@@ -3,15 +3,15 @@ package util
 import (
 	"fmt"
 
-	. "github.com/onsi/ginkgo/v2"
+	ginkgo "github.com/onsi/ginkgo/v2"
 )
 
 type GinkgoTestReporter struct{}
 
 func (g GinkgoTestReporter) Errorf(format string, args ...interface{}) {
-	Fail(fmt.Sprintf(format, args...))
+	ginkgo.Fail(fmt.Sprintf(format, args...))
 }
 
 func (g GinkgoTestReporter) Fatalf(format string, args ...interface{}) {
-	Fail(fmt.Sprintf(format, args...))
+	ginkgo.Fail(fmt.Sprintf(format, args...))
 }
