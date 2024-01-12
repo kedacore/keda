540a26d72a5d2556c733d585b47cab94dec233c7
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-01-12 14:32:10 +0100

    Add support for aws secret manager authentication Signed-off-by: geoffrey1330 <israelgeoffrey13@gmail.com>

--- vendor/github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery/middleware.go
+++ vendor/github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery/middleware.go
@@ -28,7 +28,7 @@ type DiscoverEndpointOptions struct {
 	Logger logging.Logger
 }
 
-// DiscoverEndpoint is a serialize step middleware used to discover endpoint
+// DiscoverEndpoint is a finalize step middleware used to discover endpoint
 // for an API operation.
 type DiscoverEndpoint struct {
 
@@ -38,7 +38,7 @@ type DiscoverEndpoint struct {
 
 	// DiscoverOperation represents the endpoint discovery operation that
 	// returns an Endpoint or error.
-	DiscoverOperation func(ctx context.Context, input interface{}, options ...func(*DiscoverEndpointOptions)) (WeightedAddress, error)
+	DiscoverOperation func(ctx context.Context, region string, options ...func(*DiscoverEndpointOptions)) (WeightedAddress, error)
 
 	// EndpointDiscoveryEnableState represents the customer configuration for endpoint
 	// discovery feature.
@@ -47,6 +47,9 @@ type DiscoverEndpoint struct {
 	// EndpointDiscoveryRequired states if an operation requires to perform
 	// endpoint discovery.
 	EndpointDiscoveryRequired bool
+
+	// The client region
+	Region string
 }
 
 // ID represents the middleware identifier
@@ -54,50 +57,46 @@ func (*DiscoverEndpoint) ID() string {
 	return "DiscoverEndpoint"
 }
 
-// HandleSerialize is the serialize step function handler that must be placed after
-// "ResolveEndpoint" middleware, but before "OperationSerializer" middleware.
-func (d *DiscoverEndpoint) HandleSerialize(
-	ctx context.Context, in middleware.SerializeInput, next middleware.SerializeHandler,
-) (out middleware.SerializeOutput, metadata middleware.Metadata, err error) {
-	// if endpoint discovery is explicitly disabled, skip this workflow
+// HandleFinalize performs endpoint discovery and updates the request host with
+// the result.
+//
+// The resolved host from this procedure MUST override that of modeled endpoint
+// resolution and middleware should be ordered accordingly.
+func (d *DiscoverEndpoint) HandleFinalize(
+	ctx context.Context, in middleware.FinalizeInput, next middleware.FinalizeHandler,
+) (out middleware.FinalizeOutput, metadata middleware.Metadata, err error) {
 	if d.EndpointDiscoveryEnableState == aws.EndpointDiscoveryDisabled {
-		return next.HandleSerialize(ctx, in)
+		return next.HandleFinalize(ctx, in)
 	}
 
-	// if operation does not require endpoint discovery, and endpoint discovery is not explicitly enabled,
-	// skip this workflow
 	if !d.EndpointDiscoveryRequired && d.EndpointDiscoveryEnableState != aws.EndpointDiscoveryEnabled {
-		return next.HandleSerialize(ctx, in)
+		return next.HandleFinalize(ctx, in)
 	}
 
-	// when custom endpoint is provided
 	if es := awsmiddleware.GetEndpointSource(ctx); es == aws.EndpointSourceCustom {
-		// error if endpoint discovery was explicitly enabled
 		if d.EndpointDiscoveryEnableState == aws.EndpointDiscoveryEnabled {
-			return middleware.SerializeOutput{}, middleware.Metadata{},
+			return middleware.FinalizeOutput{}, middleware.Metadata{},
 				fmt.Errorf("Invalid configuration: endpoint discovery is enabled, but a custom endpoint is provided")
 		}
 
-		// else skip this workflow
-		return next.HandleSerialize(ctx, in)
+		return next.HandleFinalize(ctx, in)
 	}
 
-	// fetch address using discover operation
-	weightedAddress, err := d.DiscoverOperation(ctx, in.Parameters, d.Options...)
+	weightedAddress, err := d.DiscoverOperation(ctx, d.Region, d.Options...)
 	if err != nil {
-		return middleware.SerializeOutput{}, middleware.Metadata{}, err
+		return middleware.FinalizeOutput{}, middleware.Metadata{}, err
 	}
 
 	req, ok := in.Request.(*smithyhttp.Request)
 	if !ok {
-		return middleware.SerializeOutput{}, middleware.Metadata{},
+		return middleware.FinalizeOutput{}, middleware.Metadata{},
 			fmt.Errorf("expected request to be of type *smithyhttp.Request, got %T", in.Request)
 	}
 
 	if weightedAddress.URL != nil {
-		// assign discovered endpoint to request url
-		req.URL = weightedAddress.URL
+		// we only want the host, normal endpoint resolution can include path/query
+		req.URL.Host = weightedAddress.URL.Host
 	}
 
-	return next.HandleSerialize(ctx, in)
+	return next.HandleFinalize(ctx, in)
 }
