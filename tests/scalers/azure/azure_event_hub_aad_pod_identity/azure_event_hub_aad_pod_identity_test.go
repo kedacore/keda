2e4ec71f6a3292877770244cc1e7da35ed1d98bf
Jorge Turrado <jorge_turrado@hotmail.es>
2024-02-02 22:26:18 +0100

    fix error
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- tests/scalers/azure/azure_event_hub_aad_pod_identity/azure_event_hub_aad_pod_identity_test.go
+++ tests/scalers/azure/azure_event_hub_aad_pod_identity/azure_event_hub_aad_pod_identity_test.go
@@ -150,12 +150,11 @@ func TestScaler(t *testing.T) {
 	ctx := context.Background()
 	t.Log("--- setting up ---")
 	require.NotEmpty(t, storageConnectionString, "TF_AZURE_STORAGE_CONNECTION_STRING env variable is required for azure eventhub test")
-	accountName = azurehelper.GetAccountFromStorageConnectionString(connectionString)
+	accountName = azurehelper.GetAccountFromStorageConnectionString(storageConnectionString)
 
 	eventHubHelper := azurehelper.NewEventHubHelper(t)
 	eventHubHelper.CreateEventHub(ctx, t)
 	blobClient, err := azblob.NewClientFromConnectionString(storageConnectionString, nil)
-	blobClient.ServiceClient().GetAccountInfo(ctx, nil)
 	assert.NoErrorf(t, err, "cannot create the queue client - %s", err)
 	_, err = blobClient.CreateContainer(ctx, checkpointContainerName, nil)
 	assert.NoErrorf(t, err, "cannot create the container - %s", err)
