ebb373347388079e1e76458c3123b04cd85b23a3
rahulii <r.sawra@gmail.com>
2024-02-07 11:23:48 +0530

    remove golang/mock module as it is no longer used
    Signed-off-by: rahulii <r.sawra@gmail.com>

--- hack/tools.go
+++ hack/tools.go
@@ -6,7 +6,6 @@ package tools
 
 import (
 	// Import code-generator to use in build tools
-	_ "github.com/golang/mock/mockgen"
 	_ "go.uber.org/mock/mockgen"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
