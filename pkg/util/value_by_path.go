177fd271ff101da8e5f9b326cc6c383048f1cc6e
Friedrich Albert Kyuri <friedrichak42@gmail.com>
2024-03-22 09:53:42 +0100

    add docstring with examples
    Signed-off-by: Friedrich Albert Kyuri <friedrichak42@gmail.com>

--- pkg/util/value_by_path.go
+++ pkg/util/value_by_path.go
@@ -6,6 +6,24 @@ import (
 )
 
 // GetValueByPath retrieves a value from a nested map using a dot-separated path
+// It also supports .number syntax to access array elements.
+//
+// This is a helper function for niche use cases.
+// Consider using https://pkg.go.dev/k8s.io/apimachinery@v0.29.3/pkg/apis/meta/v1/unstructured#NestedFieldNoCopy instead
+//
+// Examples:
+//
+//	data := map[string]interface{}{
+//	    "a": map[string]interface{}{
+//	        "b": []interface{}{
+//	            map[string]interface{}{"c": 1},
+//	            map[string]interface{}{"c": 2},
+//	        },
+//	    },
+//	}
+//
+// GetValueByPath(data, "a.b.0.c") // 1
+// GetValueByPath(data, "not.found") // error
 func GetValueByPath(data map[string]interface{}, path string) (interface{}, error) {
 	keys := strings.Split(path, ".")
 	current := data
