2c33d758b2f517d5da517de26e1c6ca9750520f5
Jorge Turrado <jorge_turrado@hotmail.es>
2024-03-26 00:26:23 +0100

    fix static checks
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- tests/scalers/rabbitmq/rabbitmq_helper.go
+++ tests/scalers/rabbitmq/rabbitmq_helper.go
@@ -7,10 +7,10 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/kedacore/keda/v2/tests/helper"
-	"github.com/stretchr/testify/assert"
 )
 
 const (
