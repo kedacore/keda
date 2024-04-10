93e605566aed894645191d33f80a8889bea839ca
Jirka Kremser <jiri.kremser@gmail.com>
2024-04-10 14:33:32 +0200

    Fix the noop predicate that's used for all namespaces
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- pkg/util/watch.go
+++ pkg/util/watch.go
@@ -18,7 +18,7 @@ func GetWatchNamespaces() (map[string]cache.Config, error) {
 		return map[string]cache.Config{}, fmt.Errorf("%s must be set", WatchNamespaceEnvVar)
 	}
 
-	if ns == "" {
+	if ns == "" || ns == "\"\"" {
 		return map[string]cache.Config{}, nil
 	}
 	nss := strings.Split(ns, ",")
@@ -31,11 +31,11 @@ func GetWatchNamespaces() (map[string]cache.Config, error) {
 }
 
 // IgnoreOtherNamespaces returns the predicate for watched events that will filter out those that are not coming
-// from a watched namespace (empty namespace denotes all)
+// from a watched namespace (empty namespace or unset env var denotes all)
 func IgnoreOtherNamespaces() predicate.Predicate {
 	nss, e := GetWatchNamespaces()
 	if len(nss) == 0 || e != nil {
-		return predicate.Or()
+		return predicate.And() // no-op predicate that returns always true
 	}
 	return predicate.Funcs{
 		GenericFunc: func(e event.GenericEvent) bool {
