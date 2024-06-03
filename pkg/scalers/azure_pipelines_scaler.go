1c864c3d4ccd537a1d9b9b074ed57edbc39c4f2d
jonas <jonas.rudlang@gmail.com>
2024-06-03 10:20:43 +0200

    remove allElementsExist, use direct count comparison for requireAllDemandsAndIgnoreOthers

--- pkg/scalers/azure_pipelines_scaler.go
+++ pkg/scalers/azure_pipelines_scaler.go
@@ -507,20 +507,10 @@ func getCanAgentDemandFulfilJob(jr JobRequest, metadata *azurePipelinesMetadata)
 
 	if metadata.requireAllDemands {
 		return countDemands == len(demandsInJob) && countDemands == len(demandsInScaler)
-	}
-	if metadata.requireAllDemandsAndIgnoreOthers {
-		return allElementsExist(demandsInScaler, demandsInJob)
+	} else if metadata.requireAllDemandsAndIgnoreOthers {
+		return countDemands == len(demandsInScaler)
 	}
 	return countDemands == len(demandsInJob)
-}
-
-func allElementsExist(a, b []string) bool {
-	for _, elem := range a {
-		if !contains(b, elem) {
-			return false
-		}
-	}
-	return true
 }
 
 // Determine if the Job and Parent Agent Template have matching capabilities
