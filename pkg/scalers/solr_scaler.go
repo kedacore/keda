c11a177816edc3d96acf7f521b3c2e06f89944a8
SpiritZhou <iammrzhouzhenghan@gmail.com>
2024-06-05 10:10:59 +0800

    Update pkg/scalers/solr_scaler.go
    Co-authored-by: Jan Wozniak <wozniak.jan@gmail.com>
    Signed-off-by: SpiritZhou <iammrzhouzhenghan@gmail.com>

--- pkg/scalers/solr_scaler.go
+++ pkg/scalers/solr_scaler.go
@@ -26,7 +26,7 @@ type solrMetadata struct {
 	triggerIndex int
 
 	Host                       string  `keda:"name=host, order=triggerMetadata"`
-	Collection                 string  `keda:"name=collection, order=triggerMetadata, optional"`
+	Collection                 string  `keda:"name=collection, order=triggerMetadata"`
 	TargetQueryValue           float64 `keda:"name=targetQueryValue, order=triggerMetadata"`
 	ActivationTargetQueryValue float64 `keda:"name=activationTargetQueryValue, order=triggerMetadata, optional, default=0"`
 	Query                      string  `keda:"name=query, order=triggerMetadata, optional"`
