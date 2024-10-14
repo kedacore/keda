7390335318618c8600a5f6b43653898346713d3d
Prajithp <prajithpalakkuda@gmail.com>
2024-10-14 07:50:02 +0530

    remove namespace from cli args
    Signed-off-by: Prajithp <prajithpalakkuda@gmail.com>

--- tests/scalers/temporal/temporal_test.go
+++ tests/scalers/temporal/temporal_test.go
@@ -75,7 +75,7 @@ spec:
       containers:
         - name: temporal
           image: temporalio/admin-tools:latest
-          command: ["temporal", "server", "start-dev", "--namespace", "v2", "--ip", "0.0.0.0"]
+          command: ["temporal", "server", "start-dev", "--ip", "0.0.0.0"]
           ports:
             - containerPort: 7233
           livenessProbe:
