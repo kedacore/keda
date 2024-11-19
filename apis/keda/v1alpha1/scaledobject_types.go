6ae98747ff960757b10d630b8daca260ed244dd7
Shane <imu_xxd@163.com>
2024-11-19 14:39:43 +0800

    revise some object name and fix fallback check
    Signed-off-by: Shane <imu_xxd@163.com>

--- apis/keda/v1alpha1/scaledobject_types.go
+++ apis/keda/v1alpha1/scaledobject_types.go
@@ -237,7 +237,7 @@ func (so *ScaledObject) IsUsingModifiers() bool {
 	return so.Spec.Advanced != nil && !reflect.DeepEqual(so.Spec.Advanced.ScalingModifiers, ScalingModifiers{})
 }
 
-// getHPAMinReplicas returns MinReplicas based on definition in ScaledObject or default value if not defined
+// GetHPAMinReplicas returns MinReplicas based on definition in ScaledObject or default value if not defined
 func (so *ScaledObject) GetHPAMinReplicas() *int32 {
 	if so.Spec.MinReplicaCount != nil && *so.Spec.MinReplicaCount > 0 {
 		return so.Spec.MinReplicaCount
@@ -246,7 +246,7 @@ func (so *ScaledObject) GetHPAMinReplicas() *int32 {
 	return &tmp
 }
 
-// getHPAMaxReplicas returns MaxReplicas based on definition in ScaledObject or default value if not defined
+// GetHPAMaxReplicas returns MaxReplicas based on definition in ScaledObject or default value if not defined
 func (so *ScaledObject) GetHPAMaxReplicas() int32 {
 	if so.Spec.MaxReplicaCount != nil {
 		return *so.Spec.MaxReplicaCount
@@ -254,7 +254,7 @@ func (so *ScaledObject) GetHPAMaxReplicas() int32 {
 	return defaultHPAMaxReplicas
 }
 
-// checkReplicaCountBoundsAreValid checks that Idle/Min/Max ReplicaCount defined in ScaledObject are correctly specified
+// CheckReplicaCountBoundsAreValid checks that Idle/Min/Max ReplicaCount defined in ScaledObject are correctly specified
 // i.e. that Min is not greater than Max or Idle greater or equal to Min
 func CheckReplicaCountBoundsAreValid(scaledObject *ScaledObject) error {
 	min := int32(0)
@@ -291,7 +291,7 @@ func CheckFallbackValid(scaledObject *ScaledObject) error {
 			return fmt.Errorf("type is %s , but fallback it is not supported by the CPU & memory scalers", trigger.Type)
 		}
 		if trigger.MetricType != autoscalingv2.AverageValueMetricType {
-			return fmt.Errorf("MetricType=%s, but Fallback can only be enabled for triggers with metric of type AverageValue", trigger.MetricType)
+			return fmt.Errorf("MetricType=%s, but fallback can only be enabled for triggers with metric of type AverageValue", trigger.MetricType)
 		}
 	}
 	return nil
