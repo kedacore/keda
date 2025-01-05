dc4ac3ad1bd4e73e6d6202d6e2fffa15b81839b3
Omer Aplatony <omerap12@gmail.com>
2025-01-05 17:52:29 +0200

    Test scaledObjectName longer then 63 chars
    Signed-off-by: Omer Aplatony <omerap12@gmail.com>

--- tests/scalers/cpu/cpu_test.go
+++ tests/scalers/cpu/cpu_test.go
@@ -28,7 +28,7 @@ var (
 	workloadDeploymentName = fmt.Sprintf("%s-workload-deployment", testName)
 	testNamespace          = fmt.Sprintf("%s-ns", testName)
 	deploymentName         = fmt.Sprintf("%s-deployment", testName)
-	scaledObjectName       = fmt.Sprintf("%s-so", testName)
+	scaledObjectName := "this-is-a-very-long-name-that-exceeds-kubernetes-label-length-limit-xxxxx"
 	hpaName                = fmt.Sprintf("keda-hpa-%s-so", testName)
 )
 
