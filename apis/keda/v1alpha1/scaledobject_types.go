3f80c64d9d1cdab6828e02cf3c5b3ba63e6e1448
Jan Wozniak <wozniak.jan@gmail.com>
2025-04-01 10:26:57 +0200

    Revert "fix!: fix CheckFallbackValid logic (#6538)"
    This reverts commit aa2c4e7c171225012f9f32fd07949deaeb6a8fcf.
    
    Signed-off-by: Jan Wozniak <wozniak.jan@gmail.com>

--- apis/keda/v1alpha1/scaledobject_types.go
+++ apis/keda/v1alpha1/scaledobject_types.go
@@ -23,7 +23,10 @@ import (
 
 	autoscalingv2 "k8s.io/api/autoscaling/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
+
+var scaledobjecttypeslog = logf.Log.WithName("scaledobject-types")
 
 // +genclient
 // +kubebuilder:object:root=true
@@ -294,25 +297,12 @@ func CheckFallbackValid(scaledObject *ScaledObject) error {
 			scaledObject.Spec.Fallback.FailureThreshold, scaledObject.Spec.Fallback.Replicas)
 	}
 
-	if scaledObject.IsUsingModifiers() {
-		if scaledObject.Spec.Advanced.ScalingModifiers.MetricType != autoscalingv2.AverageValueMetricType {
-			return fmt.Errorf("when using ScalingModifiers, ScaledObject.Spec.Advanced.ScalingModifiers.MetricType must be AverageValue to have fallback enabled")
+	for _, trigger := range scaledObject.Spec.Triggers {
+		if trigger.Type == cpuString || trigger.Type == memoryString {
+			scaledobjecttypeslog.Error(nil, fmt.Sprintf("type is %s , but fallback it is not supported by the CPU & memory scalers", trigger.Type))
 		}
-	} else {
-		fallbackValid := false
-		for _, trigger := range scaledObject.Spec.Triggers {
-			if trigger.Type == cpuString || trigger.Type == memoryString {
-				continue
-			}
-			// If at least one trigger is of the type `AverageValue`, then having fallback is valid.
-			if trigger.MetricType == autoscalingv2.AverageValueMetricType {
-				fallbackValid = true
-				break
-			}
-		}
-
-		if !fallbackValid {
-			return fmt.Errorf("at least one trigger (that is not cpu or memory) has to have the `AverageValue` type for the fallback to be enabled")
+		if trigger.MetricType != autoscalingv2.AverageValueMetricType {
+			return fmt.Errorf("MetricType=%s, but fallback can only be enabled for triggers with metric of type AverageValue", trigger.MetricType)
 		}
 	}
 	return nil
