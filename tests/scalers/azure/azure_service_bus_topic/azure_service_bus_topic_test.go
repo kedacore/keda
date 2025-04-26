b7f8619d23356b41a7f2f8a0bfbad8eecdd6fabf
Jorge Turrado <jorge_turrado@hotmail.es>
2025-04-26 22:32:44 +0200

    fix: Use pinned version for nginx image
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- tests/scalers/azure/azure_service_bus_topic/azure_service_bus_topic_test.go
+++ tests/scalers/azure/azure_service_bus_topic/azure_service_bus_topic_test.go
@@ -79,7 +79,7 @@ spec:
     spec:
       containers:
         - name: nginx
-          image: nginxinc/nginx-unprivileged
+          image: ghcr.io/nginx/nginx-unprivileged:1.26
 `
 
 	triggerAuthTemplate = `
