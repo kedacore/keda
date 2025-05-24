a97ae1d10a624705724596ab07ede9f318cb2326
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2025-05-25 01:26:50 +0530

    (feat): Fix e2e
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>

--- tests/scalers/sumologic/sumologic_test.go
+++ tests/scalers/sumologic/sumologic_test.go
@@ -4,10 +4,14 @@
 package sumologic_test
 
 import (
+	"bytes"
+	"context"
 	"encoding/base64"
 	"fmt"
+	"net/http"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/kubernetes"
@@ -16,26 +20,31 @@ import (
 )
 
 const (
-	testName = "sumologic-test"
+	testName = "sumologic-keda-test"
+	host     = "https://api.sumologic.com"
 )
 
 var (
-	testNamespace          = fmt.Sprintf("%s-ns", testName)
-	deploymentName         = fmt.Sprintf("%s-deployment", testName)
-	scaledObjectName       = fmt.Sprintf("%s-so", testName)
-	secretName             = fmt.Sprintf("%s-secret", testName)
-	accessID               = os.Getenv("SUMO_LOGIC_ACCESS_ID")
-	accessKey              = os.Getenv("SUMO_LOGIC_ACCESS_KEY")
-	query                  = "_sourceCategory=prod | count by _sourceHost"
-	queryType              = "logs"
-	resultField            = "_count"
-	timeRange              = "15"
-	timeZone               = "Asia/Kolkata"
-	queryAggregator        = "Max"
-	maxReplicaCount        = 2
-	minReplicaCount        = 0
-	scaleInTargetValue     = "10"
-	scaleInActivationValue = "15"
+	testNamespace           = fmt.Sprintf("%s-ns", testName)
+	deploymentName          = fmt.Sprintf("%s-deployment", testName)
+	scaledObjectName        = fmt.Sprintf("%s-so", testName)
+	secretName              = fmt.Sprintf("%s-secret", testName)
+	accessID                = os.Getenv("SUMO_LOGIC_ACCESS_ID")
+	accessKey               = os.Getenv("SUMO_LOGIC_ACCESS_KEY")
+	collectorURL            = os.Getenv("SUMO_LOGIC_COLLECTOR_URL")
+	logPushInterval         = 5 * time.Second
+	query                   = fmt.Sprintf("_sourceCategory=%s | count", testName)
+	queryType               = "logs"
+	resultField             = "_count"
+	timeRange               = "2m"
+	timeZone                = "Asia/Kolkata"
+	queryAggregator         = "Max"
+	minReplicaCount         = 0
+	maxReplicaCount         = 2
+	scaleInTargetValue      = "100000"
+	scaleInActivationValue  = "100000"
+	scaleOutTargetValue     = "6"
+	scaleOutActivationValue = "1"
 )
 
 type templateData struct {
@@ -47,6 +56,7 @@ type templateData struct {
 	SumologicAccessIDB64  string
 	SumologicAccessKey    string
 	SumologicAccessKeyB64 string
+	Host                  string
 	Query                 string
 	QueryType             string
 	ResultField           string
@@ -86,10 +96,14 @@ spec:
   maxReplicaCount: {{.MaxReplicaCount}}
   pollingInterval: 3
   cooldownPeriod: 1
+  fallback:
+    failureThreshold: 3
+    replicas: 0
   triggers:
     - type: sumologic
+      metricType: AverageValue
       metadata:
-        host: "https://api.sumologic.com"
+        host: "{{.Host}}"
         queryType: "{{.QueryType}}"
         query: "{{.Query}}"
         resultField: "{{.ResultField}}"
@@ -145,6 +159,7 @@ spec:
 )
 
 func TestSumologicScaler(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
 	kc := GetKubernetesClient(t)
 	data, templates := getTemplateData()
 	t.Cleanup(func() {
@@ -160,7 +175,11 @@ func TestSumologicScaler(t *testing.T) {
 
 	// Test scaling
 	testActivation(t, kc)
+
+	go pushDataToSumoLogic(t, ctx)
 	testScaleOut(t, kc)
+	cancel()
+
 	testScaleIn(t, kc)
 }
 
@@ -174,7 +193,7 @@ func testScaleOut(t *testing.T, kc *kubernetes.Clientset) {
 	t.Log("--- testing scale out ---")
 
 	// Update scaled object to force scaling out
-	data := getScaledObjectTemplateData("1", "9")
+	data := getScaledObjectTemplateData(scaleOutTargetValue, scaleOutActivationValue)
 	KubectlApplyWithTemplate(t, data, "scaledObjectTemplateToScaleOut", scaledObjectTemplate)
 
 	assert.True(t, WaitForDeploymentReplicaReadyCount(t, kc, deploymentName, testNamespace, maxReplicaCount, 60, 3),
@@ -192,6 +211,45 @@ func testScaleIn(t *testing.T, kc *kubernetes.Clientset) {
 		"replica count should be %d after 3 minutes", minReplicaCount)
 }
 
+func pushDataToSumoLogic(t *testing.T, ctx context.Context) {
+	if collectorURL == "" {
+		t.Logf("No collector URL set, failed data push to Sumo Logic")
+		return
+	}
+
+	client := &http.Client{}
+	ticker := time.NewTicker(logPushInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			payload := []byte(fmt.Sprintf("log generated at %s", time.Now().Format(time.RFC3339)))
+
+			req, err := http.NewRequest("POST", collectorURL, bytes.NewBuffer(payload))
+			if err != nil {
+				t.Logf("Failed to create request: %v", err)
+				continue
+			}
+
+			req.Header.Set("X-Sumo-Category", testName)
+			resp, err := client.Do(req)
+			if err != nil {
+				t.Logf("Failed to push data: %v", err)
+				continue
+			}
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+				t.Logf("Non-OK response from Sumo Logic: %s", resp.Status)
+			}
+		}
+	}
+}
+
 func getTemplateData() (templateData, []Template) {
 	return templateData{
 			TestNamespace:         testNamespace,
@@ -202,6 +260,7 @@ func getTemplateData() (templateData, []Template) {
 			SumologicAccessIDB64:  base64.StdEncoding.EncodeToString([]byte(accessID)),
 			SumologicAccessKey:    accessKey,
 			SumologicAccessKeyB64: base64.StdEncoding.EncodeToString([]byte(accessKey)),
+			Host:                  host,
 			Query:                 query,
 			QueryType:             queryType,
 			ResultField:           resultField,
@@ -225,6 +284,7 @@ func getScaledObjectTemplateData(targetValue, activationValue string) templateDa
 		TestNamespace:    testNamespace,
 		DeploymentName:   deploymentName,
 		ScaledObjectName: scaledObjectName,
+		Host:             host,
 		Query:            query,
 		QueryType:        queryType,
 		ResultField:      resultField,
