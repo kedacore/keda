b2e81d0042a1f8ce9aec0335bea8c3b4565da7f4
Vaibhav Mittal <53215737+mittalvaibhav1@users.noreply.github.com>
2025-07-10 17:19:31 +0530

    (feat): Add Sumo Logic scaler (#6736)
    * (feat): Add barebones for sumologic scaler
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): refactoring...
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): add client support for metric queries
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): process and return log search response as floats
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): fix tests
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * Adding sumologic scaler file
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): refactoring sumologic scaler
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): bug fixes
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): adopt a logging frameworkin in sumologic package
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): fix tests..
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): fix metrics queries
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): bug fixes
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): bug fixes
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): bug fixes
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): bug fixes
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): bug fixes
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): fixing metric name
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Adding resultfield and rollup for query result method
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): allow users to specify result field and rollup type
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): use cookies as recommended
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Updating test cases for sumologic_scaler
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Add support for multi metrics queries
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): fix tests
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): bug fixes
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): fix typo + readability improvements
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): pre-commit changes + add tests
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Fix PR checks
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Update changelog
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): PR checks
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): use TypedConfig pattern
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): remove unnecessary fields
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Use builder
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Address comments
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Fix e2e
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Address comments
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    * (feat): Trigger tests again
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    
    ---------
    
    Signed-off-by: mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
    Co-authored-by: Anoop Bansal <anoop.bansal@sumologic.com>
    Signed-off-by: David Pochopsky <david.pochopsky@united.com>

---
+++ pkg/scalers/sumologic/metrics.go
@@ -0,0 +1,172 @@
+package sumologic
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+)
+
+const (
+	metricsQueryPath = "api/v1/metricsQueries"
+)
+
+var validRollupTypes = map[string]bool{
+	"Avg":   true,
+	"Sum":   true,
+	"Min":   true,
+	"Max":   true,
+	"Count": true,
+}
+
+var validQueryAggregations = map[string]bool{
+	"Latest": true,
+	"Avg":    true,
+	"Sum":    true,
+	"Min":    true,
+	"Max":    true,
+	"Count":  true,
+}
+
+func (c *Client) createMetricsQuery(query string, quantization time.Duration, from, to, rollup string) ([]byte, error) {
+	metricsQuery := MetricsQuery{
+		RowID:        "A",
+		Query:        query,
+		Quantization: int64(quantization / time.Millisecond),
+		Rollup:       rollup,
+	}
+
+	timeRange := TimeRange{
+		Type: "BeginBoundedTimeRange",
+		From: TimeRangeBoundary{
+			Type:        "Iso8601TimeRangeBoundary",
+			Iso8601Time: from,
+		},
+		To: TimeRangeBoundary{
+			Type:        "Iso8601TimeRangeBoundary",
+			Iso8601Time: to,
+		},
+	}
+
+	requestPayload := MetricsQueryRequest{
+		Queries:   []MetricsQuery{metricsQuery},
+		TimeRange: timeRange,
+	}
+
+	payload, err := json.Marshal(requestPayload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal metrics query request: %w", err)
+	}
+
+	url := fmt.Sprintf("%s/%s", c.config.Host, metricsQueryPath)
+	return c.makeRequest("POST", url, payload)
+}
+
+func (c *Client) createMultiMetricsQuery(queries map[string]string, quantization time.Duration, from, to, rollup string) ([]byte, error) {
+	metricsQueries := make([]MetricsQuery, 0)
+	for rowID, query := range queries {
+		metricsQuery := MetricsQuery{
+			RowID:        rowID,
+			Query:        query,
+			Quantization: int64(quantization / time.Millisecond),
+			Rollup:       rollup,
+		}
+		metricsQueries = append(metricsQueries, metricsQuery)
+	}
+
+	timeRange := TimeRange{
+		Type: "BeginBoundedTimeRange",
+		From: TimeRangeBoundary{
+			Type:        "Iso8601TimeRangeBoundary",
+			Iso8601Time: from,
+		},
+		To: TimeRangeBoundary{
+			Type:        "Iso8601TimeRangeBoundary",
+			Iso8601Time: to,
+		},
+	}
+
+	requestPayload := MetricsQueryRequest{
+		Queries:   metricsQueries,
+		TimeRange: timeRange,
+	}
+
+	payload, err := json.Marshal(requestPayload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal metrics query request: %w", err)
+	}
+
+	url := fmt.Sprintf("%s/%s", c.config.Host, metricsQueryPath)
+	return c.makeRequest("POST", url, payload)
+}
+
+func (c *Client) parseMetricsQueryResponse(response []byte) (*MetricsQueryResponse, error) {
+	var metricsResponse MetricsQueryResponse
+	if err := json.Unmarshal(response, &metricsResponse); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal metrics query response: %w", err)
+	}
+
+	if metricsResponse.Errors != nil && len(metricsResponse.Errors.Errors) > 0 {
+		return nil, fmt.Errorf("query execution failed with errors %v", metricsResponse.Errors.Errors)
+	}
+
+	return &metricsResponse, nil
+}
+
+func (c *Client) metricsStats(values []float64, dimension string) (*float64, error) {
+	if len(values) == 0 {
+		return nil, errors.New("no values provided")
+	}
+
+	var result float64
+	switch dimension {
+	case "Latest":
+		result = values[len(values)-1]
+	case "Sum":
+		for _, v := range values {
+			result += v
+		}
+	case "Count":
+		result = float64(len(values))
+	case "Avg":
+		var sum float64
+		for _, v := range values {
+			sum += v
+		}
+		result = sum / float64(len(values))
+	case "Min":
+		minVal := values[0]
+		for _, v := range values[1:] {
+			if v < minVal {
+				minVal = v
+			}
+		}
+		result = minVal
+	case "Max":
+		maxVal := values[0]
+		for _, v := range values[1:] {
+			if v > maxVal {
+				maxVal = v
+			}
+		}
+		result = maxVal
+	default:
+		return nil, fmt.Errorf("invalid aggregation '%s', supported values: Latest, Avg, Sum, Count, Min, Max", dimension)
+	}
+
+	return &result, nil
+}
+
+func IsValidRollupType(rollup string) error {
+	if !validRollupTypes[rollup] {
+		return fmt.Errorf("invalid rollup value: %s, must be one of Avg, Sum, Min, Max, Count", rollup)
+	}
+	return nil
+}
+
+func IsValidQueryAggregation(aggregation string) error {
+	if !validQueryAggregations[aggregation] {
+		return fmt.Errorf("invalid aggregation '%s', supported values: Latest, Avg, Sum, Count, Min, Max", aggregation)
+	}
+	return nil
+}
---
+++ tests/scalers/sumologic/sumologic_test.go
@@ -0,0 +1,299 @@
+//go:build e2e
+// +build e2e
+
+package sumologic_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/kubernetes"
+
+	. "github.com/kedacore/keda/v2/tests/helper"
+)
+
+const (
+	testName = "sumologic-keda-test"
+	host     = "https://api.sumologic.com"
+)
+
+var (
+	testNamespace           = fmt.Sprintf("%s-ns", testName)
+	deploymentName          = fmt.Sprintf("%s-deployment", testName)
+	scaledObjectName        = fmt.Sprintf("%s-so", testName)
+	secretName              = fmt.Sprintf("%s-secret", testName)
+	accessID                = os.Getenv("SUMO_LOGIC_ACCESS_ID")
+	accessKey               = os.Getenv("SUMO_LOGIC_ACCESS_KEY")
+	collectorURL            = os.Getenv("SUMO_LOGIC_COLLECTOR_URL")
+	logPushInterval         = 5 * time.Second
+	query                   = fmt.Sprintf("_sourceCategory=%s | count", testName)
+	queryType               = "logs"
+	resultField             = "_count"
+	timeRange               = "2m"
+	timeZone                = "Asia/Kolkata"
+	queryAggregator         = "Max"
+	minReplicaCount         = 0
+	maxReplicaCount         = 2
+	scaleInTargetValue      = "100000"
+	scaleInActivationValue  = "100000"
+	scaleOutTargetValue     = "6"
+	scaleOutActivationValue = "1"
+)
+
+type templateData struct {
+	TestNamespace         string
+	DeploymentName        string
+	ScaledObjectName      string
+	SecretName            string
+	SumologicAccessID     string
+	SumologicAccessIDB64  string
+	SumologicAccessKey    string
+	SumologicAccessKeyB64 string
+	Host                  string
+	Query                 string
+	QueryType             string
+	ResultField           string
+	TimeRange             string
+	TimeZone              string
+	QueryAggregator       string
+	MinReplicaCount       string
+	MaxReplicaCount       string
+	TargetValue           string
+	ActivationValue       string
+}
+
+const (
+	secretTemplate = `
+apiVersion: v1
+kind: Secret
+metadata:
+  name: {{.SecretName}}
+  namespace: {{.TestNamespace}}
+data:
+  accessID: {{.SumologicAccessIDB64}}
+  accessKey: {{.SumologicAccessKeyB64}}
+`
+
+	scaledObjectTemplate = `
+apiVersion: keda.sh/v1alpha1
+kind: ScaledObject
+metadata:
+  name: {{.ScaledObjectName}}
+  namespace: {{.TestNamespace}}
+  labels:
+    app: {{.DeploymentName}}
+spec:
+  scaleTargetRef:
+    name: {{.DeploymentName}}
+  minReplicaCount: {{.MinReplicaCount}}
+  maxReplicaCount: {{.MaxReplicaCount}}
+  pollingInterval: 3
+  cooldownPeriod: 1
+  fallback:
+    failureThreshold: 3
+    replicas: 0
+  triggers:
+    - type: sumologic
+      metricType: AverageValue
+      metadata:
+        host: "{{.Host}}"
+        queryType: "{{.QueryType}}"
+        query: "{{.Query}}"
+        resultField: "{{.ResultField}}"
+        timerange: "{{.TimeRange}}"
+        timezone: "{{.TimeZone}}"
+        queryAggregator: "{{.QueryAggregator}}"
+        threshold: "{{.TargetValue}}"
+        activationValue: "{{.ActivationValue}}"
+      authenticationRef:
+        name: keda-trigger-auth-sumologic
+`
+
+	triggerAuthenticationTemplate = `
+apiVersion: keda.sh/v1alpha1
+kind: TriggerAuthentication
+metadata:
+  name: keda-trigger-auth-sumologic
+  namespace: {{.TestNamespace}}
+spec:
+  secretTargetRef:
+    - parameter: accessID
+      name: {{.SecretName}}
+      key: accessID
+    - parameter: accessKey
+      name: {{.SecretName}}
+      key: accessKey
+`
+
+	deploymentTemplate = `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: {{.DeploymentName}}
+  namespace: {{.TestNamespace}}
+  labels:
+    app: {{.DeploymentName}}
+spec:
+  replicas: 0
+  selector:
+    matchLabels:
+      app: {{.DeploymentName}}
+  template:
+    metadata:
+      labels:
+        app: {{.DeploymentName}}
+    spec:
+      containers:
+      - name: nginx
+        image: nginxinc/nginx-unprivileged
+        ports:
+        - containerPort: 80
+`
+)
+
+func TestSumologicScaler(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	kc := GetKubernetesClient(t)
+	data, templates := getTemplateData()
+	t.Cleanup(func() {
+		DeleteKubernetesResources(t, testNamespace, data, templates)
+	})
+
+	// Create Kubernetes resources
+	CreateKubernetesResources(t, kc, testNamespace, data, templates)
+
+	// Ensure deployment is at min replica count
+	assert.True(t, WaitForDeploymentReplicaReadyCount(t, kc, deploymentName, testNamespace, minReplicaCount, 180, 3),
+		"replica count should be %d after 3 minutes", minReplicaCount)
+
+	// Test scaling
+	testActivation(t, kc)
+
+	go pushDataToSumoLogic(t, ctx)
+	testScaleOut(t, kc)
+	cancel()
+
+	testScaleIn(t, kc)
+}
+
+func testActivation(t *testing.T, kc *kubernetes.Clientset) {
+	t.Log("--- testing activation ---")
+
+	AssertReplicaCountNotChangeDuringTimePeriod(t, kc, deploymentName, testNamespace, minReplicaCount, 60)
+}
+
+func testScaleOut(t *testing.T, kc *kubernetes.Clientset) {
+	t.Log("--- testing scale out ---")
+
+	// Update scaled object to force scaling out
+	data := getScaledObjectTemplateData(scaleOutTargetValue, scaleOutActivationValue)
+	KubectlApplyWithTemplate(t, data, "scaledObjectTemplateToScaleOut", scaledObjectTemplate)
+
+	assert.True(t, WaitForDeploymentReplicaReadyCount(t, kc, deploymentName, testNamespace, maxReplicaCount, 60, 3),
+		"replica count should be %d after 3 minutes", maxReplicaCount)
+}
+
+func testScaleIn(t *testing.T, kc *kubernetes.Clientset) {
+	t.Log("--- testing scale in ---")
+
+	// Update scaled object to force scaling in
+	data := getScaledObjectTemplateData(scaleInTargetValue, scaleInActivationValue)
+	KubectlApplyWithTemplate(t, data, "scaledObjectTemplateToScaleIn", scaledObjectTemplate)
+
+	assert.True(t, WaitForDeploymentReplicaReadyCount(t, kc, deploymentName, testNamespace, minReplicaCount, 60, 3),
+		"replica count should be %d after 3 minutes", minReplicaCount)
+}
+
+func pushDataToSumoLogic(t *testing.T, ctx context.Context) {
+	if collectorURL == "" {
+		t.Logf("No collector URL set, failed data push to Sumo Logic")
+		return
+	}
+
+	client := &http.Client{}
+	ticker := time.NewTicker(logPushInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			payload := []byte(fmt.Sprintf("log generated at %s", time.Now().Format(time.RFC3339)))
+
+			req, err := http.NewRequest("POST", collectorURL, bytes.NewBuffer(payload))
+			if err != nil {
+				t.Logf("Failed to create request: %v", err)
+				continue
+			}
+
+			req.Header.Set("X-Sumo-Category", testName)
+			resp, err := client.Do(req)
+			if err != nil {
+				t.Logf("Failed to push data: %v", err)
+				continue
+			}
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+				t.Logf("Non-OK response from Sumo Logic: %s", resp.Status)
+			}
+		}
+	}
+}
+
+func getTemplateData() (templateData, []Template) {
+	return templateData{
+			TestNamespace:         testNamespace,
+			DeploymentName:        deploymentName,
+			ScaledObjectName:      scaledObjectName,
+			SecretName:            secretName,
+			SumologicAccessID:     accessID,
+			SumologicAccessIDB64:  base64.StdEncoding.EncodeToString([]byte(accessID)),
+			SumologicAccessKey:    accessKey,
+			SumologicAccessKeyB64: base64.StdEncoding.EncodeToString([]byte(accessKey)),
+			Host:                  host,
+			Query:                 query,
+			QueryType:             queryType,
+			ResultField:           resultField,
+			TimeRange:             timeRange,
+			TimeZone:              timeZone,
+			QueryAggregator:       queryAggregator,
+			MinReplicaCount:       fmt.Sprintf("%v", minReplicaCount),
+			MaxReplicaCount:       fmt.Sprintf("%v", maxReplicaCount),
+			TargetValue:           scaleInTargetValue,
+			ActivationValue:       scaleInActivationValue,
+		}, []Template{
+			{Name: "secretTemplate", Config: secretTemplate},
+			{Name: "triggerAuthenticationTemplate", Config: triggerAuthenticationTemplate},
+			{Name: "deploymentTemplate", Config: deploymentTemplate},
+			{Name: "scaledObjectTemplate", Config: scaledObjectTemplate},
+		}
+}
+
+func getScaledObjectTemplateData(targetValue, activationValue string) templateData {
+	return templateData{
+		TestNamespace:    testNamespace,
+		DeploymentName:   deploymentName,
+		ScaledObjectName: scaledObjectName,
+		Host:             host,
+		Query:            query,
+		QueryType:        queryType,
+		ResultField:      resultField,
+		TimeRange:        timeRange,
+		TimeZone:         timeZone,
+		QueryAggregator:  queryAggregator,
+		MinReplicaCount:  fmt.Sprintf("%v", minReplicaCount),
+		MaxReplicaCount:  fmt.Sprintf("%v", maxReplicaCount),
+		TargetValue:      targetValue,
+		ActivationValue:  activationValue,
+	}
+}
