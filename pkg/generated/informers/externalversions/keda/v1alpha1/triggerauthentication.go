db40770ca9d768945d7ecccc6441f9de78d26d82
Tobias <bvrcreepyx@hotmail.de>
2026-02-15 21:42:41 +0100

    chore: make clientset-generate
    Signed-off-by: Tobias <bvrcreepyx@hotmail.de>

--- pkg/generated/informers/externalversions/keda/v1alpha1/triggerauthentication.go
+++ pkg/generated/informers/externalversions/keda/v1alpha1/triggerauthentication.go
@@ -57,7 +57,7 @@ func NewTriggerAuthenticationInformer(client versioned.Interface, namespace stri
 // one. This reduces memory footprint and number of connections to the server.
 func NewFilteredTriggerAuthenticationInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
-		&cache.ListWatch{
+		cache.ToListWatcherWithWatchListSemantics(&cache.ListWatch{
 			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
@@ -82,7 +82,7 @@ func NewFilteredTriggerAuthenticationInformer(client versioned.Interface, namesp
 				}
 				return client.KedaV1alpha1().TriggerAuthentications(namespace).Watch(ctx, options)
 			},
-		},
+		}, client),
 		&apiskedav1alpha1.TriggerAuthentication{},
 		resyncPeriod,
 		indexers,
