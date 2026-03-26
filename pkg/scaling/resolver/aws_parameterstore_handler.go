2517b5d265b69d418dfdd301445fbab5cf4fd057
Julia Jablonska <julia.jablonska@zestia.com>
2026-03-26 11:18:36 +0000

    Refactor AWS initialisation to avoid duplication
    Signed-off-by: Julia Jablonska <julia.jablonska@zestia.com>

--- pkg/scaling/resolver/aws_parameterstore_handler.go
+++ pkg/scaling/resolver/aws_parameterstore_handler.go
@@ -69,40 +69,12 @@ func (apsh *AwsParameterStoreHandler) Read(ctx context.Context, logger logr.Logg
 }
 
 func (apsh *AwsParameterStoreHandler) Initialize(ctx context.Context, client client.Client, logger logr.Logger, triggerNamespace string, secretsLister corev1listers.SecretLister, podSpec *corev1.PodSpec) error {
-	apsh.awsMetadata = awsutils.AuthorizationMetadata{
-		TriggerUniqueKey: fmt.Sprintf("aws-parameter-store-%s", triggerNamespace),
+	metadata, err := initializeAwsMetadata(ctx, client, logger, triggerNamespace, secretsLister, podSpec,
+		"aws-parameter-store", apsh.parameterStore.Region, apsh.parameterStore.PodIdentity, apsh.parameterStore.Credentials)
+	if err != nil {
+		return err
 	}
-	awsRegion := ""
-	if apsh.parameterStore.Region != "" {
-		awsRegion = apsh.parameterStore.Region
-	}
-	apsh.awsMetadata.AwsRegion = awsRegion
-	podIdentity := apsh.parameterStore.PodIdentity
-	if podIdentity == nil {
-		podIdentity = &kedav1alpha1.AuthPodIdentity{}
-	}
-
-	switch podIdentity.Provider {
-	case "", kedav1alpha1.PodIdentityProviderNone:
-		apsh.awsMetadata.AwsAccessKeyID = resolveAuthSecret(ctx, client, logger, apsh.parameterStore.Credentials.AccessKey.ValueFrom.SecretKeyRef.Name, triggerNamespace, apsh.parameterStore.Credentials.AccessKey.ValueFrom.SecretKeyRef.Key, secretsLister)
-		apsh.awsMetadata.AwsSecretAccessKey = resolveAuthSecret(ctx, client, logger, apsh.parameterStore.Credentials.AccessSecretKey.ValueFrom.SecretKeyRef.Name, triggerNamespace, apsh.parameterStore.Credentials.AccessSecretKey.ValueFrom.SecretKeyRef.Key, secretsLister)
-		if apsh.awsMetadata.AwsAccessKeyID == "" || apsh.awsMetadata.AwsSecretAccessKey == "" {
-			return fmt.Errorf("AccessKeyID and AccessSecretKey are expected when not using a pod identity provider")
-		}
-	case kedav1alpha1.PodIdentityProviderAws:
-		apsh.awsMetadata.UsingPodIdentity = true
-		if apsh.parameterStore.PodIdentity.IsWorkloadIdentityOwner() {
-			awsRoleArn, err := resolveServiceAccountAnnotation(ctx, client, podSpec.ServiceAccountName, triggerNamespace, kedav1alpha1.PodIdentityAnnotationEKS, true)
-			if err != nil {
-				return fmt.Errorf("error resolving role arn for aws: %w", err)
-			}
-			apsh.awsMetadata.AwsRoleArn = awsRoleArn
-		} else if apsh.parameterStore.PodIdentity.RoleArn != nil {
-			apsh.awsMetadata.AwsRoleArn = *apsh.parameterStore.PodIdentity.RoleArn
-		}
-	default:
-		return fmt.Errorf("pod identity provider %s not supported", podIdentity.Provider)
-	}
+	apsh.awsMetadata = metadata
 
 	config, err := awsutils.GetAwsConfig(ctx, apsh.awsMetadata)
 	if err != nil {
