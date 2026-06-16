b61831055eb45753ae2fe65ad647fdcf82e4b8a7
Vincent Link <linkvt@users.noreply.github.com>
2026-06-16 10:25:36 +0200

    chore: remove vendor directory (#7839)
    The vendor directory used 200+ MB and provided no meaningful
    benefit: go.sum ensures reproducibility, proxy.golang.org ensures
    availability, and vendoring bloated the repo and dependency update diffs.
    
    Changes:
    - Remove vendor/ directory
    - Remove -mod=vendor from build commands and -I vendor from protoc
    - Update Dockerfiles to cache and download dependencies
    - Update hack/update-codegen.sh to resolve code-generator via module cache
    - Remove vendor-related config from .golangci.yml, .pre-commit-config.yaml,
      .gitignore, and trivy.yml
    - Remove now-empty trivy.yml and its workflow reference
    
    Signed-off-by: Vincent Link <vlink@redhat.com>

--- vendor/github.com/Huawei/gophercloud/openstack/identity/v3/tokens/urls.go
+++
@@ -1,7 +0,0 @@
-package tokens
-
-import "github.com/Huawei/gophercloud"
-
-func tokenURL(c *gophercloud.ServiceClient) string {
-	return c.ServiceURL("auth", "tokens")
-}
--- vendor/github.com/robertkrimen/otto/generate.go
+++
@@ -1,4 +0,0 @@
-package otto
-
-//go:generate go run ./tools/gen-jscore -output inline.go
-//go:generate stringer -type=valueKind -trimprefix=value -output=value_kind.gen.go
