369bf983841ccd375b85261e8391ecc3959a796d
Jan Wozniak <wozniak.jan@gmail.com>
2026-09-15 10:18:14 +0200

    prune verbose comments
    Signed-off-by: Jan Wozniak <wozniak.jan@gmail.com>

--- pkg/scalers/couchdb_scaler.go
+++ pkg/scalers/couchdb_scaler.go
@@ -153,8 +153,6 @@ func (s *couchDBScaler) getQueryResult(ctx context.Context) (int64, error) {
 	}
 
 	rows := db.Find(ctx, request)
-	// Close releases the underlying HTTP response body. Exhausting the iterator
-	// closes it implicitly, but an early return below would otherwise leak it.
 	defer rows.Close()
 
 	var count int64
